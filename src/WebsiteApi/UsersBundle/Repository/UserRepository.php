1097fe0c7423b81374b356cf3506a01608ac5f12
founski <victor.cholley-barroyer@telecomnancy.net>
2018-01-09 11:36:01 +0100

    findUser put into the right repository

--- src/WebsiteApi/UsersBundle/Repository/UserRepository.php
+++ src/WebsiteApi/UsersBundle/Repository/UserRepository.php
@@ -54,4 +54,22 @@ class UserRepository extends \Doctrine\ORM\EntityRepository
         return $req;
     }
 
+    public function findUsersByFilter($lastName,$firstName,$userName,$email){
+        $req = $this->createQueryBuilder('U');
+        $req->where('1=1');
+        if($lastName != null){
+            $req->andWhere('U.lastName LIKE \'%' . $lastName.'%\'');
+        }
+        if($firstName != null){
+            $req->andWhere('U.firstName LIKE \'%' . $firstName.'%\'');
+        }
+        if($userName != null){
+            $req->andWhere('U.userName LIKE \'%' . $userName.'%\'');
+        }
+        if($email != null){
+            $req->andWhere('U.email LIKE \'%' . $email.'%\'');
+        }
+        return $req;
+    }
+
 }
