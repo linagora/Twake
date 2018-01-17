a75006093200e6b27fa7bece9a678a5e02626561
founski <victor.cholley-barroyer@telecomnancy.net>
2018-01-17 10:41:16 +0100

    adding 2 new repo

---
+++ src/Administration/AuthenticationBundle/Repository/UserDailyStatsRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: founski
+ * Date: 17/01/18
+ * Time: 10:39
+ */
+
+namespace Administration\AuthenticationBundle\Repository;
+
+
+class UserDailyStatsRepository extends \Doctrine\ORM\EntityRepository
+{
+
+}
---
+++ src/Administration/AuthenticationBundle/Repository/WorkspaceDailyStatsRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: founski
+ * Date: 17/01/18
+ * Time: 10:40
+ */
+
+namespace Administration\AuthenticationBundle\Repository;
+
+
+class WorkspaceDailyStatsRepository extends \Doctrine\ORM\EntityRepository
+{
+
+}
