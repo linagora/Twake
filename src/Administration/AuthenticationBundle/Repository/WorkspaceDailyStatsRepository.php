ad9d3d570d764811b8ed7de7e2d763af569aa433
founski <victor.cholley-barroyer@telecomnancy.net>
2018-01-19 10:47:28 +0100

    numberOfMessagePublicByWorkspace function implemented

--- src/Administration/AuthenticationBundle/Repository/WorkspaceDailyStatsRepository.php
+++ src/Administration/AuthenticationBundle/Repository/WorkspaceDailyStatsRepository.php
@@ -13,5 +13,16 @@ use Administration\AuthenticationBundle\Entity\WorkspaceDailyStats;
 
 class WorkspaceDailyStatsRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function getStatsPublicMessageByWorkspace($idWorkspace,$startdate,$enddate){
 
+        $req = $this->createQueryBuilder('U')
+            ->select('U.date, U.publicMsgCount')
+            ->where('U.workspace = ' . $idWorkspace)
+            ->andWhere('U.date >= :start')
+            ->andWhere('U.date <= :end')
+            ->setParameter("start",$startdate)
+            ->setParameter("end",$enddate);
+
+        return $req->getQuery()->getResult();
+    }
 }
