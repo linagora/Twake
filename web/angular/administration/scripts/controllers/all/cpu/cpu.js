b2d7cdb5cb6f53a5c18a0a4bdc05ea1c5a9cf8ba
goffio <demangeon.gauthier@gmail.com>
2018-02-15 16:29:12 +0100

    ADD front storage chart

--- web/angular/administration/scripts/controllers/all/cpu/cpu.js
+++ web/angular/administration/scripts/controllers/all/cpu/cpu.js
@@ -10,14 +10,16 @@ angular.module('TwakeAdministration')
         var that = this;
 
         this.update = function(){
-            console.log("test");
             $api.post("authentication/getCpuUsage", null, function (res) {
                 that.drawCPUDonut(res);
                 $api.post("authentication/getStorageSpace", null, function (res) {
                     console.log(res.data);
-                    //that.drawStorageDonut(res);
-                    $scope.$apply();
+                    that.drawStorageDonut(res.data);
+                    /*$api.post("authentication/getRamUsage", null, function (res) {
+                        console.log(res.data);
+                    });*/
                 });
+                $scope.$apply();
             });
         }
 
@@ -41,6 +43,40 @@ angular.module('TwakeAdministration')
 
                     // These labels appear in the legend and in the tooltips when hovering different arcs
                     labels: labels
+                },
+                options: {
+                    responsive: true,
+                    title: {
+                        display: false,
+                        position: "top",
+                        text: "Pie Chart",
+                        fontSize: 18,
+                        fontColor: "#111"
+                    },
+                    legend: {
+                        display: true,
+                        position: "bottom",
+                        labels: {
+                            fontColor: "#333",
+                            fontSize: 16
+                        }
+                    }
+                }
+            });
+        };
+        this.drawStorageDonut = function (data) {
+
+            var ctx = document.getElementById("myDonut2");
+            var myDoughnutChart = new Chart(ctx, {
+                type: 'doughnut',
+                data: data = {
+                    datasets: [{
+                        data: [data.utilise,data.total - data.utilise],
+                        backgroundColor: ["rgba(205,0,0,0.8)","rgba(0,139,0,0.8)"]
+                    }],
+
+                    // These labels appear in the legend and in the tooltips when hovering different arcs
+                    labels: ["Used Storage", "Free Storage"]
                 },
                 options: {
                     responsive: true,
