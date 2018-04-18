b5f706a9a2c40b29a12351cd7da0f9ba808255c2
Elliot THIS <minisephirot@hotmail.fr>
2018-04-18 14:45:09 +0200

    Added backend route for creating rights for workspaces

--- src/WebsiteApi/WorkspacesBundle/Controller/WorkspaceLevelsController.php
+++ src/WebsiteApi/WorkspacesBundle/Controller/WorkspaceLevelsController.php
@@ -7,8 +7,8 @@
  */
 
 namespace WebsiteApi\WorkspacesBundle\Controller;
-use WebsiteApi\WorkspacesBundle\Entity\WorkspaceUser;
-use WebsiteApi\WorkspacesBundle\Entity\Workspace;
+use WebsiteApi\WorkspacesBundle\Entity\WorkspaceLevel;
+use WebsiteApi\WorkspacesBundle\Model\WorkspaceLevelsInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -36,5 +36,29 @@ class WorkspaceLevelsController extends Controller
 
 		return new JsonResponse($response);
 	}
+
+    /**
+     * Get list of workspace levels
+     */
+    public function createLevelAction(Request $request){
+
+        $response = Array("errors"=>Array(), "data"=>Array());
+
+        $workspaceId = $request->request->getInt("workspaceId");
+        $label = $request->request->get("label");
+        $rights = $this->get("app.workspace_levels")->getDefaultLevel($workspaceId)->getRights();
+
+        $res = $this->get("app.workspace_levels")->addLevel($workspaceId,$label,$rights, $this->getUser()->getId());
+
+        if ($res){
+            $response["data"] = "success";
+        }else{
+            $response["errors"] = "notauthorized";
+        }
+
+        return new JsonResponse($response);
+    }
+
+
 
 }
