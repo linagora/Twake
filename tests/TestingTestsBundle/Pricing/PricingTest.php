e989bcd44fd3fdd71dccd36ea5f6d49e8db4a070
yoanf <yoanfath@laposte.net>
2018-05-02 15:03:43 +0200

    Unary Test pricing

---
+++ tests/TestingTestsBundle/Pricing/PricingTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use Tests\WebTestCaseExtended;
+
+/**
+ * Created by PhpStorm.
+ * User: yoanf
+ * Date: 02/05/2018
+ * Time: 14:09
+ */
+class PricingTest extends WebTestCaseExtended
+{
+
+    public function initData(){
+        $this->destroyTestData();
+
+        $user = $this->newUser();
+        $this->getDoctrine()->persist($user);
+        $this->getDoctrine()->flush();
+
+        $group = $this->newGroup($user->getId());
+        $this->getDoctrine()->persist($group);
+        $this->getDoctrine()->flush();
+
+        $work = $this->newWorkspace($group->getId());
+        $this->getDoctrine()->persist($work);
+        $this->getDoctrine()->flush();
+
+        $this->get("app.workspace_members")->addMember($work->getId(),$user->getId());
+
+        // creer des utilisateurs
+        for($i = 1; $i < 10 ; $i++){
+            $user = $this->newUserByName("PHPUNIT".$i);
+            $this->get("app.workspace_members")->addMember($work->getId(),$user->getId());
+            $this->getDoctrine()->persist($user);
+        }
+
+        $this->getDoctrine()->flush();
+    }
+
+    public function testIndex()
+    {
+
+
+    }
+
+}
