f4639818e1847e112f7f53e6651c95e17d08d8c8
Elliot THIS <minisephirot@hotmail.fr>
2018-06-01 14:00:21 +0200

    Added more possibility to sp *HUM* collect global data

--- src/Administration/AuthenticationBundle/Services/AdministrationServerStats.php
+++ src/Administration/AuthenticationBundle/Services/AdministrationServerStats.php
@@ -115,12 +115,30 @@ class AdministrationServerStats
             ->select('count(U)');
         $accounts = $req2->getQuery()->getSingleScalarResult();
 
+        $req3 = $this->doctrine->getRepository("TwakeCalendarBundle:CalendarEvent")
+            ->createQueryBuilder('U')
+            ->select('count(U)');
+        $eventnumber = $req3->getQuery()->getSingleScalarResult();
+
+        $req4 = $this->doctrine->getRepository("TwakeDriveBundle:DriveFile")
+            ->createQueryBuilder('U')
+            ->select('count(U)');
+        $filesnumber = $req4->getQuery()->getSingleScalarResult();
+
+        $req5 = $this->doctrine->getRepository("TwakeDiscussionBundle:Message")
+            ->createQueryBuilder('U')
+            ->select('count(U)');
+        $messagesnumber = $req5->getQuery()->getSingleScalarResult();
+
         $em = $this->doctrine;
 
         $serverStat = new ServerUsersStats();
         $serverStat->setDateSave(new \DateTime("now"));
         $serverStat->setConnected($connected);
         $serverStat->setAccounts($accounts);
+        $serverStat->setEvent($eventnumber);
+        $serverStat->setFiles($filesnumber);
+        $serverStat->setMessages($messagesnumber);
 
         $em->persist($serverStat);
         $em->flush();
@@ -150,6 +168,15 @@ class AdministrationServerStats
             }
             if (!isset($currentValues["connected"]) || $currentValues["connected"] < $el->getAsArray()["connected"]) {
                 $currentValues["connected"] = $el->getAsArray()["connected"];
+            }
+            if (!isset($currentValues["event"]) || $currentValues["event"] < $el->getAsArray()["event"]) {
+                $currentValues["event"] = $el->getAsArray()["event"];
+            }
+            if (!isset($currentValues["files"]) || $currentValues["files"] < $el->getAsArray()["files"]) {
+                $currentValues["files"] = $el->getAsArray()["files"];
+            }
+            if (!isset($currentValues["messages"]) || $currentValues["messages"] < $el->getAsArray()["messages"]) {
+                $currentValues["messages"] = $el->getAsArray()["messages"];
             }
             $currentValues["datesave"] = $el->getAsArray()["datesave"];
         }
