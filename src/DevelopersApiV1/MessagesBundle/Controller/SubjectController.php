6364bd60975522efdd3e1087d2a249878b326635
Valentin Débénath <valentin.debenath@gmail.com>
2018-06-06 17:11:23 +0200

    Implement : Implement dev api functions related to subjects

---
+++ src/DevelopersApiV1/MessagesBundle/Controller/SubjectController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ehlnofey
+ * Date: 06/06/18
+ * Time: 15:59
+ */
+
+namespace DevelopersApiV1\MessagesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class SubjectController extends Controller
+{
+    //TODO : routing, impl, test, doc
+    //createSubject
+
+    //TODO : routing, impl, test, doc
+    //editSubject
+
+    //TODO : routing, impl, test, doc
+    //getSubjectsFromStream
+}
