1c40507b7dda946a584dd6e75b81c6656e704a09
Valentin Débénath <valentin.debenath@gmail.com>
2018-06-08 09:14:07 +0200

    Fix : Error and success message to JSON format

--- src/DevelopersApiV1/CoreBundle/Services/ApiStatus.php
+++ src/DevelopersApiV1/CoreBundle/Services/ApiStatus.php
@@ -26,18 +26,41 @@ class ApiStatus {
         $this->map[2000] = "";
 
         //2999 < errors from Messages < 4000
+        $this->map[3000] = "You can't access to this stream";
+        $this->map[3001] = "You can't access to this message";
+        $this->map[3002] = "Failed to post";
+        $this->map[3003] = "Fail to get message";
+        $this->map[3004] = "Fail to get message children";
+        $this->map[3005] = "Fail to edit a message";
+        $this->map[3006] = "Fail to delete a message";
+        $this->map[3007] = "Fail to get stream content";
+        $this->map[3008] = "";
+        $this->map[3009] = "Fail to get stream list";
+        $this->map[3010] = "Fail to change message in subject";
+        $this->map[3011] = "Fail to move a message in a message";
+        $this->map[3012] = "";
+        $this->map[3013] = "Fail to close a subject";
+        $this->map[3014] = "Fail to move message in subject";
+        $this->map[3015] = "Fail to delete a stream";
+        $this->map[3016] = "Fail to create a stream";
+        $this->map[3017] = "Fail to edit a stream";
+        $this->map[3018] = "";
+        $this->map[3019] = "Fail to add a member";
+        $this->map[3020] = "Fail to remove a member";
+        $this->map[3021] = "Fail to edit a subject";
+        $this->map[3022] = "Fail to get all subjects from a stream";
 
         //3999 < errors from Calendar < 5000
     }
 
     public function getError($code){
         $message = $this->map[$code];
-        $response = Array("status : error", "code : ".$code,"message : ".$message);
+        $response = Array("status" => "error", "code" => $code,"message" => $message);
         return $response;
     }
 
     public function getSuccess(){
-        $response = Array("status : success");
+        $response = Array("status" => "success");
         return $response;
     }
 
