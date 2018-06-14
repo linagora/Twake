04b24d4181781c41e00ae2fe7662fda9dc903b65
Valentin Débénath <valentin.debenath@gmail.com>
2018-06-14 15:21:20 +0200

    Add PDFBuilderSystem class

---
+++ src/WebsiteApi/PaymentsBundle/Services/PDFBuilderSystem.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ehlnofey
+ * Date: 14/06/18
+ * Time: 14:59
+ */
+
+namespace WebsiteApi\PaymentsBundle\Services;
+
+
+use WebsiteApi\PaymentsBundle\Model\PDFBuilderInterface;
+
+class PDFBuilderSystem implements PDFBuilderInterface
+{
+    public function makeBillPDF($data)
+    {
+        // TODO: Implement makeBillPDF() method.
+    }
+
+    public function makeUsageStatPDF($data)
+    {
+        // TODO: Implement makeUsageStatPDF() method.
+    }
+}
