0a24515e870459e7e49221d5342a4810074d70e8
Valentin Débénath <valentin.debenath@gmail.com>
2018-06-15 16:33:30 +0200

    Add verification in subscription controller

--- src/WebsiteApi/PaymentsBundle/Controller/SubscriptionController.php
+++ src/WebsiteApi/PaymentsBundle/Controller/SubscriptionController.php
@@ -9,23 +9,52 @@
 namespace WebsiteApi\PaymentsBundle\Controller;
 
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use WebsiteApi\PaymentsBundle\Services\SubscriptionManagerSystem;
 
 class SubscriptionController extends Controller
 {
-    public function newSubscriptionAction(){
-        $group = 1;
-        $pricing_plan = 2;
-        $balance = 1000;
-        $start_date = new \DateTime();
-        $end_date = (new \DateTime())->add(new \DateInterval("P1M"));
-        $auto_renew = true;
-        $auto_withdrawal = true;
+    public function newSubscriptionAction(Request $request){
+        $group = $request->request->get("group");
+        $pricing_plan = $request->request->get("pricing_plan");
+        $balance = $request->request->get("balance");
+        $start_date = $request->request->get("startDate");
+        $end_date = $request->request->get("endDate");
+        $auto_renew = $request->request->get("autoRenew");
+        $auto_withdrawal = $request->request->get("autoWithdrawal");
         $cost = $balance;
+        $data["errors"] = Array();
 
-        return new JsonResponse($this->get("app.subscription_manager")->newSubscription($group, $pricing_plan, $balance, $start_date, $end_date, $auto_withdrawal, $auto_renew, $cost));
+        if(!is_numeric($group)){
+            $data["errors"][] = "group error";
+            return new JsonResponse($data);
+        }
+
+        if(!is_numeric($pricing_plan)){
+            $data["errors"][] = "pricing_plan error";
+            return new JsonResponse($data);
+        }
+
+        if(strtotime($start_date) > strtotime($end_date)){
+            $data["errors"][] = "date error";
+            return new JsonResponse($data);
+        }
+
+        if( ! ($auto_withdrawal===true || $auto_withdrawal===false)){
+            $data["errors"][] = "auto_withdrawal error";
+            return new JsonResponse($data);
+        }
+
+        if( ! ($auto_renew===true || $auto_renew===false)){
+            $data["errors"][] = "auto_renew error";
+            return new JsonResponse($data);
+        }
+
+        $data["data"] = $this->get("app.subscription_manager")->newSubscription($group, $pricing_plan, $balance, $start_date, $end_date, $auto_withdrawal, $auto_renew, $cost);
+
+        return new JsonResponse($data);
     }
 
     public function checkOverusingAction(){
