0c0cbcfeb20de68689f124e0dde3c4cb886e2e45
Valentin Débénath <valentin.debenath@gmail.com>
2018-06-27 14:42:29 +0200

    Add external token system

---
+++ src/WebsiteApi/DriveBundle/Repository/ExternalDriveRepository.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ehlnofey
+ * Date: 12/06/18
+ * Time: 10:59
+ */
+
+namespace WebsiteApi\DriveBundle\Repository;
+
+
+use WebsiteApi\DriveBundle\Entity\ExternalDrive;
+
+class ExternalDriveRepository extends \Doctrine\ORM\EntityRepository
+{
+}
---
+++ src/WebsiteApi/UsersBundle/Entity/Token.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace WebsiteApi\UsersBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Token
+ *
+ * @ORM\Table(name="external_token",options={"engine":"MyISAM"})
+ * @ORM\Entity(repositoryClass="WebsiteApi\UsersBundle\Repository\TokenRepository")
+ */
+class Token
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity="WebsiteApi\UsersBundle\Entity\User")
+	 */
+    private $user;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=350, nullable=true)
+     */
+    private $token;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=350)
+     */
+    private $externalServiceName;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getExternalServiceName()
+    {
+        return $this->externalServiceName;
+    }
+
+    /**
+     * @param string $externalServiceName
+     */
+    public function setExternalServiceName($externalServiceName)
+    {
+        $this->externalServiceName = $externalServiceName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken()
+    {
+        if($this->token==null)
+            return null;
+        return json_decode($this->token);
+    }
+
+    /**
+     * @param string $token
+     */
+    public function setToken($token)
+    {
+        if ($token == null)
+            $this->token = null;
+        else
+            $this->token = json_encode($token);
+    }
+
+    public function __construct($tokenString, $user, $externalService)
+    {
+        $this->setToken($tokenString);
+        $this->setUser($user);
+        $this->setExternalServiceName($externalService);
+    }
+}
+
