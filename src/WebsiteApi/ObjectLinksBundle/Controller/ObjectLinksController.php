f5239f7072240611a1be5cc6850cbec86e9f77bb
Laura Trivino <laura.trivino@wanadoo.fr>
2018-07-16 15:29:49 +0200

    Add service route and controller

--- src/WebsiteApi/ObjectLinksBundle/Controller/ObjectLinksController.php
+++ src/WebsiteApi/ObjectLinksBundle/Controller/ObjectLinksController.php
@@ -9,4 +9,21 @@ use Symfony\Component\HttpFoundation\Request;
 class ObjectLinksController extends Controller
 {
 
+ public function createLink(Request $request){
+     $data = Array(
+         'errors' => Array(),
+         'data' => Array()
+     );
+
+     $typeA = $request->request->get("typeA");
+     $typeB = $request->request->get("typeB");
+     $idA = $request->request->get("idA");
+     $idB = $request->request->get("idB");
+
+     $data['data'] = $this->get("app.objectLinks")->createLink($typeA, $typeB, $idA,$idB);
+
+     return new JsonResponse($data);
+
+ }
+
 }
