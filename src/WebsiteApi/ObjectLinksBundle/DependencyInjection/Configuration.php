0de5162239ade86db143a746e4f9fad6c123131b
Valentin Débénath <valentin.debenath@gmail.com>
2018-07-16 10:55:42 +0200

    Fix ObjectLinks bundle

--- src/WebsiteApi/ObjectLinksBundle/DependencyInjection/Configuration.php
+++ src/WebsiteApi/ObjectLinksBundle/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace WebsiteApi\CallsBundle\DependencyInjection;
+namespace WebsiteApi\ObjectLinksBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
