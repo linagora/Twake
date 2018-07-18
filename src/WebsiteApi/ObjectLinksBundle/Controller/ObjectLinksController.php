2d529f3f05c8ea5b736752f2d3a60ad31b945e8d
Laura Trivino <laura.trivino@wanadoo.fr>
2018-07-18 15:15:27 +0200

    Delete objectLink

--- src/WebsiteApi/ObjectLinksBundle/Controller/ObjectLinksController.php
+++ src/WebsiteApi/ObjectLinksBundle/Controller/ObjectLinksController.php
@@ -62,4 +62,35 @@ class ObjectLinksController extends Controller
 
     }
 
+    public function deleteLinksAction(Request $request){
+        $data = Array(
+          'errors' => Array(),
+          'data' => Array()
+        );
+
+
+        $tab = ["file","event","task"];
+
+        $typeA = $request->request->get("typeA");
+        $typeB = $request->request->get("typeB");
+        $idA = $request->request->get("idA");
+        $idB = $request->request->get("idB");
+
+        if(in_array($typeA,$tab) && in_array($typeB,$tab)) {
+
+            $message = $this->get("app.objectLinks")->deleteObjectLink($typeA, $typeB, $idA, $idB);
+
+            if ($message == "success") {
+
+                $data['data'] = $message;
+            } else {
+                $data['errors'] = $message;
+            }
+        }else{
+            $data['errors'] = 'wrong data sent';
+        }
+        return new JsonResponse($data);
+
+    }
+
 }
