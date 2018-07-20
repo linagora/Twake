fd67742efa01b8366def17924ce0147aa0d500a3
Romaric Mourgues <romaric.mourgues@twakeapp.com>
2018-07-20 15:11:19 +0200

    Reformat icons for office and wekan

--- src/WebsiteApi/CoreBundle/Command/InitCommand.php
+++ src/WebsiteApi/CoreBundle/Command/InitCommand.php
@@ -326,8 +326,9 @@ class InitCommand extends ContainerAwareCommand
             ["/medias/apps/web/trello.png", "Trello", "trello.com", "trello", "0079bf", Array()],
             ["/medias/apps/web/gitlab.png", "Gitlab", "gitlab.com", "gitlab", "fc9626", Array()],
             ["/medias/apps/web/github.png", "Github", "github.com", "github", "24292e", Array()],
+            ["/medias/apps/web/overleaf.png", "Overleaf", "overleaf.com", "overleaf", "61994F", Array()],
 
-            //Altassian
+            //Altassian_remember_me
             ["/medias/apps/web/atlassian.png", "Atlassian", "atlassian.net/projects", "atlassian", "0047B3", Array()],
 
             //gSuite
