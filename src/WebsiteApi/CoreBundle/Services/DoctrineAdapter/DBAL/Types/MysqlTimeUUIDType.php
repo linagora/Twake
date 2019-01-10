0de7781f510d17a569ea1504ee6e0bc92a77e568
Romaric Mourgues <romaric.mourgues@twakeapp.com>
2019-01-10 13:55:28 +0100

    Use string identifiers and not binary identifiers

--- src/WebsiteApi/CoreBundle/Services/DoctrineAdapter/DBAL/Types/MysqlTimeUUIDType.php
+++ src/WebsiteApi/CoreBundle/Services/DoctrineAdapter/DBAL/Types/MysqlTimeUUIDType.php
@@ -20,9 +20,63 @@
 namespace WebsiteApi\CoreBundle\Services\DoctrineAdapter\DBAL\Types;
 
 use Ramsey\Uuid\Doctrine\UuidBinaryOrderedTimeType;
+use InvalidArgumentException;
+use Ramsey\Uuid\Codec\OrderedTimeCodec;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Ramsey\Uuid\UuidFactory;
+use Ramsey\Uuid\UuidInterface;
 
 class MysqlTimeUUIDType extends UuidBinaryOrderedTimeType
 {
-    const NAME = 'twake_timeuuid';
+
+    const NAME = 'uuid_binary_ordered_time';
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return $platform->getVarcharTypeDeclarationSQL(
+            array(
+                'length' => '42'
+            )
+        );
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        if ($value instanceof UuidInterface) {
+            return $value;
+        }
+
+        try {
+            return parent::convertToPHPValue(hex2bin(str_replace("-", "", $value)), $platform);
+        } catch (InvalidArgumentException $e) {
+            throw ConversionException::conversionFailed($value, self::NAME);
+        }
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        if ($value instanceof UuidInterface) {
+            return bin2hex(parent::convertToDatabaseValue($value, $platform));
+        }
+
+        try {
+            if (is_string($value) || method_exists($value, '__toString')) {
+                return bin2hex(parent::convertToDatabaseValue($value, $platform));
+            }
+        } catch (InvalidArgumentException $e) {
+            // Ignore the exception and pass through.
+        }
+
+        throw ConversionException::conversionFailed($value, self::NAME);
+    }
 
 }
