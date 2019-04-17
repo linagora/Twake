1b66fb643635fe4356c8c027e6129f149978fb8c
Romaric Mourgues <romaric.mourgues@twakeapp.com>
2019-04-17 15:41:31 +0200

    Implement monitoring

---
+++ src/WebsiteApi/CoreBundle/Services/Monitoring/CollectorRegistry.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace WebsiteApi\CoreBundle\Services\Monitoring;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use TweedeGolf\PrometheusClient\CollectorRegistry as PrometheusCollectorRegistry;
+
+/**
+ * Class CollectorRegistry
+ *
+ * The registry of data collectors in the API
+ *
+ * @package Suez\Bundle\PrometheusMonitoringBundle\Monitoring
+ */
+class CollectorRegistry
+{
+    /**
+     * @var AbstractCollector[]
+     */
+    protected $collectors = [];
+
+    /**
+     * Registry of collector to translate and save collected metrics into Prometheus format
+     *
+     * @var PrometheusCollectorRegistry
+     */
+    protected $registry;
+
+    /**
+     * The current name to label metrics
+     *
+     * @var string
+     */
+    protected $routeName;
+
+    /**
+     * The code of the App to label metrics
+     * @var string
+     */
+    protected $appCode;
+
+    /**
+     * CollectorRegistry constructor.
+     *
+     * @param string $appCode
+     * @param PrometheusCollectorRegistry $registry
+     */
+    public function __construct(string $appCode, PrometheusCollectorRegistry $registry)
+    {
+        $this->appCode = $appCode;
+        $this->registry = $registry;
+    }
+
+    public function init()
+    {
+        $this->addCollector(new MemoryCollector());
+        $this->addCollector(new ResponseCodeCollector());
+        $this->addCollector(new ResponseSizeCollector());
+        $this->addCollector(new ResponseTimeCollector());
+    }
+
+    /**
+     * Add a collector to the registry
+     *
+     * @param AbstractCollector $collector
+     */
+    public function addCollector(AbstractCollector $collector)
+    {
+        $this->collectors[] = $collector;
+    }
+
+    /**
+     * Trigger data collection on all collectors
+     *
+     * @param Request $request
+     * @param Response $response
+     */
+    public function collect(Request $request, Response $response)
+    {
+        foreach ($this->collectors as $collector) {
+            $collector->collect($request, $response);
+        }
+    }
+
+    /**
+     * Set the current route name
+     *
+     * @param string $routeName
+     */
+    public function setCurrentRoute($routeName)
+    {
+        $this->routeName = $routeName;
+    }
+
+    /**
+     * Save the collected metrics to the backend in Prometheus format
+     *
+     * @throws \TweedeGolf\PrometheusClient\PrometheusException
+     */
+    public function save()
+    {
+        foreach ($this->collectors as $collector) {
+            $collector->save(
+                $this->registry->get($collector->getCollectorName()),
+                [$this->appCode, $this->routeName]
+            );
+        }
+
+        $this->registry->getCounter('app_collectors_call_count')->inc(
+            1,
+            [$this->appCode, $this->routeName]
+        );
+    }
+}
