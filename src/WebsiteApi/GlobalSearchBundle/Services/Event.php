4f03faa075664ef44cab3a902c6d9096e23f6ad9
Grassellini Thomas <thomas.grassellini@telecomnancy.eu>
2019-04-19 09:59:10 +0200

    Add new pdf to text

--- src/WebsiteApi/GlobalSearchBundle/Services/Event.php
+++ src/WebsiteApi/GlobalSearchBundle/Services/Event.php
@@ -13,26 +13,42 @@ class Event
 
     public function TestSearch()
     {
-        $stopWords = Array(" ");
-        //error_log(print_r(getcwd(),true));
-        $content=file_get_contents("testfile.txt");
+       // error_log(print_r(getcwd(),true));
+        //error_log(print_r($pages,true));
+
+
+        //error_log(print_r($text,true));jh
+        //$text = preg_replace('~\b[a-z]{1,3}\b\s*~', '', $text);
+        //$text = preg_replace('/[0-9]+/', '', $text);
+        $content = (new \Spatie\PdfToText\Pdf())
+            ->setPdf('civ.pdf')
+            ->text();
+        $content = iconv(mb_detect_encoding($content, mb_detect_order(), true), "ISO-8859-1//IGNORE", $content);
+        $content = iconv("ISO-8859-1","UTF-8",$content);
+        $content = preg_replace('~\b[a-z]{1,3}\b\s*~', '', $content);
+        //error_log(print_r($content,true));
+        $size = substr_count($content, ' ');
+        error_log(print_r($size,true));
+        //error_log(print_r(sizeof($words),true));
 
         //$words = array_count_values(str_word_count(strtolower($content), 1));
         $content = str_replace(array("\\'", "'")," ",$content);
         $words = str_word_count(strtolower($content),1, 'ÀÂÄÇÉÈÊËÎÏÔÖÙÛÜŸÆŒàâäçéèêëîïôöùûüÿæœ');
+        //error_log(print_r(sizeof($words),true));
+        //error_log(print_r($words,true));
         $totalwords=1;
 
         $keywords=Array();
 
         foreach ($words as $value){
             if (strlen($value) > 3 && is_numeric($value)==false) {
-                if ($totalwords < 101) //we define the weight of word trough the text
-                    $weight = 10;
-                elseif ($totalwords < 301)
-                    $weight = 7;
+                if ($totalwords < floor($size*0.20)) //we define the weight of word trough the text
+                    $weight = 20;
+                elseif ($totalwords > floor($size*80))
+                    $weight = 20;
                 else
-                    $weight = 4;
-                if(!($keywords[$value])){ //if the word is not in our table
+                    $weight = 3;
+                if(!($keywords[$value]) || substr($value, -1) == "s"){ //if the word is not in our table
                     if (substr($value, -1) == "s") { //we check if it's a plural
                         $maybesinglar = substr($value, 0, strlen($value) - 1);
                         if ($keywords[$maybesinglar]) { // we check if their is already a singular for this word
@@ -40,27 +56,28 @@ class Event
                         }
                         else { // if not we add the new words or it's the first time we saw the word so we need to add it
                             //error_log(print_r($maybesinglar,true));
-                            $keywords[$value] = $weight +max(strlen($value)-4,0)*1;
+                            $keywords[$value] = $weight +max(strlen($value)-4,0)*1.5;
                         }
                     }
                     else {
-                        $keywords[$value] = $weight+max(strlen($value)-4,0)*1; // we add the new word which is not a plural or it the first time we saw it
+                        $keywords[$value] = $weight+max(strlen($value)-4,0)*1.5; // we add the new word which is not a plural or it the first time we saw it
                     }
                 }
                 else{ //if the word is in the table
-                    $keywords[$value] += $weight+max(strlen($value)-4,0)*1; // we adjust his weight in the table
+                    $keywords[$value] += $weight+max(strlen($value)-4,0)*1.5; // we adjust his weight in the table
                 }
-                $totalwords++; //we add our total of word to alter the weight of futur word.
             }
+            $totalwords++; //we add our total of word to alter the weight of futur word.
         }
 
         error_log(print_r($totalwords,true));
 
         //$keywords=array_count_values($keywords);
         arsort($keywords); // Sort based on frequency
+        error_log(print_r($keywords,true));
 
-        $fin = array_slice($keywords, 0, 10);
-        error_log(print_r($fin,true));
+        //$fin = array_slice($keywords, 0, 20);
+       // error_log(print_r($fin,true));
         echo("\n");
 
 
