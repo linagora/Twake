e3266aa9cf2f8d179fa1e73fb3bf416ff206ec86
harryless <manseur_aghiles@hotmail.fr>
2019-05-15 11:33:14 +0200

    Get calendarList

--- src/DevelopersApiV1/CalendarBundle/Controller/CalendarController.php
+++ src/DevelopersApiV1/CalendarBundle/Controller/CalendarController.php
@@ -78,4 +78,35 @@ class CalendarController extends Controller
         return new JsonResponse(Array("object" => $object));
 
     }
+
+    public function getCalendarList(Request $request){
+        $privileges = ["workspace_calendar"];
+
+        $application = $this->get("app.applications_api")->getAppFromRequest($request, [], $privileges);
+        if (is_array($application) && $application["error"]) {
+            return new JsonResponse($application);
+        }
+        $objects = false;
+        $user_id = $request->request->get("user_id", "");
+        $workspace_id = $request->request->get("workspace_id", "");
+        if ($workspace_id){
+            if ($user_id) {
+                $user_entity = $this->get("app.users")->getById($user_id, true);
+            }
+            if($user_entity) {
+                $objects = $this->get("app.calendar.calendar")->get(Array("workspace_id" => $workspace_id), $user_entity);
+            }
+        }
+
+        if ($objects === false) {
+            return new JsonResponse(Array("error" => "payload_error"));
+        }
+
+        $res = [];
+        foreach ($objects as $object) {
+            $res[] = $object;
+        }
+
+        return new JsonResponse(Array("data" => $res));
+    }
 }
