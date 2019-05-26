d4d2550be2c781f167b3323089ded917ff44fc88
Romaric Mourgues <romaric.mourgues@twakeapp.com>
2019-05-26 20:33:57 +0200

    Fix errors on export token

--- src/WebsiteApi/CalendarBundle/Controller/ExportController.php
+++ src/WebsiteApi/CalendarBundle/Controller/ExportController.php
@@ -9,8 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 class ExportController extends Controller
 {
     public function exportAction(Request $request){
-        //$token = $request->request->get("token");
-        $token = "23d17e55f50460fbc08b36360a2ffe6db4975dcaeb61c45852a18836ded1ae7e";
+        $token = $request->query->get("token");
         if ($token){
             return $this->get("app.calendar.export")->exportCalendar($token, $this->get("app.calendar.event"));
         }
@@ -20,9 +19,7 @@ class ExportController extends Controller
 
 
     public function generateTokenAction(Request $request){
-        error_log("generateTokenAction");
         $user = $this->getUser();
-        $this->get("app.calendar.export")->generateToken($request, $user);
-        return new JsonResponse("generateTokenAction");
+        return new JsonResponse(Array("token" => $this->get("app.calendar.export")->generateToken($request, $user)));
     }
 }
