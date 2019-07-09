739d090966b768396e2c0a296a745a0fba399544
mahaut <mahaut.dherent@telecomnancy.eu>
2019-07-09 14:21:23 +0200

    Add docker record

---
+++ Docker/twake-php/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ -z "$TIMEZONE"  ]; then
+    TIMEZONE=Etc/UTC
+fi
+
+echo $TIMEZONE > /etc/timezone && \
+dpkg-reconfigure -f noninteractive tzdata
+
+sed -i "s|;date.timezone =.*|date.timezone = $TIMEZONE|" /etc/php5/*/php.ini
+
+if [ -n "$PHP_MEMORY_LIMIT"  ]; then
+    sed -i "s/^memory_limit =.*/memory_limit = $PHP_MEMORY_LIMIT/" /etc/php5/*/php.ini
+fi
+
+exec "$@"
+
+chmod -R 777 /tmp/
+cron -f &
+docker-php-entrypoint php-fpm
