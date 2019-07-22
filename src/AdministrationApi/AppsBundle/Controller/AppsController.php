d4f67b7e7e47b460766aed256f6b1ebd38ca6f54
Justin Noel <justin.noel@telecomnancy.net>
2019-07-22 17:30:32 +0200

    Add Apps bundle for administration API, fix getApps for other services where we could only get the app ID

--- src/AdministrationApi/AppsBundle/Controller/AppsController.php
+++ src/AdministrationApi/AppsBundle/Controller/AppsController.php
@@ -21,7 +21,37 @@ class AppsController extends Controller {
         $validate_token = $validation->validateAuthentication($token);
 
         if ($validate_token) {
-            //TODO Implement service
+            //TODO Implement listing service
+        } else {
+            $data["errors"][] = "invalid_authentication_token";
+        }
+
+        return new JsonResponse($data);
+    }
+
+    public function getOneAppAction(Request $request) {
+
+        $data = Array(
+            "data" => Array(),
+            "errors" => Array()
+        );
+
+        $validation = $this->get("administration.validation");
+        $token = $request->request->get("token");
+        $validate_token = $validation->validateAuthentication($token);
+
+        if ($validate_token) {
+            $app_id = $request->request->get("id");
+
+            $app_service = $this->get("administration.apps");
+
+            $app = $app_service->getOneApp($app_id);
+
+            if ($app) {
+                $data["data"] = $app->getAsArray();
+            } else {
+                $data["errors"][] = "app_not_found";
+            }
         } else {
             $data["errors"][] = "invalid_authentication_token";
         }
