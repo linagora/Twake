37be0f6af2096d522692adfc39ed064ab9c8dbd5
Justin Noel <justin.noel@telecomnancy.net>
2019-08-28 17:56:45 +0200

    Add controller function for increment counter

--- src/AdministrationApi/CounterBundle/Controller/CounterController.php
+++ src/AdministrationApi/CounterBundle/Controller/CounterController.php
@@ -10,4 +10,26 @@ use Symfony\Component\HttpFoundation\Request;
 class CounterController extends Controller
 {
 
+    public function incrementValueAction(Request $request) {
+
+        $data = array(
+            "data" => array(),
+            "errors" => array()
+        );
+
+        $counter_id = $request->request->get('key');
+
+        $counter_value = $request->request->get('value');
+
+        $counter_service = $this->get("administration.counter");
+
+        if (isset($counter)) {
+            $counter_service->incrementValue($counter_id, $counter_value);
+        } else {
+            $counter_service->incrementValue($counter_id);
+        }
+
+        return new JsonResponse($data);
+    }
+
 }
--- src/AdministrationApi/CounterBundle/Services/CounterService.php
+++ src/AdministrationApi/CounterBundle/Services/CounterService.php
@@ -18,17 +18,17 @@ class CounterService
     public function incrementValue($key, $increment = 1) {
         $counter_repository = $this->em->getRepository("AdministrationApiCounterBundle:Counter");
 
-        $counter_tab = $counter_repository->findBy(Array('counter_key'=>$key), );
+        $counter_tab = $counter_repository->findBy(Array('counter_key'=>$key), array('date' => 'DESC'));
 
-        if (count($counter_tab) == 0 || $counter_tab[0]->getDate() != date("d-m-Y")) {
-            $counter = new Counter($key, date("d-m-Y"));
+        if (count($counter_tab) == 0 || $counter_tab[0]->getDate() != date("Y-m-d")) {
+            $counter = new Counter($key, date("Y-m-d"));
+
+            $this->em->persist($counter);
         } else {
             $counter = $counter_tab[0];
         }
 
         $counter->setValue($counter->getValue() + $increment);
-
-
 
         $this->em->flush();
     }
