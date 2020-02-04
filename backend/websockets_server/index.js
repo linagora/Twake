e2057aa6038d379a9811113efad6eaf0ad1106b7
Romaric Mourgues <romaric.mourgues@twakeapp.com>
2020-02-04 20:04:21 +0100

    Reorganise code

---
+++ backend/websockets_server/index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const socketClusterServer = require("socketcluster-server");
+
+let httpServer = http.createServer();
+let agServer = socketClusterServer.attach(httpServer);
+let port = 8080;
+
+(async () => {
+    console.log("Listening on port " + port);
+
+    for await (let {socket} of agServer.listener("connection")) {
+        (async () => {
+            for await (let req of socket.procedure("ping/ping")) {
+                req.end("OK");
+            }
+        })();
+    }
+})();
+
+httpServer.listen(port);
