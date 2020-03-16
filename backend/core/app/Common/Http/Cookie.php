d144f24d9f5f5ee7617fffc83d5a8062f46a20c1
- <->
2020-03-16 10:25:32 +0100

    Fix cookies definition for Chrome last update

--- backend/core/app/Common/Http/Cookie.php
+++ backend/core/app/Common/Http/Cookie.php
@@ -32,12 +32,12 @@ class Cookie
         $str .= '=';
 
         if ('' === (string)$this->value) {
-            $str .= 'deleted; expires=' . gmdate('D, d-M-Y H:i:s T', time() - 31536001) . '; Max-Age=0';
+            $str .= 'deleted; expires=' . gmdate('D, d-M-Y H:i:s T', time() - 31536001) . '; Max-Age=0; SameSite=None; Secure';
         } else {
             $str .= rawurlencode($this->value);
 
             if (0 !== $this->expire) {
-                $str .= '; expires=' . gmdate('D, d-M-Y H:i:s T', $this->expire) . '; Max-Age=' . $this->getMaxAge();
+                $str .= '; expires=' . gmdate('D, d-M-Y H:i:s T', $this->expire) . '; Max-Age=' . $this->getMaxAge() . "; SameSite=None; Secure";
             }
         }
 
