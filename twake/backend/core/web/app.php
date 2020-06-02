d2ccad3fe8abf5ef3f09c4efa79835e75d6b9db7
- <->
2020-06-02 17:37:35 +0200

    Fix header access All-Cookies

--- twake/backend/core/web/app.php
+++ twake/backend/core/web/app.php
@@ -6,7 +6,7 @@ function getDefaultHeaders()
     if (isset($_SERVER['HTTP_ORIGIN']) && strpos("http://localhost", $_SERVER['HTTP_ORIGIN']) == 0) {
         @header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN'], true);
     }
-    @header('Access-Control-Allow-Headers: ' . 'Content-Type, *', true);
+    @header('Access-Control-Allow-Headers: ' . 'All-Cookies, Content-Type, *', true);
     @header('Access-Control-Allow-Credentials: true', true);
     @header('Access-Control-Allow-Methods: GET, POST', true);
     @header('Access-Control-Max-Age: 600', true);
