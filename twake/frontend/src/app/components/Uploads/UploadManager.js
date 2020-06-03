a9e815ade366baa9ed9b71e6fe77343e491d26ad
- <->
2020-06-03 21:52:56 +0200

    - Fix dev server - Fix environment.js.dist - Fix header in Upload manager

--- twake/frontend/src/app/components/Uploads/UploadManager.js
+++ twake/frontend/src/app/components/Uploads/UploadManager.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import Observable from 'services/observable.js';
 import Number from 'services/utils/Numbers.js';
@@ -395,117 +395,122 @@ class UploadManager extends Observable {
       extension: extension[extension.length - 1],
     };
 
-    //Use Resumable.js for upload by chunk
-    var r = new Resumable({
-      target: Globals.window.api_root_url + '/ajax/' + 'driveupload/upload',
-      chunkSize: 50000000,
-      testChunks: false,
-      simultaneousUploads: 5,
-      withCredentials: true,
-      maxChunkRetries: 2,
-      query: {
-        object: JSON.stringify({
-          id: upload_options.new_version ? upload_options.file_id : null,
-          _once_new_version: upload_options.new_version,
-          front_id: Number.unid(),
-          is_directory: false,
-          name: element.name,
-          parent_id: drive_parent ? drive_parent.id : null,
-          workspace_id: upload_options.workspace_id,
-          detached: upload_options.detached,
-        }),
-      },
-      generateUniqueIdentifier: (file, event) => {
-        return 'no_id';
-      },
-    });
+    Globals.getAllCookies(cookies => {
+      //Use Resumable.js for upload by chunk
+      var r = new Resumable({
+        target: Globals.window.api_root_url + '/ajax/' + 'driveupload/upload',
+        chunkSize: 50000000,
+        testChunks: false,
+        simultaneousUploads: 5,
+        withCredentials: true,
+        maxChunkRetries: 2,
+        headers: {
+          'All-Cookies': JSON.stringify(cookies),
+        },
+        query: {
+          object: JSON.stringify({
+            id: upload_options.new_version ? upload_options.file_id : null,
+            _once_new_version: upload_options.new_version,
+            front_id: Number.unid(),
+            is_directory: false,
+            name: element.name,
+            parent_id: drive_parent ? drive_parent.id : null,
+            workspace_id: upload_options.workspace_id,
+            detached: upload_options.detached,
+          }),
+        },
+        generateUniqueIdentifier: (file, event) => {
+          return 'no_id';
+        },
+      });
 
-    current_item.resumable = r;
+      current_item.resumable = r;
 
-    current_item.timeout = setTimeout(() => {
-      this.currentUploadingFilesNumber--;
-    }, 60000);
-
-    r.on('fileSuccess', function(file, message) {
-      clearTimeout(current_item.timeout);
-
-      that.currentUploadedFilesNumber++;
-      that.currentUploadingFilesNumber--;
-      current_item.progress = 1;
-
-      if (
-        that.currentUploadedFilesNumber +
-          that.currentCancelledFilesNumber +
-          that.currentErrorFilesNumber >=
-        that.currentUploadTotalNumber
-      ) {
-        that.reinitAfterDelay();
-      }
-
-      var resp = JSON.parse(message);
-
-      if (resp.data && resp.data.object) {
-        Collections.get('drive').updateObject(resp.data.object);
-        Collections.get('drive').share(resp.data.object);
-
-        if (callback) callback(Collections.get('drive').find(resp.data.object.id));
-
-        var parent = Collections.get('drive').find(resp.data.object.parent_id);
-        if (parent) {
-          parent.size = parseInt(parent.size) + parseInt(resp.data.object.size || 0);
-          Collections.get('drive').updateObject(parent);
-        }
-      } else {
-        current_item.error = 1;
-      }
-
-      that.notify();
-    });
-    r.on('progress', function(file, message) {
-      current_item.progress = Math.min(r.progress(), 0.99);
-      that.notify();
-
-      clearTimeout(current_item.timeout);
       current_item.timeout = setTimeout(() => {
         this.currentUploadingFilesNumber--;
       }, 60000);
+
+      r.on('fileSuccess', function(file, message) {
+        clearTimeout(current_item.timeout);
+
+        that.currentUploadedFilesNumber++;
+        that.currentUploadingFilesNumber--;
+        current_item.progress = 1;
+
+        if (
+          that.currentUploadedFilesNumber +
+            that.currentCancelledFilesNumber +
+            that.currentErrorFilesNumber >=
+          that.currentUploadTotalNumber
+        ) {
+          that.reinitAfterDelay();
+        }
+
+        var resp = JSON.parse(message);
+
+        if (resp.data && resp.data.object) {
+          Collections.get('drive').updateObject(resp.data.object);
+          Collections.get('drive').share(resp.data.object);
+
+          if (callback) callback(Collections.get('drive').find(resp.data.object.id));
+
+          var parent = Collections.get('drive').find(resp.data.object.parent_id);
+          if (parent) {
+            parent.size = parseInt(parent.size) + parseInt(resp.data.object.size || 0);
+            Collections.get('drive').updateObject(parent);
+          }
+        } else {
+          current_item.error = 1;
+        }
+
+        that.notify();
+      });
+      r.on('progress', function(file, message) {
+        current_item.progress = Math.min(r.progress(), 0.99);
+        that.notify();
+
+        clearTimeout(current_item.timeout);
+        current_item.timeout = setTimeout(() => {
+          this.currentUploadingFilesNumber--;
+        }, 60000);
+      });
+      r.on('fileError', function(file, message) {
+        clearTimeout(current_item.timeout);
+
+        that.currentErrorFilesNumber++;
+        that.currentUploadingFilesNumber--;
+        current_item.error = true;
+
+        if (
+          that.currentUploadedFilesNumber +
+            that.currentCancelledFilesNumber +
+            that.currentErrorFilesNumber >=
+          that.currentUploadTotalNumber
+        ) {
+          that.reinitAfterDelay();
+        }
+
+        that.notify();
+      });
+
+      r.addFile(element);
+
+      Api.post(
+        'driveupload/preprocess',
+        {
+          workspace_id: r_data.workspace_id,
+          name: r_data.name,
+          identifier: r_data.identifier,
+          extension: r_data.extension,
+        },
+        res => {
+          var identifier = res.identifier;
+          var file = r.getFromUniqueIdentifier('no_id');
+          file.uniqueIdentifier = identifier;
+          r.upload();
+        },
+      );
     });
-    r.on('fileError', function(file, message) {
-      clearTimeout(current_item.timeout);
-
-      that.currentErrorFilesNumber++;
-      that.currentUploadingFilesNumber--;
-      current_item.error = true;
-
-      if (
-        that.currentUploadedFilesNumber +
-          that.currentCancelledFilesNumber +
-          that.currentErrorFilesNumber >=
-        that.currentUploadTotalNumber
-      ) {
-        that.reinitAfterDelay();
-      }
-
-      that.notify();
-    });
-
-    r.addFile(element);
-
-    Api.post(
-      'driveupload/preprocess',
-      {
-        workspace_id: r_data.workspace_id,
-        name: r_data.name,
-        identifier: r_data.identifier,
-        extension: r_data.extension,
-      },
-      res => {
-        var identifier = res.identifier;
-        var file = r.getFromUniqueIdentifier('no_id');
-        file.uniqueIdentifier = identifier;
-        r.upload();
-      },
-    );
   }
 }
 
