ca9c1e33b8c7dd4ebacb995488a30c6d77f0fc1b
- <->
2020-06-23 18:29:06 +0200

    Fix errors on Drive and Channels members manager

--- twake/backend/core/src/Twake/Drive/Services/OldFileSystem/DriveAdapterSelector.php
+++ twake/backend/core/src/Twake/Drive/Services/OldFileSystem/DriveAdapterSelector.php
@@ -9,6 +9,7 @@ class DriveAdapterSelector
 
     public function __construct(App $app)
     {
+        $this->app = $app;
         $this->aws = $app->getContainer()->getParameter("aws");
         $this->openstack = $app->getContainer()->getParameter("openstack");
     }
@@ -17,11 +18,11 @@ class DriveAdapterSelector
     {
 
         if (isset($this->aws["S3"]["use"]) && $this->aws["S3"]["use"]) {
-            $this->aws_file_system = $app->getServices()->get("app.drive.old.AWS_FileSystem");
+            $this->aws_file_system = $this->app->getServices()->get("app.drive.old.AWS_FileSystem");
             return $this->aws_file_system;
         }
         if (isset($this->openstack["use"]) && $this->openstack["use"]) {
-            $this->openstack_file_system = $app->getServices()->get("app.drive.old.OpenStack_FileSystem");
+            $this->openstack_file_system = $this->app->getServices()->get("app.drive.old.OpenStack_FileSystem");
             return $this->openstack_file_system;
         }
 
