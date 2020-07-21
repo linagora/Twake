fd995bff700ea1695e77812e693c7b80f3391932
Romaric Mourgues <rmourgues@linagora.com>
2020-07-21 15:46:41 +0200

    🛠 Fix RabbitMQ consumer #50 (#51)
    Fix #50

--- twake/install.sh
+++
@@ -1,9 +0,0 @@
-echo "⬆️ Increase vm.max_map_count..."
-
-sudo sysctl -w vm.max_map_count=262144
-
-echo "⏳ Install/Update docker..."
-
-docker-compose pull
-
-echo "✅ Twake is ready, run start.sh to start Twake."
