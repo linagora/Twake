a589efd758bf10ed609fb14bb79f426f7a28fd8b
Christophe HAMERLING <chamerling@linagora.com>
2020-10-01 12:18:08 +0200

    🚀 Add configuration

--- twake/backend/node/src/webserver/index.ts
+++ twake/backend/node/src/webserver/index.ts
@@ -2,6 +2,7 @@ import { Server, IncomingMessage, ServerResponse } from "http";
 import * as fastify from "fastify";
 import { serverErrorHandler } from "./error";
 import { registerRoutes } from "./api";
+import config from "../core/config";
 
 const server: fastify.FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify.fastify({
   logger: true
@@ -12,7 +13,7 @@ registerRoutes(server);
 
 const start = async (): Promise<void> => {
   try {
-    await server.listen(4000, "0.0.0.0");
+    await server.listen(config.get("web.port"), "0.0.0.0");
   } catch (err) {
     server.log.error(err);
     process.exit(1);
