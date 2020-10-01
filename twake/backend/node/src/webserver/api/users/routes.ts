bd69ed2c549ab713d819029281e9bc6db4b97d37
Christophe HAMERLING <chamerling@linagora.com>
2020-10-01 16:36:09 +0200

    🔐 Add JWT Auth

--- twake/backend/node/src/webserver/api/users/routes.ts
+++ twake/backend/node/src/webserver/api/users/routes.ts
@@ -4,20 +4,32 @@ import * as controller from "./controller";
 import User from "../../../core/types/user";
 
 const routes: FastifyPluginCallback = (fastify: FastifyInstance, _opts, next) => {
-  fastify.get("/", async (req): Promise<User[]> => {
-    req.log.info("Get users");
+  fastify.get(
+    "/",
+    {
+      preValidation: [fastify.authenticate]
+    },
+    async (req): Promise<User[]> => {
+      req.log.info("Get users");
 
-    return controller.getUsers();
+      return controller.getUsers();
   });
 
   fastify.get<{
     Params: UserParams,
-  }>("/:id", async (req): Promise<User> => {
-    req.log.info(`Get user ${req.params.id}`);
-    return controller.getUser(req.params.id);
-  });
+  }>(
+    "/:id",
+    {
+      preValidation: [fastify.authenticate]
+    },
+    async (req): Promise<User> => {
+      req.log.debug(`Current user ${JSON.stringify(req.user)}`);
+      req.log.info(`Get user ${req.params.id}`);
+      return controller.getUser(req.params.id);
+    });
 
   const routeOptions: RouteShorthandOptions = {
+    preValidation: [fastify.authenticate],
     schema: {
       body: {
         type: "object",
