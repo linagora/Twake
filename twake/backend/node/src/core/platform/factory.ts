835dcbf35798c3f351552c05f43c628e74fe21cd
Christophe HAMERLING <chamerling@linagora.com>
2020-10-05 16:50:14 +0200

    Introduce the Twake platform

---
+++ twake/backend/node/src/core/platform/factory.ts
@@ -0,0 +1,18 @@
+import { TwakeService, TwakeServiceOptions, TwakeServiceProvider, TwakeServiceState, TwakeServiceConfiguration, TwakeContext } from "./service";
+
+class StaticTwakeServiceFactory {
+
+  public async create<T extends TwakeService<TwakeServiceProvider> = TwakeService<TwakeServiceProvider>>(
+    module: { new (options?: TwakeServiceOptions<TwakeServiceConfiguration>): T },
+    context: TwakeContext,
+    options?: TwakeServiceOptions<TwakeServiceConfiguration>
+  ): Promise<T> {
+    const instance = new module(options);
+    instance.state = TwakeServiceState.Ready;
+    instance.context = context;
+
+    return instance;
+  }
+}
+
+export const TwakeServiceFactory = new StaticTwakeServiceFactory();
---
+++ twake/backend/node/src/core/platform/platform.ts
@@ -0,0 +1,129 @@
+import { TwakeServiceFactory } from "./factory";
+import { TwakePlatform, TwakeService, TwakeServiceProvider } from "./service";
+import Configuration from "./configuration";
+import AuthService from "../../services/auth";
+import UserService from "../../services/user";
+import MessageService from "../../services/messages";
+import WebServerService from "../../services/webserver";
+import logger from "./logger";
+
+class Platform extends TwakePlatform {
+  private services: Map<string, TwakeService<TwakeServiceProvider>> = new Map<string, TwakeService<TwakeServiceProvider>>();
+
+  api(): TwakeServiceProvider {
+    return null;
+  }
+
+  async doInit(): Promise<this> {
+    logger.info("Init %s", this.name);
+    // TODO: Load services from options
+    // TODO: Dynamic loading
+    logger.info("Init services", this.options.services);
+
+    const context = {
+      getProvider: this.getProvider.bind(this)
+    };
+
+    const webserver = await TwakeServiceFactory.create(WebServerService, context, { prefix: "/api", configuration: new Configuration("web"), consumes: [] });
+    this.providers.set(webserver.name, webserver.api());
+
+    const auth = await TwakeServiceFactory.create(AuthService, context, { prefix: "/api/auth", consumes: ["webserver", "user"] });
+    const user = await TwakeServiceFactory.create(UserService, context, { prefix: "/api/users", consumes: ["webserver"] });
+    const message = await TwakeServiceFactory.create(MessageService, context, { prefix: "/api/messages", consumes: ["webserver"] });
+
+    this.services.set(webserver.name, webserver);
+    this.services.set(auth.name, auth);
+    this.services.set(user.name, user);
+    this.services.set(message.name, message);
+
+    await this.launchInit();
+
+    return this;
+  }
+
+  private async launchInit(): Promise<this> {
+    const promisesToWaitFor: Map<string, Promise<string>[]> = new Map<string, Promise<string>[]>();
+    const allInitialized: Promise<string>[] = [...this.services.values()].map(service => service.initPromise);
+
+    logger.info("Initializing Twake...");
+
+    for(const [name, service] of this.services) {
+      const consumes = service.getConsumes();
+      logger.info("service '%s' service is consuming service '%s'", name, consumes);
+
+      const promisesForService = consumes.map(serviceName => {
+        // TODO: Check for empty
+        // TODO: Check for loops
+        logger.info("service '%s' will wait for service '%s' to be initialized", name, serviceName);
+        return this.services.get(serviceName).initPromise;
+      });
+
+      promisesToWaitFor.set(name, promisesForService);
+    }
+
+    for(const [name, service] of this.services) {
+      Promise.all(promisesToWaitFor.get(name)).then(started => {
+        logger.info("services %o are now initialized, now asking for service '%s' to init", started, name);
+        service.init().then(() => {
+          logger.info("service '%s' is initialized", name);
+        });
+      })
+      .catch(err => {
+        logger.error("Init error for service '%s'", name);
+        logger.error(err);
+        // TODO throw err
+      });
+    }
+
+    logger.info("Waiting for all services to be initialized...");
+    await Promise.all(allInitialized);
+    logger.info("All services are now initialized");
+
+    return this;
+  }
+
+  async doStart(): Promise<this> {
+    await this.launchStart();
+
+    return this;
+  }
+
+  private async launchStart(): Promise<this> {
+    const promisesToWaitFor: Map<string, Promise<string>[]> = new Map<string, Promise<string>[]>();
+    const allStarted: Promise<string>[] = [...this.services.values()].map(service => service.startupPromise);
+
+    logger.info("Starting Twake...");
+
+    for(const [name, service] of this.services) {
+      const consumes = service.getConsumes();
+      logger.info("service '%s' is consuming service '%s'", name, consumes);
+
+      const promisesForService = consumes.map(serviceName => {
+        logger.info("service '%s' will wait for service '%s'", name, serviceName);
+        return this.services.get(serviceName).startupPromise;
+      });
+
+      promisesToWaitFor.set(name, promisesForService);
+    }
+
+
+    for(const [name, service] of this.services) {
+      Promise.all(promisesToWaitFor.get(name)).then(started => {
+        logger.info("services %o are started, now asking for service '%s' to start...", started, name);
+        service.start();
+      })
+      .catch(err => {
+        logger.error("Start error for service %s", name);
+        logger.error(err);
+      });
+    }
+
+    logger.info("Waiting for all services to be started...");
+    await Promise.all(allStarted);
+    logger.info("All services are now started");
+
+    return this;
+  }
+}
+
+export default Platform;
