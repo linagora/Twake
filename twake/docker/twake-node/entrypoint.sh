cefe04720b25d184cbd6f72816368965b45acef4
Romaric Mourgues <rmourgues@linagora.com>
2020-10-07 18:28:56 +0200

    🐳 Add docker and route forwarding for new backend

--- twake/docker/twake-node/entrypoint.sh
+++ twake/docker/twake-node/entrypoint.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
+echo $1
+
 if [ "$1" = "dev" ]
 then
-  if test -f "/twake-node/configuration/configuration.json"; then
-    echo "Configuration exists, doing nothing."
-  else
-    cp /twake-node/configuration/configuration.json.dist /twake-node/configuration/configuration.json
-  fi
-
-  npm run start:prod
+  npm run watch
 else
-  if test -f "/configuration/configuration.json"; then
-    cp /configuration/* /twake-node/configuration/
-  else
-    cp /twake-node/configuration/configuration.json.dist /twake-node/configuration/configuration.json
+  if test -f "/configuration/production.json"; then
+    cp /configuration/* /twake-node/config/
   fi
 
-  npm run start:dev
+  npm run serve
 fi
 
