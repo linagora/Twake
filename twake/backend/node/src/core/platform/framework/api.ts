43b2859d86414f4556306c25706ba3f3a478133f
Christophe HAMERLING <chamerling@linagora.com>
2020-10-10 17:05:51 +0200

    Use RxJS instead of promises to manage service states

--- twake/backend/node/src/core/platform/framework/api.ts
+++ twake/backend/node/src/core/platform/framework/api.ts
@@ -1,14 +1,23 @@
+import { combineLatest, BehaviorSubject } from "rxjs";
+import { filter } from "rxjs/operators";
 import { TwakeServiceConfiguration } from "./configuration";
 import { CONSUMES_METADATA, PREFIX_METADATA } from "./constants";
 import { logger } from "./logger";
 import { Registry } from "./registry";
 
 
-
 class TwakeServiceOptions<TwakeServiceConfiguration> {
   name?: string;
   // TODO: configuration is abstract and comes from all others
   configuration?: TwakeServiceConfiguration;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Class = { new(...args: any[]): any; };
+
+interface ServiceDefinition {
+  name: string;
+  clazz: Class;
 }
 
 class TwakeAppConfiguration extends TwakeServiceOptions<TwakeServiceConfiguration> {
@@ -18,7 +27,9 @@ class TwakeAppConfiguration extends TwakeServiceOptions<TwakeServiceConfiguratio
 
 enum TwakeServiceState {
   Ready = "READY",
+  Initializing = "INITIALIZING",
   Initialized = "INITIALIZED",
+  Starting = "STARTING",
   Started = "STARTED",
   Errored = "ERRORED"
 }
@@ -38,27 +49,14 @@ interface TwakeContext {
 }
 
 abstract class TwakeService<TwakeServiceProvider> implements TwakeServiceInterface<TwakeServiceProvider> {
-  readonly initPromise: Promise<string>;
-  isInitialized: (value: string | PromiseLike<string>) => void;
-  isInitFailure: (reason?: Error) => void;
-  readonly startupPromise: Promise<string>;
-  isStarted: (value: string | PromiseLike<string>) => void;
-  isStartFailure: (reason?: Error) => void;
+  state: BehaviorSubject<TwakeServiceState>;
   readonly name: string;
   protected readonly configuration: TwakeServiceConfiguration;
-  state: TwakeServiceState;
   context: TwakeContext;
 
   constructor(protected options?: TwakeServiceOptions<TwakeServiceConfiguration>) {
+    this.state = new BehaviorSubject<TwakeServiceState>(TwakeServiceState.Ready);
     this.configuration = options?.configuration;
-    this.startupPromise = new Promise((resolve, reject) => {
-      this.isStarted = resolve;
-      this.isStartFailure = reject;
-    });
-    this.initPromise = new Promise((resolve, reject) => {
-      this.isInitialized = resolve;
-      this.isInitFailure = reject;
-    });
   }
 
   abstract api(): TwakeServiceProvider;
@@ -72,18 +70,22 @@ abstract class TwakeService<TwakeServiceProvider> implements TwakeServiceInterfa
   }
 
   async init(): Promise<this> {
+    if (this.state.value !== TwakeServiceState.Ready) {
+      logger.info("Service %s is already initialized", this.name);
+      return this;
+    }
+
     try {
       logger.info("Initializing service %s", this.name);
+      this.state.next(TwakeServiceState.Initializing);
       await this.doInit();
-      this.state = TwakeServiceState.Initialized;
-      this.isInitialized(this.name);
+      this.state.next(TwakeServiceState.Initialized);
 
       return this;
     } catch (err) {
       logger.error("Error while initializing service %s", this.name);
       logger.error(err);
-      this.state = TwakeServiceState.Errored;
-      this.isInitFailure(err);
+      this.state.error(new Error(`Error while initializing service ${this.name}`));
 
       throw err;
     }
@@ -98,19 +100,23 @@ abstract class TwakeService<TwakeServiceProvider> implements TwakeServiceInterfa
   }
 
   async start(): Promise<this> {
+    if (this.state.value === TwakeServiceState.Starting || this.state.value === TwakeServiceState.Started) {
+      logger.info("Service %s is already started", this.name);
+      return this;
+    }
+
     try {
       logger.info("Starting service %s", this.name);
+      this.state.next(TwakeServiceState.Starting);
       await this.doStart();
-      this.state = TwakeServiceState.Started;
-      this.isStarted(this.name);
+      this.state.next(TwakeServiceState.Started);
       logger.info("Service %s is started", this.name);
 
       return this;
     } catch (err) {
       logger.error("Error while starting service %s", this.name, err);
       logger.error(err);
-      this.state = TwakeServiceState.Errored;
-      this.isStartFailure(err);
+      this.state.error(new Error(`Error while starting service ${this.name}`));
 
       throw err;
     }
@@ -137,7 +143,55 @@ abstract class TwakePlatform extends TwakeService<TwakeServiceProvider> implemen
   }
 }
 
+class TwakeComponent {
+  instance: TwakeService<TwakeServiceProvider>;
+  components: Array<TwakeComponent> = new Array<TwakeComponent>();
+
+  constructor(public name: string, private serviceDefinition: ServiceDefinition) {}
+
+  getServiceDefinition(): ServiceDefinition {
+    return this.serviceDefinition;
+  }
+
+  setServiceInstance(instance: TwakeService<TwakeServiceProvider>): void {
+    this.instance = instance;
+  }
+
+  getServiceInstance(): TwakeService<TwakeServiceProvider> {
+    return this.instance;
+  }
+
+  addDependency(component: TwakeComponent): void {
+    this.components.push(component);
+  }
+
+  getStateTree(): string {
+    return `${this.name}(${this.instance.state.value}) => {${this.components.map(component => component.getStateTree()).join(",")}}`;
+  }
+
+  switchToState(state: TwakeServiceState.Initialized | TwakeServiceState.Started): void {
+    const states: BehaviorSubject<TwakeServiceState>[] = this.components.map(component => component.instance.state);
+
+    this.components.forEach(component => {
+      state === TwakeServiceState.Initialized && component.instance.init();
+      state === TwakeServiceState.Started && component.instance.start();
+    });
+
+    const subscription = combineLatest(states).pipe(
+      filter((value: Array<TwakeServiceState>) => value.every(v => v === state)),
+    ).subscribe(() => {
+      logger.info(`Children of ${this.name} are all in ${state} state`);
+      logger.info(this.getStateTree());
+      state === TwakeServiceState.Initialized && this.instance.init();
+      state === TwakeServiceState.Started && this.instance.start();
+
+      subscription.unsubscribe();
+    });
+  }
+}
+
 export {
+  TwakeComponent,
   TwakePlatform,
   TwakeService,
   TwakeServiceInterface,
