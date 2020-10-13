4db686319a75d53e4a88fd26915ef2da351e1f33
Christophe HAMERLING <chamerling@linagora.com>
2020-10-13 09:55:24 +0200

    Make tests work again

--- twake/backend/node/babel.config.js
+++ twake/backend/node/babel.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "@babel/preset-typescript",
   ],
   plugins: [
-    ["@babel/plugin-proposal-decorators", { legacy: true }]
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }]
   ]
 };
