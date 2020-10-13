44f6817a888a1b85ece44dd98a5014d47740f3be
Christophe HAMERLING <chamerling@linagora.com>
2020-10-13 14:24:43 +0200

    #403 Split api into different modules

---
+++ twake/backend/node/src/core/platform/framework/api/service-definition.ts
@@ -0,0 +1,6 @@
+import { Class } from "./class";
+
+export interface ServiceDefinition {
+  name: string;
+  clazz: Class;
+}
---
+++ twake/backend/node/src/core/platform/framework/api/service-options.ts
@@ -0,0 +1,5 @@
+export class TwakeServiceOptions<TwakeServiceConfiguration> {
+  name?: string;
+  // TODO: configuration is abstract and comes from all others
+  configuration?: TwakeServiceConfiguration;
+}
