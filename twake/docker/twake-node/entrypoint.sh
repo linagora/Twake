976179d45e1cf694602a27606f760d5246de3997
Romaric Mourgues <rmourgues@linagora.com>
2020-10-21 11:14:04 +0200

    🐳 Update docker-compose for dev usage

--- twake/docker/twake-node/entrypoint.sh
+++ twake/docker/twake-node/entrypoint.sh
@@ -4,12 +4,14 @@ echo $1
 
 if [ "$1" = "dev" ]
 then
+  npm run build
   npm run watch
 else
   if test -f "/configuration/production.json"; then
     cp /configuration/* /twake-node/config/
   fi
 
+  npm run build
   npm run serve
 fi
 
