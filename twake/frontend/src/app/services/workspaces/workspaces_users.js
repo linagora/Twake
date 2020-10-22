a658871d6ea8d7822d38d521ed26e31c3905d769
Romaric Mourgues <rmourgues@linagora.com>
2020-10-22 18:50:57 +0200

    🩹 Closes #432

--- twake/frontend/src/app/services/workspaces/workspaces_users.js
+++ twake/frontend/src/app/services/workspaces/workspaces_users.js
@@ -498,7 +498,7 @@ class WorkspacesUsers extends Observable {
   leaveWorkspace() {
     var that = this;
     var has_other_admin = false;
-    if (!workspaceService.hasWorkspacePrivilege()) {
+    if (!WorkspaceUserRights.hasWorkspacePrivilege()) {
       has_other_admin = true;
     } else {
       var users = WorkspacesMembersTable.getList(workspaceService.currentWorkspaceId, 'members');
