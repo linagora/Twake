0284487f9870da9442365459b7218f34c456907e
Christophe HAMERLING <chamerling@linagora.com>
2020-10-23 16:08:41 +0200

    #449 Remove typeorm

--- twake/backend/node/src/services/orm/provider.ts
+++
@@ -1,8 +0,0 @@
-import { TwakeServiceProvider } from "../../core/platform/framework";
-import { ConnectionOptions, Connection, EntityManager } from "typeorm";
-
-export default interface ORMServiceAPI extends TwakeServiceProvider {
-  connect(options: ConnectionOptions): Promise<Connection>;
-
-  readonly manager: EntityManager;
-}
