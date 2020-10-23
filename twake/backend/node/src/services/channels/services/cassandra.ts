814b4a9f4f873dd5b3378b4f8b0726784d7ec09d
Christophe HAMERLING <chamerling@linagora.com>
2020-10-23 12:57:42 +0200

    #449 Implement mongo service for channels

---
+++ twake/backend/node/src/services/channels/services/cassandra.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Channel } from "../entities";
+import ChannelServiceAPI from "../provider";
+import { RealtimeDeleted, RealtimeCreated, RealtimeUpdated } from "../../../core/platform/framework/decorators";
+import { UpdateResult, CreateResult, DeleteResult } from "../../../core/platform/framework/api/crud-service";
+
+export class CassandraChannelService implements ChannelServiceAPI<Channel> {
+  version = "1";
+
+  @RealtimeCreated<Channel>("/channels", channel => `/channels/${channel.id}`)
+  create(channel: Channel): Promise<CreateResult<Channel>> {
+    throw new Error("Not implemented");
+  }
+
+  get(id: string): Promise<Channel> {
+    throw new Error("Not implemented");
+  }
+
+  @RealtimeUpdated<string>("/channels", id => `/channels/${id}`)
+  async update(id: string, entity: Channel): Promise<UpdateResult<Channel>> {
+    throw new Error("Not implemented");
+  }
+
+  @RealtimeDeleted<Channel>("/channels", channel => `/channels/${channel.id}`)
+  async delete(id: string): Promise<DeleteResult<Channel>> {
+    throw new Error("Not implemented");
+  }
+
+  async list(/* TODO: Options */): Promise<Channel[]> {
+    throw new Error("Not implemented");
+  }
+}
