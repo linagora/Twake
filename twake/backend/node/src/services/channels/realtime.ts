1bfefdcf65555b3b50ed6d62c398cb058ff729a5
Christophe HAMERLING <chamerling@linagora.com>
2020-10-28 17:15:58 +0100

    #470 Generate right websocket rooms

--- twake/backend/node/src/services/channels/realtime.ts
+++ twake/backend/node/src/services/channels/realtime.ts
@@ -1,12 +1,32 @@
+import { User, Workspace, WebsocketMetadata } from "../types";
 import { Channel } from "./entities";
-import { ChannelWebsocket } from "./web/types";
 
-export function getWebsocketInformation(channel: Channel): ChannelWebsocket {
+export function getWebsocketInformation(channel: Channel): WebsocketMetadata {
   return {
     name: channel.name,
     room: getChannelPath(channel),
     encryption_key: ""
   };
+}
+
+export function getWorkspaceRooms(workspace: Workspace, user: User, includeDirect: boolean): WebsocketMetadata[] {
+  return [
+    { room: getPublicRoomName(workspace) },
+    { room: getPrivateRoomName(workspace, user) },
+    ...(includeDirect? [{ room: getDirectChannelRoomName(workspace, user)}]: [])
+  ];
+}
+
+export function getDirectChannelRoomName(workspace: Workspace, user: User): string {
+  return `/companies/${workspace.company_id}/workspaces/direct/channels?type=direct&user=${user.id}`;
+}
+
+export function getPrivateRoomName(workspace: Workspace, user: User): string {
+  return `/companies/${workspace.company_id}/workspaces/${workspace.workspace_id}/channels?type=private&user=${user.id}`;
+}
+
+export function getPublicRoomName(workspace: Workspace): string {
+  return `/companies/${workspace.company_id}/workspaces/${workspace.workspace_id}/channels?type=public`;
 }
 
 export function getChannelPath(channel: Channel): string {
