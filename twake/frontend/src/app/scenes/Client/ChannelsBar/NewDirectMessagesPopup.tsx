b030f52f4dcadbf69ccbfdd4f79011d3a97e6bd9
Stéphane VIEIRA <svieira@linagora.com>
2020-11-03 15:27:49 +0100

    #520 Use new channel collection for direct messages popup

--- twake/frontend/src/app/scenes/Client/ChannelsBar/NewDirectMessagesPopup.tsx
+++ twake/frontend/src/app/scenes/Client/ChannelsBar/NewDirectMessagesPopup.tsx
@@ -6,9 +6,38 @@ import ChannelsService from 'services/channels/channels.js';
 import MediumPopupComponent from 'app/services/Modal/ModalManager';
 import { ObjectModal, ObjectModalTitle } from 'components/ObjectModal/ObjectModal.js';
 import UserListManager from 'components/UserListManager/UserListManager.js';
+import { ChannelType, ChannelResource } from 'app/models/Channel';
+import Collections from 'app/services/CollectionsReact/Collections';
+
+import { useParams } from 'react-router-dom';
+import RouterServices from 'services/RouterServices';
+import OldCollections from 'services/Depreciated/Collections/Collections';
 
 const NewDirectMessagesPopup: FC = () => {
   const [newUserDiscussion, setNewUserDiscussion] = useState<string[]>([]);
+
+  // TODO find a better way to get company id
+  const params: any = useParams();
+  const workspaceId = RouterServices.translateToUUID(params.workspaceId);
+  const company_id = OldCollections.get('workspaces').find(workspaceId)?.group?.id;
+
+  const collectionPath: string = `companies/${company_id}/workspaces/direct/channels/`;
+  const ChannelsCollections = Collections.get(collectionPath);
+
+  const upsertDirectMessage = async (): Promise<any> => {
+    const newDirectMessage: ChannelType = {
+      company_id: company_id,
+      workspace_id: workspaceId,
+      visibility: 'direct',
+    };
+
+    await ChannelsCollections.upsert(new ChannelResource(newDirectMessage), {
+      httpOptions: { members: newUserDiscussion },
+    });
+    // Do not use this, this is cheating !
+    // await ChannelsService.openDiscussion(newUserDiscussion);
+    await MediumPopupComponent.closeAll();
+  };
 
   return (
     <ObjectModal
@@ -24,10 +53,7 @@ const NewDirectMessagesPopup: FC = () => {
           className="small primary"
           style={{ width: 'auto', float: 'right' }}
           disabled={newUserDiscussion.length === 0}
-          onClick={() => {
-            ChannelsService.openDiscussion(newUserDiscussion);
-            MediumPopupComponent.closeAll();
-          }}
+          onClick={() => upsertDirectMessage()}
         >
           {Languages.t('general.continue', [], 'Continue')}
         </Button>
