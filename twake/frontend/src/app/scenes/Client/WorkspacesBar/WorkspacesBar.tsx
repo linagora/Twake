4befa6211d36ff1c9c8d9ca9feb63d0f89d56705
Romaric Mourgues <rmourgues@linagora.com>
2020-11-06 11:33:03 +0100

    Fix workspace bar UI

--- twake/frontend/src/app/scenes/Client/WorkspacesBar/WorkspacesBar.tsx
+++ twake/frontend/src/app/scenes/Client/WorkspacesBar/WorkspacesBar.tsx
@@ -1,15 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-import Languages from 'services/languages/languages.js';
 import { Layout } from 'antd';
 import Groups from 'services/workspaces/groups.js';
 import Workspaces from 'services/workspaces/workspaces.js';
 import Group from './Components/Group.js';
 import Workspace from './Components/Workspace.js';
-import PopupManager from 'services/popupManager/popupManager.js';
-import CreateWorkspacePage from 'app/scenes/Client/Popup/CreateWorkspacePage/CreateWorkspacePage.js';
-import WorkspaceAdd from 'components/Leftbar/Workspace/WorkspaceAdd.js';
-import WorkspaceUserRights from 'services/workspaces/workspace_user_rights.js';
 import ElectronService from 'services/electron/electron.js';
 import './WorkspacesBar.scss';
 
@@ -21,7 +16,6 @@ export default () => {
 
   Workspaces.useListener();
   Groups.useListener();
-  WorkspaceUserRights.useListener();
 
   Workspaces.initSelection();
 
@@ -34,9 +28,6 @@ export default () => {
         {Workspaces.getOrderedWorkspacesInGroup(companyId, false).map((item: any) => (
           <Workspace key={item.id} workspace={item} isSelected={workspaceId === item.id} />
         ))}
-        {!!WorkspaceUserRights.hasGroupPrivilege('MANAGE_DATA') && (
-          <WorkspaceAdd onClick={() => PopupManager.open(<CreateWorkspacePage />)} />
-        )}
       </PerfectScrollbar>
 
       <Group selected={companyId} />
