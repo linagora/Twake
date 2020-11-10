08a888606e4b6631f893907837959990be052ed8
Romaric Mourgues <rmourgues@linagora.com>
2020-11-10 15:40:44 +0100

    Fix backend tests

--- twake/backend/node/src/core/platform/services/auth/web/routes.ts
+++ twake/backend/node/src/core/platform/services/auth/web/routes.ts
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 
 const routes: FastifyPluginCallback = (fastify: FastifyInstance, _opts, next) => {
   fastify.get("/login", async (_request, reply) => {
-    reply.send({ token: fastify.jwt.sign({ id: uuidv4() }) });
+    reply.send({ token: fastify.jwt.sign({ sub: uuidv4() }) });
   });
 
   next();
