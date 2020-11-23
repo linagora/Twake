e1a4e25da781364674ec81ad5449323f5ce4ce47
Christophe HAMERLING <chamerling@linagora.com>
2020-11-23 14:13:38 +0100

    #570 Add user in login response

--- twake/backend/node/src/core/platform/services/auth/web/routes.ts
+++ twake/backend/node/src/core/platform/services/auth/web/routes.ts
@@ -3,7 +3,13 @@ import { v4 as uuidv4 } from "uuid";
 
 const routes: FastifyPluginCallback = (fastify: FastifyInstance, _opts, next) => {
   fastify.get("/login", async (_request, reply) => {
-    reply.send({ token: fastify.jwt.sign({ sub: uuidv4() }) });
+    const userId = uuidv4();
+    reply.send({
+      token: fastify.jwt.sign({ sub: userId }),
+      user: {
+        id: userId,
+      },
+    });
   });
 
   next();
