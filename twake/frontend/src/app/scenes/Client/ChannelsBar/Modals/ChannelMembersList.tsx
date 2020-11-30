36e958ece75479782387466f2103e180207f9fa3
Romaric Mourgues <rmourgues@linagora.com>
2020-11-30 10:53:16 +0100

    Add margin to members popup

--- twake/frontend/src/app/scenes/Client/ChannelsBar/Modals/ChannelMembersList.tsx
+++ twake/frontend/src/app/scenes/Client/ChannelsBar/Modals/ChannelMembersList.tsx
@@ -90,7 +90,7 @@ const ChannelMembersList: FC<Props> = props => {
         </Row>
       </div>
       {channelMembers && (
-        <div className="x-margin">
+        <div className="x-margin bottom-margin">
           {channelMembers.map((item: ChannelMemberResource) => {
             const user: UserType = DepreciatedCollections.get('users').find(item.data.user_id);
 
