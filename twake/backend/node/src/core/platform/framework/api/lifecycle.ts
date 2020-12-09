7e301aa7f73a5e2859b302daced92b33ce7e08ac
Christophe HAMERLING <chamerling@linagora.com>
2020-12-09 14:21:22 +0100

    #658 Add optional context in init

--- twake/backend/node/src/core/platform/framework/api/lifecycle.ts
+++ twake/backend/node/src/core/platform/framework/api/lifecycle.ts
@@ -1,4 +1,6 @@
+import { TwakeContext } from "./context";
+
 export interface Initializable {
   // TODO: Flag to tell if a service which fails to initialize must break all
-  init?(): Promise<this>;
+  init?(context?: TwakeContext): Promise<this>;
 }
