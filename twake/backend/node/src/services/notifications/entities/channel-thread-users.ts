5cf96decad8cc689c4d0c44bd4d59756c7d74cd7
Christophe HAMERLING <chamerling@linagora.com>
2020-12-14 19:40:31 +0100

    #672 Add the channelThreadUser subservice in notifications

--- twake/backend/node/src/services/notifications/entities/channel-thread-users.ts
+++ twake/backend/node/src/services/notifications/entities/channel-thread-users.ts
@@ -1,31 +1,41 @@
 import { Type } from "class-transformer";
+import { Column, Entity } from "../../../core/platform/services/database/services/orm/decorators";
 
+@Entity(ChannelThreadUsers.TYPE, {
+  primaryKey: [["company_id", "channel_id", "thread_id"]],
+  type: ChannelThreadUsers.TYPE,
+})
 export class ChannelThreadUsers {
+  static TYPE = "channel_thread_users";
   /**
    * UUIDv4
    * Primary key / partition key
    */
   @Type(() => String)
+  @Column("company_id", "uuid")
   company_id: string;
 
   /**
    * UUIDv4
-   * Primary key
+   * Primary key / partition key
    */
   @Type(() => String)
+  @Column("channel_id", "uuid")
   channel_id: string;
 
   /**
    * UUIDv4
-   * Primary key
+   * Primary key / partition key
    */
   @Type(() => String)
+  @Column("thread_id", "uuid")
   thread_id: string;
 
   /**
    * UUIDv4
    */
   @Type(() => String)
+  @Column("user_id", "uuid")
   user_id: string;
 }
 
