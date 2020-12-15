fb16d862533ed46ccaf2c95eed6752d7bbff2c3f
Christophe HAMERLING <chamerling@linagora.com>
2020-12-15 12:59:18 +0100

    #672 Add pubsub handlers

--- twake/backend/node/src/core/platform/services/pubsub/api.ts
+++ twake/backend/node/src/core/platform/services/pubsub/api.ts
@@ -1,4 +1,4 @@
-import { TwakeServiceProvider } from "../../framework";
+import { Initializable, logger, TwakeContext, TwakeServiceProvider } from "../../framework";
 
 export interface PubsubMessage<T> {
   data: T;
@@ -31,10 +31,8 @@ export interface PubsubEventBus {
   publish<T>(message: PubsubEventMessage<T>): boolean;
 }
 
-export abstract class PubsubServiceSubscription<Service> {
+export abstract class PubsubServiceSubscription {
   protected pubsub: PubsubServiceAPI;
-
-  constructor(protected service: Service) {}
 
   async subscribe(pubsub: PubsubServiceAPI): Promise<void> {
     if (!pubsub) {
@@ -46,4 +44,86 @@ export abstract class PubsubServiceSubscription<Service> {
   }
 
   abstract doSubscribe(): Promise<void>;
+}
+
+export abstract class PubsubServiceProcessor<In, Out>
+  extends PubsubServiceSubscription
+  implements Initializable {
+  constructor(protected handler: PubsubHandler<IncomingPubsubMessage<In>, PubsubMessage<Out>>) {
+    super();
+  }
+
+  async init(context: TwakeContext): Promise<this> {
+    try {
+      await this.subscribe(context.getProvider("pubsub"));
+    } catch (err) {
+      logger.warn({ err }, "Not able to start the PubsubServiceProcessor");
+    }
+
+    return this;
+  }
+
+  async process(message: IncomingPubsubMessage<In>): Promise<Out> {
+    const result = await this.handler.process(message);
+
+    return result.data;
+  }
+
+  async doSubscribe(): Promise<void> {
+    await this.pubsub.subscribe(this.handler.topics.in, this.processMessage.bind(this));
+  }
+
+  private async processMessage(message: IncomingPubsubMessage<In>): Promise<void> {
+    try {
+      const result = await this.process(message);
+
+      if (result) {
+        await this.doPublish(result);
+      }
+    } catch (error) {
+      if (this.handler.topics.error) {
+        this.pubsub.publish(this.handler.topics.error, {
+          data: {
+            type: "error",
+            message: error instanceof Error ? (error as Error).message : String(error),
+          },
+        });
+      }
+    }
+  }
+
+  private async doPublish(message: Out): Promise<void> {
+    if (!this.handler.topics.out) {
+      return;
+    }
+
+    return this.pubsub.publish(this.handler.topics.out, {
+      data: message,
+    });
+  }
+}
+
+/**
+ * A pubsub handler is in charge of processing message from a topic and publishing the processing result to another topic
+ */
+export interface PubsubHandler<InputMessage, OutputMessage> extends Initializable {
+  readonly topics: {
+    // The topic to subscribe to
+    in: string;
+    // The topic to push process result to
+    out: string;
+    // The topic to push error to. When topic is undefined, do not push the error
+    error?: string;
+  };
+
+  /**
+   * The handler name
+   */
+  readonly name: string;
+
+  /**
+   * Process the message and potentially produces result which will be published elsewhere
+   * @param message
+   */
+  process(message: InputMessage): Promise<OutputMessage>;
 }
