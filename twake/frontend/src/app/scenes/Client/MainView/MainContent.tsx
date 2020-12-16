e6602ac32055563a70c5784cf36ce8ce143e2f7b
Romaric Mourgues <rmourgues@linagora.com>
2020-12-16 10:35:02 +0100

    Add missing title in side view

--- twake/frontend/src/app/scenes/Client/MainView/MainContent.tsx
+++ twake/frontend/src/app/scenes/Client/MainView/MainContent.tsx
@@ -7,7 +7,7 @@ import SideViewService from 'app/services/AppView/SideViewService';
 import MainViewService from 'app/services/AppView/MainViewService';
 import NoApp from './NoApp';
 import { X } from 'react-feather';
-import Languages from 'services/languages/languages.js';
+import ViewName from './AppView/ViewName';
 
 const MainContent: FC<{}> = () => {
   const [mainType, mainId] = MainViewService.useWatcher(() => [
@@ -23,39 +23,47 @@ const MainContent: FC<{}> = () => {
   return (
     <Layout.Content className={'global-view-content'}>
       <Layout style={{ height: '100%' }}>
-        <Layout.Content>
-          <Layout className="main-view-layout">
-            {mainConfiguration.hasTabs && (
-              <Layout.Header className="main-view-tabs-header">
-                <Tabs key={mainId} />
-              </Layout.Header>
+        {mainConfiguration.hasTabs && (
+          <Layout.Header className="main-view-tabs-header">
+            <Tabs key={mainId} />
+          </Layout.Header>
+        )}
+        <Layout style={{ height: '100%' }}>
+          <Layout.Content>
+            <Layout className="main-view-layout">
+              <Layout.Content className="main-view-content">
+                {mainType !== '' && (
+                  <AppView key={mainId} id={mainId} viewService={MainViewService} />
+                )}
+                {mainType == '' && <NoApp />}
+              </Layout.Content>
+            </Layout>
+          </Layout.Content>
+          <Layout.Sider
+            className="global-side-view"
+            breakpoint="lg"
+            collapsedWidth="0"
+            theme="light"
+            width="40%"
+            collapsed={sideType === ''}
+          >
+            {!!sideType && (
+              <Layout style={{ height: '100%' }}>
+                <Layout.Header className="side-header">
+                  <ViewName
+                    key={sideId + '-side-header'}
+                    id={sideId}
+                    viewService={SideViewService}
+                  />
+                  <X onClick={() => SideViewService.select('', { context: {} })} />
+                </Layout.Header>
+                <Layout.Content style={{ flex: 1 }}>
+                  <AppView key={sideId + '-side'} id={sideId} viewService={SideViewService} />
+                </Layout.Content>
+              </Layout>
             )}
-            <Layout.Content className="main-view-content">
-              {mainType !== '' && (
-                <AppView key={mainId} id={mainId} viewService={MainViewService} />
-              )}
-              {mainType == '' && <NoApp />}
-            </Layout.Content>
-          </Layout>
-        </Layout.Content>
-        <Layout.Sider
-          className="global-side-view"
-          breakpoint="lg"
-          collapsedWidth="0"
-          theme="light"
-          width="40%"
-          collapsed={sideType === ''}
-        >
-          <div className="side-header">
-            <span>
-              {Languages.t('scenes.app.side_app.messages_thread_title', ['Software • FT - Twake'])}
-            </span>
-            <X onClick={() => SideViewService.select('', { context: {} })} />
-          </div>
-          {sideType !== '' && (
-            <AppView key={sideId + '-side'} id={sideId} viewService={SideViewService} />
-          )}
-        </Layout.Sider>
+          </Layout.Sider>
+        </Layout>
       </Layout>
     </Layout.Content>
   );
