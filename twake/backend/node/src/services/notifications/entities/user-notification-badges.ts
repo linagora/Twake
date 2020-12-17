6138aadd54e26c19824618e343f0c40baca0cc5f
Christophe HAMERLING <chamerling@linagora.com>
2020-12-17 16:20:17 +0100

    #672 Annotate user notification badge

--- twake/backend/node/src/services/notifications/entities/user-notification-badges.ts
+++ twake/backend/node/src/services/notifications/entities/user-notification-badges.ts
@@ -1,29 +1,38 @@
 import { Type } from "class-transformer";
 import { ChannelType } from "../../types";
+import { Column, Entity } from "../../../core/platform/services/database/services/orm/decorators";
 
 /**
  * Table user-notification-badges
  */
+@Entity(UserNotificationBadge.TYPE, {
+  primaryKey: [["company_id"], "user_id", "workspace_id", "channel_id", "thread_id"],
+  type: UserNotificationBadge.TYPE,
+})
 export class UserNotificationBadge {
+  static TYPE = "user_notification_badges";
+
   /**
    * UUIDv4
    * Primary key / partition key
    */
   @Type(() => String)
-  user_id: string;
+  @Column("company_id", "uuid")
+  company_id: string;
 
   /**
    * UUIDv4
-   * Primary key
    */
   @Type(() => String)
-  company_id: string;
+  @Column("user_id", "uuid")
+  user_id: string;
 
   /**
    * Text
    * Primary key
    */
   @Type(() => String)
+  @Column("workspace_id", "string")
   workspace_id: string | ChannelType.DIRECT;
 
   /**
@@ -31,12 +40,19 @@ export class UserNotificationBadge {
    * Primary key
    */
   @Type(() => String)
+  @Column("channel_id", "uuid")
   channel_id: string;
 
-  message_count = 0;
+  /**
+   * UUIDv4
+   * Primary key
+   */
+  @Type(() => String)
+  @Column("thread_id", "uuid")
+  thread_id: string;
 }
 
 export type UserNotificationBadgePrimaryKey = Pick<
   UserNotificationBadge,
-  "user_id" | "company_id" | "workspace_id" | "channel_id"
+  "user_id" | "company_id" | "workspace_id" | "channel_id" | "thread_id"
 >;
