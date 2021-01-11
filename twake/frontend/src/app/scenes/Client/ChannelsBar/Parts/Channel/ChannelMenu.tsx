784f7eae93619b09dc26f62cd17b93f4547dce6c
Romaric Mourgues <rmourgues@linagora.com>
2021-01-11 10:31:10 +0100

    Fix missing direct notifications and disable "hide discussion"

--- twake/frontend/src/app/scenes/Client/ChannelsBar/Parts/Channel/ChannelMenu.tsx
+++ twake/frontend/src/app/scenes/Client/ChannelsBar/Parts/Channel/ChannelMenu.tsx
@@ -133,6 +133,7 @@ export default (props: Props): JSX.Element => {
     },
     {
       type: 'menu',
+      hide: props.channel.data.visibility === 'direct', //TODO remove when #714 fixed
       text: Languages.t(
         props.channel.data.visibility === 'direct'
           ? 'scenes.app.channelsbar.hide_discussion_leaving'
