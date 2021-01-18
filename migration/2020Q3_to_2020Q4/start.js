47652bf4c53821ba5b0e8d0c0c7ce5331afe8a63
Romaric Mourgues <rmourgues@linagora.com>
2021-01-18 13:17:57 +0100

    Add config in json to prepare production execution

--- migration/2020Q3_to_2020Q4/start.js
+++ migration/2020Q3_to_2020Q4/start.js
@@ -1,16 +1,12 @@
 "use strict";
+const config = require("config");
 
 const configuration = {
-  encryption: {
-    key: "c9a17eab88ab63bb3e90c027196a89776651a7c06651a7c0",
-    defaultIv: "twake_constantiv",
-  },
-  db: {
-    contactPoints: ["127.0.0.1"],
-    localDataCenter: "datacenter1",
-    keyspace: "twake",
-  },
+  db: config.get("db"),
+  encryption: config.get("enryption"),
 };
+
+console.log(configuration);
 
 const migration = require("./migration/migrate.js");
 migration(configuration);
