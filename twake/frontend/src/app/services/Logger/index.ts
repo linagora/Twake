9a86a1486a2b7837f87487b333ebd4c51eb24b23
Christophe HAMERLING <chamerling@linagora.com>
2021-01-19 15:54:42 +0100

    Change logger export

--- twake/frontend/src/app/services/Logger/index.ts
+++ twake/frontend/src/app/services/Logger/index.ts
@@ -2,12 +2,10 @@ import log from "loglevel";
 import prefix from "loglevel-plugin-prefix";
 const isProduction = process.env?.NODE_ENV === "production";
 
-const logger = log.getLogger("Twake");
-
-isProduction ? logger.setDefaultLevel(log.levels.WARN) : logger.setDefaultLevel(log.levels.DEBUG);
+isProduction ? log.setDefaultLevel(log.levels.WARN) : log.setDefaultLevel(log.levels.DEBUG);
 
 prefix.reg(log);
-prefix.apply(logger, {
+prefix.apply(log, {
   template: `${isProduction ? "" : "[%t] "}%l - %n -`,
   levelFormatter(level) {
     return level.toUpperCase();
@@ -20,4 +18,4 @@ prefix.apply(logger, {
   },
 });
 
-export default logger;
+export default log;
