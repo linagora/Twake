067eb60d111c0fa8fa1abcfed33a5c9fa8ae74ba
Aiman R'KYEK <arkyek@linagora.com>
2021-01-25 09:58:18 +0100

    Implement the script

--- migration/2020Q3_to_2020Q4/migration/migrate.js
+++ migration/2020Q3_to_2020Q4/migration/migrate.js
@@ -2,107 +2,182 @@
 
 const cassandra = require("cassandra-driver");
 const decrypt = require("./decrypt.js");
+const config = require("config");
+
+const configuration = {
+  db: config.get("db"),
+  encryption: config.get("encryption"),
+};
 
 const client = new cassandra.Client({
-  contactPoints: ["127.0.0.1"],
-  localDataCenter: "datacenter1",
-  keyspace: "twake",
+  contactPoints: configuration.db.contactPoints, //["172.17.0.1:9042"]
+  localDataCenter: configuration.db.localDataCenter,
+  keyspace: configuration.db.keyspace,
   wait: false,
   retries: 10,
   delay: 200,
 });
 
-const decryptChannels = async (channels, key, defaultIv) => {
-  return Promise.all(
-    channels.map(async (channel) => decrypt(channel, key, defaultIv))
-  );
-};
-
-const getchannels = () => {
-  const query = "SELECT * FROM channel ";
+const getChannels = () => {
+  const query = "SELECT * FROM channel";
 
   return new Promise((resolve, reject) => {
     client.execute(query, (err, result) => {
-      if (err) reject(err);
+      if (err) console.log(err);
+
+      if (!result) throw "Something went wrong in getting Channels";
 
       resolve(result.rows);
 
-      client.shutdown();
+      // client.shutdown();
     });
   });
 };
 
-const init = async (configuration) => {
-  //TODO: store the configuration somewhere and use it
+const getUserCompanies = (userId) => {
+  const query = `SELECT group_id FROM group_user WHERE user_id = ${userId}`;
 
-  //Test
-  console.log(
-    await decrypt(
-      ["hello"],
-      configuration.encryption.key,
-      configuration.encryption.defaultIv
-    )
+  return new Promise((resolve, reject) => {
+    client.execute(query, (err, result) => {
+      if (err) console.log(err);
+
+      resolve(result.rows);
+
+      // client.shutdown();
+    });
+  });
+};
+
+const getNumberOfUsersInCompany = (companyId) => {
+  const query = `SELECT member_count FROM group_entity WHERE id = ${companyId}`;
+
+  return new Promise((resolve, reject) => {
+    client.execute(query, (err, result) => {
+      if (err) console.log(err);
+
+      if (!result) throw "Something went wrong in getting Channels";
+
+      resolve({ companyId, numberOfUsers: result.rows[0].member_count });
+
+      // client.shutdown();
+    });
+  });
+};
+
+const addChannelEntity = (channel, direct_channel_company_id) => {
+  let newChannel = {};
+
+  // Store in channels
+  newChannel = {
+    company_id: channel.original_group_id,
+    workspace_id: channel.direct ? "direct" : channel.original_workspace_id,
+    id: channel.id,
+    archived: false,
+    channel_group: channel.direct
+      ? direct_channel_company_id
+      : channel.channel_group_name, // pass it in prams!
+    connectors: channel.connectors, //json.decode?? parse
+    description: channel.description,
+    icon: channel.icon,
+    is_default: true,
+    name: channel.name,
+    owner: "",
+    visibility: channel.direct
+      ? "direct"
+      : channel.private
+      ? "private"
+      : "public",
+    members: channel.direct ? channel.identifier.split("+") : [],
+  };
+
+  // Store it in Direct channels
+  if (channel.direct) {
+    newChannel = {
+      company_id: direct_channel_company_id,
+      users: channel.identifier.split("+").sort().join(","),
+      channel_id: channel.id,
+    };
+  }
+
+  // console.log(newChannel);
+};
+
+const getCompanyWithBigestNumberOfUsers = async (matchedCompanies) => {
+  const companies = await Promise.all(
+    matchedCompanies.map(({ group_id }) => getNumberOfUsersInCompany(group_id))
   );
 
-  //
-  getchannels()
-    .then((channels) =>
-      decryptChannels(
-        channels,
-        configuration.encryption.key,
-        configuration.encryption.defaultIv
-      )
-    )
-    .then((res) => console.log(res));
+  /*
+  [
+    {
+      companyId: TimeUuid: 5bae5ba6-f409-11ea-919a-0242ac120006,
+      numberOfUsers: Long: 0 
+    },
+    {
+      companyId: TimeUuid: 6a938522-f640-11ea-b92e-0242ac120006,
+      numberOfUsers: Long: 1
+    }
+  ]
+  */
+};
+
+const determinareCompanyId = async (channel) => {
+  let directChannelCompanyId = "";
+  let userCompanies = [];
+
+  const _fillCompanyId = async (matchedCompanies) => {
+    switch (true) {
+      case !matchedCompanies.length:
+        //delete channel
+        // console.log("Delete");
+        break;
+      case matchedCompanies.length === 1:
+        // console.log(matchedCompanies[0].group_id);
+        // directChannelCompanyId = matchedCompanies[0]
+        break;
+      case matchedCompanies.length > 1:
+        await getCompanyWithBigestNumberOfUsers(matchedCompanies);
+        // console.log("222222222222222222");
+        break;
+    }
+  };
+
+  // Todo: handle the case when channel.member is an object
+
+  if (!!JSON.parse(channel.members).length) {
+    userCompanies = await Promise.all(
+      JSON.parse(channel.members).map((userId) => getUserCompanies(userId))
+    );
+
+    const matchedCompanies = userCompanies.shift().filter((v) => {
+      return userCompanies.every((company) => {
+        return company.indexOf(v) !== -1;
+      });
+    });
+
+    _fillCompanyId(matchedCompanies);
+  }
+
+  // console.log(userCompanies);
+};
+
+const importChannel = async (channel) => {
+  const decryptedChannel = await decrypt(
+    channel,
+    configuration.encryption.key,
+    configuration.encryption.defaultIv
+  );
+
+  // Determinate a company id: direct_channel_company_id
+  const directChannelCompanyId = determinareCompanyId(decryptedChannel);
+
+  // addChannelEntity(decryptedChannel, "direct_channel_company_id");
+};
+
+const init = () => {
+  getChannels().then((channels) =>
+    Promise.all(channels.map(async (channel) => importChannel(channel)))
+  );
 };
 
 module.exports = init;
-
-// const getChannelMembers = () => {
-//   const query = "SELECT * FROM channel_member";
-
-//   client.execute(query, (err, result) => {
-//     if (err) return console.log("ERROR!!!", err);
-
-//     console.log(result.rows);
-
-//     client.shutdown();
-//   });
-// };
-
-/*
-
-Cold migration: offline (No dual Writes)
-1. Migrating schema
-2. Forklifting existing data
-3. Validation
-4. Cut-over; fading off the old to the new system
-
- We need to ignore:
- Channels that are application channels (anything with a .app_id != null)
- Channels that have messages_increment == 0
-*/
-
-/*
-  ChannelType = {
-    company_id 
-    workspace_id <=== original_workspace_id
-    type <=== direct? "direct" : "workspace"
-    id <=== id
-    icon <=== icon
-    name <=== name
-    description <=== description
-    channel_group <=== channel_group_name
-    visibility <=== private ? ("private" | "public" | "direct")
-    default <=== What to choose by default?
-    direct_channel_members <=== members
-    owner
-    members_count <=== members_count
-    guests_count <=== ext_members
-    messages_count <=== messages_count
-    archived
-    archivation_date
-    user_member
-    connectors <=== connectors
-  }
-  */
--- migration/2020Q3_to_2020Q4/start.js
+++ migration/2020Q3_to_2020Q4/start.js
@@ -1,12 +1,5 @@
 "use strict";
-const config = require("config");
-
-const configuration = {
-  db: config.get("db"),
-  encryption: config.get("enryption"),
-};
-
-console.log(configuration);
 
 const migration = require("./migration/migrate.js");
-migration(configuration);
+
+migration();
