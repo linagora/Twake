98cc9bf990fc3b6afb15335f708ef693116cdf88
Romaric Mourgues <rmourgues@linagora.com>
2021-02-15 11:50:47 +0100

    Remove unused code creating errors

--- twake/backend/core/src/Twake/Channels/Resources/Services.php
+++ twake/backend/core/src/Twake/Channels/Resources/Services.php
@@ -8,11 +8,7 @@ class Services extends BaseServices
 {
     protected $services = [
         "app.channels.direct_messages_system" => "DirectMessagesSystem",
-//            arguments: ["@app.twake_doctrine", "@app.messages", "@app.applications_api","@app.workspace_members"]
-        "app.channels.channels_system" => "ChannelsSystem",
-//            arguments: ["@app.twake_doctrine", "@app.messages", "@app.websockets", "@app.applications_api", "@app.workspace_members", "@app.accessmanager"]
-        "app.channels.notifications" => "ChannelsNotificationsSystem",
-//            arguments: [ "@app.twake_doctrine", "@app.notifications", "@app.pusher","@app.workspace_members"]
+        "app.channels.channels_system" => "ChannelsSystem"
     ];
 
 }
