694474500f4011cffb1bbf1b9a04adf313491609
Christophe HAMERLING <chamerling@linagora.com>
2021-02-22 13:58:33 +0100

    Add TTL on subscription

--- twake/backend/node/src/core/platform/services/pubsub/amqp/pubsubclient.ts
+++ twake/backend/node/src/core/platform/services/pubsub/amqp/pubsubclient.ts
@@ -1,6 +1,6 @@
 import { logger } from "../../../framework/logger";
 import { constants as CONSTANTS } from "./constants";
-import { AmqpClient, AmqpCallbackType, PublishOptions } from "./client";
+import { AmqpClient, AmqpCallbackType, PublishOptions, SubscribeOptions } from "./client";
 import { Options } from "amqplib";
 
 const LOG_PREFIX = "service.pubsub.amqp.AmqpPubsubClient -";
@@ -25,10 +25,15 @@ export class AmqpPubsubClient extends AmqpClient {
     );
   }
 
-  subscribe(topic: string, callback: AmqpCallbackType): Promise<void> {
+  subscribe(topic: string, options: SubscribeOptions, callback: AmqpCallbackType): Promise<void> {
+    const queueOptions: Options.AssertQueue = {
+      ...CONSTANTS.SUBSCRIBER.queueOptions,
+      ... options?.ttl ? { messageTtl: options.ttl } : {},
+    };
+
     return this.assertExchange(topic, CONSTANTS.PUBSUB_EXCHANGE.type)
       .then(() =>
-        this.assertQueue(CONSTANTS.SUBSCRIBER.queueName, CONSTANTS.SUBSCRIBER.queueOptions),
+        this.assertQueue(CONSTANTS.SUBSCRIBER.queueName, queueOptions),
       )
       .then(res => this.assertBinding(res.queue, topic).then(() => res))
       .then(res => this.consume(res.queue, CONSTANTS.SUBSCRIBER.consumeOptions, callback));
@@ -37,7 +42,7 @@ export class AmqpPubsubClient extends AmqpClient {
   /**
    * Creates a new consumer which asserts that it will listen to messages in a durable queue.
    * The durable queue goal is to be able to receive events 'from the past' ie events which has been push to the queue while subscribers were not bound.
-   * This is quite useful in case of ESN restart to not miss anything between stop and start.
+   * This is quite useful in case of restart to not miss anything between stop and start.
    * Important: If multiple consumers are connected to the same durable queue, AMQP will deliver message to one and only one consumer with a Round-robin behavior by default.
    *
    * @param {String} exchangeName - The exchange name
@@ -47,11 +52,18 @@ export class AmqpPubsubClient extends AmqpClient {
   subscribeToDurableQueue(
     exchangeName: string,
     queueName: string,
+    options: SubscribeOptions,
     callback: AmqpCallbackType,
   ): Promise<void> {
-    logger.debug(`${LOG_PREFIX} Subscribing to durable queue ${queueName}`);
+    const durableQueueOptions: Options.AssertQueue = {
+      ...CONSTANTS.SUBSCRIBER.durableQueueOptions,
+      ...options?.ttl ? { messageTtl: options.ttl } : {}
+    };
+
+    logger.debug(`${LOG_PREFIX} Subscribing to durable queue ${queueName} with options %o`, durableQueueOptions);
+    
     return this.assertExchange(exchangeName, CONSTANTS.PUBSUB_EXCHANGE.type)
-      .then(() => this.assertQueue(queueName, CONSTANTS.SUBSCRIBER.durableQueueOptions))
+      .then(() => this.assertQueue(queueName, durableQueueOptions))
       .then(() => this.assertBinding(queueName, exchangeName))
       .then(() => this.consume(queueName, CONSTANTS.SUBSCRIBER.consumeOptions, callback));
   }
