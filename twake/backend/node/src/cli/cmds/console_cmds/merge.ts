2754b15a423630cf9dc73728e763f8f59cc2be56
Christophe HAMERLING <chamerling@linagora.com>
2021-03-03 18:20:18 +0100

    The cli uses the console service

--- twake/backend/node/src/cli/cmds/console_cmds/merge.ts
+++ twake/backend/node/src/cli/cmds/console_cmds/merge.ts
@@ -1,29 +1,25 @@
-import axios, { AxiosInstance } from "axios";
-import { BehaviorSubject, concat, EMPTY, from, Observable } from "rxjs";
-import { distinct, map, mergeMap, tap } from "rxjs/operators";
 import yargs from "yargs";
-import Company from "../../../services/user/entities/company";
-import User from "../../../services/user/entities/user";
-import UserServiceAPI from "../../../services/user/api";
 import twake from "../../../twake";
-import { Paginable } from "../../../core/platform/framework/api/crud-service";
 import { getLogger } from "../../../core/platform/framework/logger";
-
-const services = ["user", "channels", "notifications", "database", "webserver", "pubsub"];
-const logger = getLogger("cli");
+import { ConsoleServiceAPI } from "../../../services/console/api";
+import { count, share } from "rxjs/operators";
 
 type MergeParams = {
   url: string;
   concurrent: number;
+  dry: boolean;
 };
 
-type ConsoleCompany = {
-  code: string;
-};
-
-type ConsoleUser = {
-  id: string;
-};
+const services = [
+  "user",
+  "channels",
+  "notifications",
+  "database",
+  "webserver",
+  "pubsub",
+  "console",
+];
+const logger = getLogger("cli");
 
 const command: yargs.CommandModule<MergeParams, MergeParams> = {
   command: "merge",
@@ -39,151 +35,64 @@ const command: yargs.CommandModule<MergeParams, MergeParams> = {
       type: "number",
       description: "Number of concurrent imports",
     },
+    dry: {
+      default: false,
+      type: "boolean",
+      description: "Make a dry run without creating anything on the Twake console",
+    },
   },
   handler: async argv => {
-    const companyCreated = new BehaviorSubject(0);
-    const usersCreated = new BehaviorSubject(0);
-    const concurrent = argv.concurrent;
     const platform = await twake.run(services);
-    const consoleClient = new ConsoleClient(argv.url);
-    const userService = platform.getProvider<UserServiceAPI>("user");
-
-    const createCompany = (
-      company: Company,
-    ): Promise<{ source: Company; destination: ConsoleCompany }> => {
-      const result: ConsoleCompany = {
-        code: company.id,
-      };
-      return new Promise(resolve =>
-        setTimeout(
-          () =>
-            resolve({
-              source: company,
-              destination: result,
-            }),
-          10,
-        ),
-      );
+    const consoleService = platform.getProvider<ConsoleServiceAPI>("console");
+    const merge = consoleService.merge(argv.url, argv.concurrent, argv.dry);
+    const stats = {
+      users: 0,
+      companies: 0,
     };
+    const companies = [];
 
-    const createUser = (
-      company: Company,
-      user: User,
-    ): Promise<{ source: { user: User; company: Company }; destination: ConsoleUser }> => {
-      const result: ConsoleUser = {
-        id: user.id,
-      };
-      return new Promise(resolve =>
-        setTimeout(
-          () =>
-            resolve({
-              source: { user, company },
-              destination: result,
-            }),
-          1,
-        ),
-      );
-    };
+    const users$ = merge.users$.pipe(share());
+    const companies$ = merge.companies$.pipe(share());
+    const countUsers = users$.pipe(count()).subscribe({
+      next: count => (stats.users = count),
+    });
+    const countCompanies = companies$.pipe(count()).subscribe({
+      next: count => (stats.companies = count),
+    });
 
-    const getUsers = (company: Company, paginable?: Paginable): Observable<User> => {
-      return from(
-        userService.companies.getUsersForCompany({ group_id: company.id }, paginable),
-      ).pipe(
-        mergeMap(usersResult => {
-          const items$ = from(usersResult.getEntities());
-          const next$ = usersResult?.nextPage?.page_token
-            ? getUsers(company, usersResult.nextPage)
-            : EMPTY;
-          return concat(items$, next$);
-        }),
-        distinct(user => user.id),
-      );
-    };
+    const companiesSubscription = companies$.subscribe({
+      next: company => {
+        logger.info("New company created %s", company.source.displayName);
+        companies.push(company);
+      },
+      error: async (err: Error) => {
+        logger.error("Error while creating a company: %s", err.message);
+      },
+      complete: () => logger.info("All companies are created"),
+    });
 
-    const getCompanies = (paginable?: Paginable): Observable<Company> => {
-      return from(userService.companies.getCompanies(paginable)).pipe(
-        mergeMap(companiesResult => {
-          const items$ = from(companiesResult.getEntities());
-          const next$ = companiesResult?.nextPage?.page_token
-            ? getCompanies(companiesResult.nextPage)
-            : EMPTY;
+    const usersSubscription = users$.subscribe({
+      next: user => logger.info("New user created - %s", user.destination.id),
+      error: async (err: Error) => {
+        logger.error("Error while creating user %s: ", err.message);
+        await tearDown();
+        logger.info("⛔️ Merge is not complete: some users may not have been imported %o", stats);
+      },
+      complete: async () => {
+        await tearDown();
+        logger.info("✅ Merge is complete: %o", stats);
+        console.log("COMPA", companies.length);
+      },
+    });
 
-          return concat(items$, next$);
-        }),
-        distinct((company: Company) => company.id),
-      );
-    };
-
-    const subscription = getCompanies()
-      .pipe(
-        tap(company => logger.info("Creating company in the Console", company.displayName)),
-        mergeMap(company => createCompany(company), concurrent),
-        tap(company => logger.info("Company created in the Console", company.destination.code)),
-        tap(() => {
-          // This is RXJS anti pattern, bit let's say that we can have others subscribing to this subject...
-          companyCreated.next(companyCreated.getValue() + 1);
-        }),
-        mergeMap(company =>
-          getUsers(company.source).pipe(
-            map(user => ({
-              user,
-              company,
-            })),
-          ),
-        ),
-        mergeMap(
-          userInCompany => createUser(userInCompany.company.source, userInCompany.user),
-          concurrent,
-        ),
-      )
-      .subscribe({
-        next: userInCompany => {
-          logger.info(
-            `Company ${userInCompany.source.company.id} - User ${userInCompany.source.user.id} has been imported`,
-          );
-          usersCreated.next(usersCreated.getValue() + 1);
-        },
-        error: err => console.log(err),
-        complete: () => {
-          subscription.unsubscribe();
-          usersCreated.complete();
-        },
-      });
-
-    return usersCreated
-      .toPromise()
-      .then(() => platform.stop())
-      .finally(() => {
-        logger.info(
-          `✅ Merge is complete: ${usersCreated.getValue()} users in ${companyCreated.getValue()} companies`,
-        );
-      });
+    async function tearDown() {
+      await platform.stop();
+      usersSubscription.unsubscribe();
+      companiesSubscription.unsubscribe();
+      countUsers.unsubscribe();
+      countCompanies.unsubscribe();
+    }
   },
 };
-
-class ConsoleClient {
-  private client: AxiosInstance;
-
-  constructor(private baseURL: string = "http://localhost:3000") {
-    this.client = axios.create({
-      baseURL,
-      timeout: 5000,
-    });
-  }
-
-  async createCompany(company: ConsoleCompany): Promise<ConsoleCompany> {
-    //return axios.post(`/companies/${company.code}`, company, {
-    //  headers: {
-    //    "Content-Type": "application/json",
-    //  }
-    //}).then(({data}) => data);
-
-    return new Promise(resolve => setTimeout(() => resolve(company), 500));
-  }
-
-  async createUser(user: ConsoleUser): Promise<ConsoleUser> {
-    return new Promise(resolve => setTimeout(() => resolve(user), 100));
-  }
-}
 
 export default command;
