10392a5dace740e8aabedaf2e68d20aabb81962e
Christophe HAMERLING <chamerling@linagora.com>
2021-03-08 16:17:15 +0100

    Add verbose option in CLI

--- twake/backend/node/src/cli/index.ts
+++ twake/backend/node/src/cli/index.ts
@@ -1,12 +1,24 @@
 import yargs from "yargs";
+import { logger } from "../core/platform/framework/logger";
 
-process.env.NODE_ENV = "CLI";
+process.env.NODE_ENV = "cli";
 
 yargs
   .strict()
   .usage("Usage: $0 <command> [options]")
+  .middleware([
+    argv => {
+      logger.level = argv.verbose ? "debug" : "fatal";
+    },
+  ])
   .commandDir("cmds", {
     visit: commandModule => commandModule.default,
+  })
+  .option("verbose", {
+    alias: "v",
+    default: false,
+    type: "boolean",
+    description: "Run with verbose logging",
   })
   .demandCommand(1, "Please supply a valid command")
   .alias("help", "h")
