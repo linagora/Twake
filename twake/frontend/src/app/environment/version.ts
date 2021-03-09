4898bbf06597dbaba53a7d88fe8cd3cdcf13f5f3
Romaric Mourgues <rmourgues@linagora.com>
2021-03-09 12:07:23 +0100

    Update software version number

--- twake/frontend/src/app/environment/version.ts
+++ twake/frontend/src/app/environment/version.ts
@@ -1,5 +1,5 @@
 export default {
   version: /* @VERSION */ '2021.Q1',
-  version_detail: /* @VERSION_DETAIL */ '2021.Q1.385',
+  version_detail: /* @VERSION_DETAIL */ '2021.Q1.401',
   version_name: /* @VERSION_NAME */ 'Albatros',
 };
