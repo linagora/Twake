f3f88889f6a58af1c27efece6eadf8f193d728cc
Romaric Mourgues <rmourgues@linagora.com>
2021-03-11 10:05:36 +0100

    Fix calls in event url format

--- twake/frontend/src/app/scenes/Apps/Calendar/Modals/EventCreation.js
+++ twake/frontend/src/app/scenes/Apps/Calendar/Modals/EventCreation.js
@@ -120,7 +120,9 @@ export default class EventCreation extends Component {
                   '//' +
                   window.location.host +
                   '/bundle/connectors/jitsi/call/twake_event_' +
-                  event.front_id,
+                  (event.front_id || '').replace(/-/g, '_') +
+                  '__' +
+                  (event.front_id || '').replace(/-/g, '_'),
               );
             }}
           >
