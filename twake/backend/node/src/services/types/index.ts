d7dfb9482c6cccfb3b4649367ab7501b114727f4
Romaric Mourgues <rmourgues@linagora.com>
2021-03-29 12:27:25 +0200

    Fix non mandatory type

--- twake/backend/node/src/services/types/index.ts
+++ twake/backend/node/src/services/types/index.ts
@@ -41,7 +41,7 @@ export interface User {
   // unique user id
   id: uuid;
   // unique console user id
-  identity_provider_id: uuid;
+  identity_provider_id?: uuid;
   // user email
   email?: string;
   // Organisation properties
