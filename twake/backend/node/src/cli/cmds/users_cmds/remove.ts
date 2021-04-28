944d63262bd742954952d681fc7507f6080196cb
Stéphane VIEIRA <svieira@linagora.com>
2021-04-28 16:43:33 +0200

    #1173 Clean fields and set deteled field to true

--- twake/backend/node/src/cli/cmds/users_cmds/remove.ts
+++ twake/backend/node/src/cli/cmds/users_cmds/remove.ts
@@ -50,7 +50,17 @@ const command: yargs.CommandModule<unknown, CLIArgs> = {
       // Table before
       tableBefore.push([user.id, user.username_canonical, user.deleted]);
 
-      user.username_canonical = `deleted-user-${user.id}`;
+      const partialId = user.id.toString().split("-")[0];
+
+      user.username_canonical = `deleted-user-${partialId}`;
+      user.email_canonical = `${partialId}@twake.removed`;
+      user.first_name = "";
+      user.last_name = "";
+      user.phone = "";
+      user.picture = "";
+      user.thumbnail_id = null;
+      user.status_icon = null;
+      user.deleted = true;
 
       await userService.users.save(user);
 
