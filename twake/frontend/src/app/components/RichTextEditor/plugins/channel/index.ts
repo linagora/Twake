cb1834835ace4e4e010bfdb53be1a77598ccef68
Christophe HAMERLING <chamerling@linagora.com>
2021-06-07 19:31:02 +0200

    Update the channel selection

--- twake/frontend/src/app/components/RichTextEditor/plugins/channel/index.ts
+++ twake/frontend/src/app/components/RichTextEditor/plugins/channel/index.ts
@@ -1,14 +1,15 @@
 import { ContentBlock, ContentState, EditorState, Modifier } from "draft-js";
+import { getSelectedBlock } from "draftjs-utils";
 import { Channel } from "./Channel";
 import ChannelsService from 'services/channels/channels';
 import { ChannelType, ChannelResource } from 'app/models/Channel';
-import { getInsertRange } from "../../EditorUtils";
 import ChannelSuggestion from "./ChannelSuggestion";
-import { EditorSuggestionPlugin } from "../../EditorPlugins";
+import { EditorSuggestionPlugin, SelectOrInsertOptions } from "../../EditorPlugins";
 
 export type ChannelSuggestionType = ChannelType & { autocomplete_id: number };
 
 export const ChannelResourceType = "CHANNEL";
+const CHANNEL_CHAR = "#";
 
 const findChannelEntities = (contentBlock: ContentBlock, callback: any, contentState: ContentState) => {
   contentBlock.findEntityRanges(
@@ -34,31 +35,55 @@ const resolver = (text: string, max: number, callback: (channels: ChannelSuggest
   });
 }
 
-const addChannel = (channel: ChannelSuggestionType, editorState: EditorState): EditorState => {
-  const { start, end } = getInsertRange(editorState, "#")
-  const contentState = editorState.getCurrentContent()
-  const currentSelection = editorState.getSelection()
-  const selection = currentSelection.merge({
-    anchorOffset: start,
-    focusOffset: end,
-  })
-  
-  const contentStateWithEntity = contentState.createEntity(ChannelResourceType, 'IMMUTABLE', channel);
-  const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+const addChannel = (channel: ChannelSuggestionType, editorState: EditorState, options: SelectOrInsertOptions): EditorState => {
+  let spaceAlreadyPresent = false;
   const channelAsString = (channel.name || "").toLocaleLowerCase().replace(/[^a-z0-9_\-.\u00C0-\u017F]/g, '');
+  const entityKey = editorState.getCurrentContent().createEntity(ChannelResourceType, 'IMMUTABLE', channel).getLastCreatedEntityKey();
+  const selectedBlock = getSelectedBlock(editorState);
+  const selectedBlockText = selectedBlock.getText();
+  let focusOffset = editorState.getSelection().getFocusOffset();
+  const channelIndex = (selectedBlockText.lastIndexOf(` ${CHANNEL_CHAR}`, focusOffset) || 0) + 1;
 
-  const newContentState = Modifier.replaceText(
-    contentStateWithEntity,
-    selection,
-    `#${channelAsString}`,
-    undefined,
-    entityKey);
+  if (selectedBlockText.length === channelIndex + 1) {
+    focusOffset = selectedBlockText.length;
+  }
 
-  const newEditorState = EditorState.push(
-    editorState, newContentState, "insert-fragment"
+  if (selectedBlockText[focusOffset] === ' ') {
+    spaceAlreadyPresent = true;
+  }
+
+  let updatedSelection = editorState.getSelection().merge({
+    anchorOffset: channelIndex,
+    focusOffset,
+  });
+  let newEditorState = EditorState.acceptSelection(editorState, updatedSelection);
+  let contentState = Modifier.replaceText(
+    newEditorState.getCurrentContent(),
+    updatedSelection,
+    `${CHANNEL_CHAR}${channelAsString}`,
+    newEditorState.getCurrentInlineStyle(),
+    entityKey,
   );
 
-  return EditorState.forceSelection( newEditorState, newContentState.getSelectionAfter());
+  newEditorState = EditorState.push(newEditorState, contentState, 'insert-characters');
+
+  if (!spaceAlreadyPresent && options.addSpaceAfter) {
+    // insert a blank space after mention
+    updatedSelection = newEditorState.getSelection().merge({
+      anchorOffset: channelIndex + channelAsString.length + CHANNEL_CHAR.length,
+      focusOffset: channelIndex + channelAsString.length + CHANNEL_CHAR.length,
+    });
+    newEditorState = EditorState.acceptSelection(newEditorState, updatedSelection);
+    contentState = Modifier.insertText(
+      newEditorState.getCurrentContent(),
+      updatedSelection,
+      ' ',
+      newEditorState.getCurrentInlineStyle(),
+      undefined,
+    );
+  }
+
+  return EditorState.push(newEditorState, contentState, 'insert-characters');
 };
 
 export default (options: { maxSuggestions: number } = { maxSuggestions: 10 }): EditorSuggestionPlugin<ChannelSuggestionType> => ({
@@ -69,6 +94,6 @@ export default (options: { maxSuggestions: number } = { maxSuggestions: 10 }): E
   },
   trigger: /\B#([a-zA-Z\u00C0-\u017F]+)$/,
   resourceType: ChannelResourceType,
-  onSelected: (channel: ChannelSuggestionType, editorState: EditorState) => addChannel(channel, editorState),
+  onSelected: (channel: ChannelSuggestionType, editorState: EditorState, options: SelectOrInsertOptions = { addSpaceAfter: true }) => addChannel(channel, editorState, options),
   renderSuggestion: (channel: ChannelSuggestionType) => ChannelSuggestion(channel),
 });
