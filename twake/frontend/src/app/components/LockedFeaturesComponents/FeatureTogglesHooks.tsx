993de3f9aa1eebfde26c260ce729ea82b3efeefb
Stéphane VIEIRA <svieira@linagora.com>
2021-06-15 10:43:12 +0200

    #1334 Add FeatureTogglesHooks

---
+++ twake/frontend/src/app/components/LockedFeaturesComponents/FeatureTogglesHooks.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import FeatureTogglesService from 'app/services/FeatureTogglesService';
+import Groups from 'services/workspaces/groups.js';
+import { FeatureToggles, Feature, withFeatures } from '@paralleldrive/react-feature-toggles';
+
+export const useFeatureToggles = () => {
+  const { activeFeatureNames } = FeatureTogglesService;
+  const companyId = Groups.currentGroupId;
+  const userGroups: { [key: string]: any } = Groups.user_groups;
+
+  Groups.useListener();
+  const companyPlan: { [key: string]: boolean } = userGroups[companyId].plan;
+
+  useEffect(() => {
+    companyPlan && FeatureTogglesService.setFeaturesFromCompanyPlan(companyPlan);
+  });
+
+  return {
+    activeFeatureNames,
+    FeatureToggles,
+    Feature,
+    withFeatures,
+  };
+};
