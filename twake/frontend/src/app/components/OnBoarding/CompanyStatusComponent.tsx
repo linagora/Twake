e55dedb33bf11c2327b042828ddd426d665f85a1
Stéphane VIEIRA <svieira@linagora.com>
2021-06-21 17:11:24 +0200

    #1334 Display BlockedCompanyPopup when there is billing status error

--- twake/frontend/src/app/components/OnBoarding/CompanyStatusComponent.tsx
+++ twake/frontend/src/app/components/OnBoarding/CompanyStatusComponent.tsx
@@ -5,6 +5,9 @@ import WelcomeOnTwake from './popups/WelcomeOnTwake';
 import DepreciatedCollections from 'app/services/Depreciated/Collections/Collections.js';
 import InitService from 'app/services/InitService';
 import RouterServices from 'app/services/RouterService';
+import Groups from 'services/workspaces/groups.js';
+import { CompanyType } from 'app/models/Company';
+import BlockedCompany from './popups/BlockedCompany';
 
 const CompanyStatusComponent = (): JSX.Element => {
   const { companyId, workspaceId } = RouterServices.getStateFromRoute();
@@ -16,6 +19,9 @@ const CompanyStatusComponent = (): JSX.Element => {
     if (InitService.server_infos?.configuration?.accounts?.type === 'console') {
       isNewAccount();
     }
+
+    isBlockedCompany();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const isNewCompany = (): boolean => {
@@ -42,6 +48,23 @@ const CompanyStatusComponent = (): JSX.Element => {
         position: 'center',
         size: { width: '600px' },
       });
+    }
+  };
+
+  const isBlockedCompany = () => {
+    if (!companyId) return;
+    const userGroups: { [key: string]: CompanyType } = Groups.user_groups;
+    const { plan } = userGroups[companyId];
+
+    if (plan?.billing?.status === 'error') {
+      return ModalManager.open(
+        <BlockedCompany />,
+        {
+          position: 'center',
+          size: { width: '600px' },
+        },
+        false,
+      );
     }
   };
 
