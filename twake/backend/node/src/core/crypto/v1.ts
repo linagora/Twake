4657f66e308acce8feb3b585ced4780ef7b91a24
Romaric Mourgues <rmourgues@linagora.com>
2021-06-25 23:42:38 +0200

    🔒 Upgrade crypto v1 service

--- twake/backend/node/src/core/crypto/v1.ts
+++ twake/backend/node/src/core/crypto/v1.ts
@@ -1,4 +1,4 @@
-import { createCipheriv, createDecipheriv, randomBytes } from "crypto";
+import { createCipheriv, createDecipheriv, createHash, randomBytes } from "crypto";
 import { CryptoResult } from ".";
 
 export default {
@@ -7,9 +7,10 @@ export default {
 };
 
 function encrypt(data: any, encryptionKey: any): CryptoResult {
+  const key = createHash("sha256").update(String(encryptionKey)).digest("base64").substr(0, 32);
   try {
     const iv = randomBytes(16);
-    const cipher = createCipheriv("aes-256-cbc", encryptionKey, iv);
+    const cipher = createCipheriv("aes-256-cbc", key, iv);
     const encrypted = Buffer.concat([cipher.update(JSON.stringify(data)), cipher.final()]).toString(
       "hex",
     );
@@ -27,8 +28,10 @@ function encrypt(data: any, encryptionKey: any): CryptoResult {
 }
 
 function decrypt(data: string, encryptionKey: any): CryptoResult {
+  const key = createHash("sha256").update(String(encryptionKey)).digest("base64").substr(0, 32);
+
   const encryptedArray = data.split(":");
-  
+
   if (!encryptedArray.length || encryptedArray.length !== 2) {
     return {
       data,
@@ -38,7 +41,7 @@ function decrypt(data: string, encryptionKey: any): CryptoResult {
 
   const iv = Buffer.from(encryptedArray[0], "hex");
   const encrypted = Buffer.from(encryptedArray[1], "hex");
-  const decipher = createDecipheriv("aes-256-cbc", encryptionKey, iv);
+  const decipher = createDecipheriv("aes-256-cbc", key, iv);
   const decrypt = JSON.parse(
     Buffer.concat([decipher.update(encrypted), decipher.final()]).toString(),
   );
