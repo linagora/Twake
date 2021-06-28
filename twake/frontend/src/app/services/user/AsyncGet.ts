2586e02aad24124ae48e9a66820603f8ebeac298
Christophe HAMERLING <chamerling@linagora.com>
2021-06-28 18:11:38 +0200

    Fix collection var name

--- twake/frontend/src/app/services/user/AsyncGet.ts
+++ twake/frontend/src/app/services/user/AsyncGet.ts
@@ -3,15 +3,15 @@ import UserAPIClient from "./UserAPIClient";
 import Collections from 'app/services/Depreciated/Collections/Collections';
 
 const currentUserRequests: { [key: string]: PromiseLike<UserType | null> } = {};
-const users_repository = Collections.get('users');
+const usersCollection = Collections.get('users');
 
 export default async (id: string): Promise<UserType | null> => {
   if (
-    users_repository.known_objects_by_id[id] &&
-    new Date(users_repository.known_objects_by_id[id]?._last_modified || 0).getTime() >
+    usersCollection.known_objects_by_id[id] &&
+    new Date(usersCollection.known_objects_by_id[id]?._last_modified || 0).getTime() >
       new Date().getTime() - 1000 * 60 * 60
   ) {
-    return users_repository.known_objects_by_id[id];
+    return usersCollection.known_objects_by_id[id];
   }
 
   if (currentUserRequests[id]) {
@@ -19,12 +19,12 @@ export default async (id: string): Promise<UserType | null> => {
     // FIXME: We should have a timeout
     await currentUserRequests[id];
     delete currentUserRequests[id];
-    return users_repository.known_objects_by_id[id];
+    return usersCollection.known_objects_by_id[id];
   } else {
     const userPromise = new Promise<UserType | null>(async resolve => {
       const users = await UserAPIClient._list([id]);
       const user = users.length ? users[0] : null;
-      user && users_repository.updateObject(user);
+      user && usersCollection.updateObject(user);
       resolve(user);
     });
     currentUserRequests[id] = userPromise;
