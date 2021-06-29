ee109e0b311a29bf36499a9a5eb80c26a10a9e50
Christophe HAMERLING <chamerling@linagora.com>
2021-06-29 11:48:32 +0200

    Rename CSS file

--- twake/frontend/src/app/components/UserCard/UserCard.tsx
+++ twake/frontend/src/app/components/UserCard/UserCard.tsx
@@ -5,7 +5,7 @@ import Languages from 'services/languages/languages';
 import User from 'components/User/User';
 import UserService from 'services/user/UserService';
 import { UserType } from 'app/models/User';
-import './User-Card.scss';
+import './UserCard.scss';
 
 type PropsType = {
   user: UserType;
