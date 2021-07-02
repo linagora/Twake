e8bc136bfa0b7fd4d4ee5720d055a6acea5f7ef7
Stéphane VIEIRA <svieira@linagora.com>
2021-07-02 16:49:01 +0200

    #429 lint TwakeService

--- twake/frontend/src/app/services/Decorators/TwakeService.ts
+++ twake/frontend/src/app/services/Decorators/TwakeService.ts
@@ -1,10 +1,10 @@
-import ServiceRegistry from "services/ServiceRegistry";
+import ServiceRegistry from 'services/ServiceRegistry';
 
 export function TwakeService(name: string): ClassDecorator {
   return function DecoratedTwakeService(target: any): any {
     const originalConstrutor = target;
 
-    const decorated: any = function(...args: any) {
+    const decorated: any = function (...args: any) {
       const newService = new originalConstrutor(...args);
 
       if (name) {
@@ -12,10 +12,13 @@ export function TwakeService(name: string): ClassDecorator {
         console.log(ServiceRegistry.services);
       }
       return newService;
-    }
-  
+    };
+
     decorated.prototype = originalConstrutor.prototype;
-    Object.keys(originalConstrutor).forEach((name: string) => { decorated[name] = (<any>originalConstrutor)[name]; });
+    Object.keys(originalConstrutor).forEach((name: string) => {
+      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+      decorated[name] = (<any>originalConstrutor)[name];
+    });
 
     return decorated;
   };
