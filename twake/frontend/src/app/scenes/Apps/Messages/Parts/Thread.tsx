d91ecd15276684c59ac33d2c1e1f024c74722660
Stéphane VIEIRA <svieira@linagora.com>
2021-07-15 11:07:41 +0200

    #1242 Implement file upload limitation for messages

--- twake/frontend/src/app/scenes/Apps/Messages/Parts/Thread.tsx
+++ twake/frontend/src/app/scenes/Apps/Messages/Parts/Thread.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { PureComponent, useEffect, useState } from 'react';
 import './Threads.scss';
 import Draggable from 'components/Draggable/Draggable.js';
 import UploadZone from 'components/Uploads/UploadZone.js';
 import Workspaces from 'services/workspaces/workspaces.js';
 import MessageEditorsManager from 'app/services/Apps/Messages/MessageEditorServiceFactory';
 import { Message } from 'app/models/Message';
+import classNames from 'classnames';
 
-type Props = {
+// FIX ME use real File type instead
+type FileType = { [key: string]: any };
+
+type PropsType = {
   collectionKey?: string;
   channelId?: string;
   threadId?: string;
@@ -23,78 +27,82 @@ type Props = {
   allowUpload?: boolean;
 };
 
-export default (props: Props) => (
-  <div
-    className={
-      'thread-container ' +
-      (props.loading ? 'loading ' : '') +
-      (props.hidden ? 'hidden ' : '') +
-      (props.highlighted ? 'highlighted ' : '') +
-      (props.className ? props.className + ' ' : '')
-    }
-    onClick={props.onClick}
-  >
-    {!!props.loading && (
-      <div className="thread-section">
-        <div className="message">
-          <div className="sender-space">
-            <div className="sender-head" />
-          </div>
-          <div className="message-content">
-            <div className="message-content-header">
-              <span className="sender-name"></span>
-            </div>
-            <div className="content-parent"></div>
-            <div className="content-parent" style={{ width: '40%' }}></div>
-          </div>
-        </div>
+const LoadingComponent = (
+  <div className="thread-section">
+    <div className="message">
+      <div className="sender-space">
+        <div className="sender-head" />
       </div>
-    )}
-    {!props.loading && (
-      <UploadZone
-        className="thread-centerer"
-        ref={node => {
-          MessageEditorsManager.get(
-            props.message?.channel_id || props.channelId || '',
-          ).setUploadZone(props.message?.id || '', node);
-        }}
-        disableClick
-        parent={''}
-        driveCollectionKey={props.collectionKey}
-        uploadOptions={{ workspace_id: Workspaces.currentWorkspaceId, detached: true }}
-        onUploaded={(file: any) => {
-          MessageEditorsManager.get(
-            props.message?.channel_id || props.channelId || '',
-          ).onAddAttachment(props.message?.id || '', file);
-        }}
-        onDragEnter={() =>
-          MessageEditorsManager.get(props.message?.channel_id || props.channelId || '').openEditor(
-            props.threadId || '',
-            '',
-            props.threadId ? '' : props.threadMain ? 'main' : '',
-          )
-        }
-        multiple={true}
-        allowPaste={true}
-        disabled={
-          !(
-            props.allowUpload ||
-            (props.message && !props.message?.parent_message_id && props.collectionKey)
-          )
-        }
-      >
-        <Draggable
-          dragHandler="js-drag-handler-message"
-          data={{ type: 'message', data: props.message }}
-          parentClassOnDrag="dragged"
-          onDragStart={(evt: any) => {}}
-          minMove={10}
-          className={'thread ' + (props.withBlock ? 'with-block ' : '')}
-          deactivated={!(props.canDrag && props.message)}
-        >
-          {props.children}
-        </Draggable>
-      </UploadZone>
-    )}
+      <div className="message-content">
+        <div className="message-content-header">
+          <span className="sender-name"></span>
+        </div>
+        <div className="content-parent"></div>
+        <div className="content-parent" style={{ width: '40%' }}></div>
+      </div>
+    </div>
   </div>
 );
+
+export default (props: PropsType) => {
+  const disabledUpload = !(
+    props.allowUpload ||
+    (props.message && !props.message?.parent_message_id && props.collectionKey)
+  );
+
+  const messageEditorService = MessageEditorsManager.get(
+    props.message?.channel_id || props.channelId || '',
+  );
+  const setUploadZoneRef = (node: UploadZone): void =>
+    messageEditorService.setUploadZone(props.message?.id || '', node);
+
+  const onDragEnter = (): void =>
+    messageEditorService.openEditor(
+      props.threadId || '',
+      '',
+      props.threadId ? '' : props.threadMain ? 'main' : '',
+    );
+
+  const onUploaded = (file: FileType) =>
+    messageEditorService.onAddAttachment(props.message?.id || '', file);
+
+  return (
+    <div
+      className={classNames(
+        'thread-container',
+        { loading: props.loading, hidden: props.hidden, highlighted: props.highlighted },
+        props.className,
+      )}
+      onClick={props.onClick}
+    >
+      {!props.loading ? (
+        <UploadZone
+          className="thread-centerer"
+          ref={setUploadZoneRef}
+          disableClick
+          parent={''}
+          driveCollectionKey={props.collectionKey}
+          uploadOptions={{ workspace_id: Workspaces.currentWorkspaceId, detached: true }}
+          onUploaded={onUploaded}
+          onDragEnter={onDragEnter}
+          multiple={true}
+          allowPaste={true}
+          disabled={disabledUpload}
+        >
+          <Draggable
+            className={classNames('thread', { 'with-block': props.withBlock })}
+            dragHandler="js-drag-handler-message"
+            data={{ type: 'message', data: props.message }}
+            parentClassOnDrag="dragged"
+            minMove={10}
+            deactivated={!(props.canDrag && props.message)}
+          >
+            {props.children}
+          </Draggable>
+        </UploadZone>
+      ) : (
+        LoadingComponent
+      )}
+    </div>
+  );
+};
