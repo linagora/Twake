5456ca084c85ea0300ecf8ff652b6bdcd94e5217
Roman Bykovskiy <8026787@gmail.com>
2021-08-23 15:46:14 +0300

    🛠 Resolved linagora/Twake#1467 (#1526)
    * Resolved linagora/Twake#1467
    
    company admin is now always a workspace admin for his company
    
    * company "owner" is admin of the workspace too
    
    * company "owner" is admin of the workspace too
    
    * company "owner" is admin of the workspace too
    
    * company "owner" is admin of the workspace too

--- twake/backend/node/src/services/workspaces/services/workspace/service.ts
+++ twake/backend/node/src/services/workspaces/services/workspace/service.ts
@@ -250,7 +250,6 @@ export class WorkspaceService implements WorkspaceServiceAPI {
     companyId: CompanyPrimaryKey,
   ): Promise<WorkspaceUser[]> {
     const allCompanyWorkspaces = await this.getAllForCompany(companyId.id);
-
     const UserWorkspaces = await Promise.all(
       allCompanyWorkspaces.map(workspace =>
         this.workspaceUserRepository.findOne({
