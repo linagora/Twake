35092975d71e27235effcd55a6cdc07fb479c2a2
tissarni <tissarni>
2021-09-02 17:29:29 +0200

    Fix format param for resize previews  + add counter service in default.json

--- twake/backend/node/src/services/previews/services/processing/image.ts
+++ twake/backend/node/src/services/previews/services/processing/image.ts
@@ -1,4 +1,4 @@
-import sharp from "sharp";
+import sharp, { Sharp } from "sharp";
 import { unlink } from "fs/promises";
 import { getTmpFile } from "../../utils";
 import { ThumbnailResult } from "../../types";
@@ -13,9 +13,13 @@ export async function generatePreview(
 
   for (const inputPath of inputPaths) {
     var result: sharp.OutputInfo;
+
     try {
       const outputPath = getTmpFile();
-      result = await sharp(inputPath).resize(300, 200).toFile(outputPath); //FIX ME: put in parameter the resize format
+      const inputMetadata = await sharp(inputPath).metadata();
+      const outputFormat = computeNewFormat(inputMetadata);
+
+      result = await sharp(inputPath).resize(outputFormat).toFile(outputPath);
       output.push({
         path: outputPath,
         width: result.width,
@@ -34,4 +38,17 @@ export async function generatePreview(
     output,
     done: true,
   };
+}
+
+function computeNewFormat(inputMetadata: sharp.Metadata): { width: number; height: number } {
+  const maxOutputWidth = 300;
+  const maxOutputHeight = 200;
+
+  const inputWidth = inputMetadata.width;
+  const inputHeight = inputMetadata.height;
+
+  const scale = Math.max(inputWidth / maxOutputWidth, inputHeight / maxOutputHeight);
+  let output = { width: Math.round(inputWidth / scale), height: Math.round(inputHeight / scale) };
+
+  return output;
 }
