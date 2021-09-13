b3795aff9a010aa3ee2f7b02cf8635f5922b95ab
Romaric Mourgues <rmourgues@linagora.com>
2021-09-13 12:00:59 +0200

    Update version to Q3.735

--- twake/backend/node/src/version.ts
+++ twake/backend/node/src/version.ts
@@ -1,7 +1,7 @@
 export default {
-  current: /* @VERSION_DETAIL */ "2021.Q3.640",
+  current: /* @VERSION_DETAIL */ "2021.Q3.735",
   minimal: {
-    web: /* @MIN_VERSION_WEB */ "2021.Q3.640",
-    mobile: /* @MIN_VERSION_MOBILE */ "2021.Q3.640",
+    web: /* @MIN_VERSION_WEB */ "2021.Q3.735",
+    mobile: /* @MIN_VERSION_MOBILE */ "2021.Q3.735",
   },
 };
--- twake/frontend/src/app/environment/version.ts
+++ twake/frontend/src/app/environment/version.ts
@@ -1,5 +1,5 @@
 export default {
   version: /* @VERSION */ '2021.Q3',
-  version_detail: /* @VERSION_DETAIL */ '2021.Q3.640',
+  version_detail: /* @VERSION_DETAIL */ '2021.Q3.735',
   version_name: /* @VERSION_NAME */ 'Albatros',
 };
