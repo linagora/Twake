5a69dfbc295e374452dc46bbde9ce38f4ee576b8
Romaric Mourgues <rmourgues@linagora.com>
2021-09-13 11:08:17 +0200

    Fake applications in channels getter

--- twake/backend/core/src/Twake/Channels/Controller/Channels.php
+++ twake/backend/core/src/Twake/Channels/Controller/Channels.php
@@ -10,8 +10,40 @@ class Channels extends BaseController
 {	
     public function getAction(Request $request)	
     {	
-        $options = $request->request->get("options");	
-        $objects = $this->get("app.channels.channels_system")->get($options, $this->getUser());	
+        $options = $request->request->get("options");
+
+        //This is only used now to get old "applications as channels" feature
+        //$objects = $this->get("app.channels.channels_system")->get($options, $this->getUser());	
+        $workspace = $this->get("app.workspaces")->get($options["workspace_id"]);
+        $apps = $this->get("app.group_apps")->getApps($workspace->getGroup());
+
+        $objects = [];
+        foreach($apps as $app){
+            $objects[] = [
+                "app_bot_identifier" =>"",
+                "app_group_id" =>"",
+                "app_id" =>$app->getId(),
+                "application" =>true,
+                "channel_group_name" =>"",
+                "connectors" =>[],
+                "description" =>"",
+                "direct" =>false,
+                "ext_members" =>[],
+                "front_id" => $app->getId(),
+                "icon" =>"",
+                "id" => $app->getId(),
+                "last_activity" =>1626706970,
+                "members" =>[$this->getUser()->getId()],
+                "members_count" => null,
+                "messages_increment" =>0,
+                "name" =>"",
+                "original_group" => $workspace->getGroup(),
+                "original_workspace" => $options["workspace_id"],
+                "private" =>false,
+                "tabs" =>[]
+            ];
+        }
+
         if ($objects === false) {	
             return new Response(Array("status" => "error"));	
         }	
