68d0210d9fac6996e12a9fc9196a7d386fa339a4
Christophe Hamerling <chamerling@linagora.com>
2021-09-17 16:31:46 +0200

    Rename client component

--- twake/frontend/src/app/scenes/App.tsx
+++ twake/frontend/src/app/scenes/App.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-use-before-define
 import React from 'react';
 
-import ClientPage from 'app/scenes/Client/Client';
+import Client from 'app/scenes/Client/Client';
 import InitService from 'app/services/InitService';
 
 export default () => {
@@ -11,5 +11,5 @@ export default () => {
     return <div />;
   }
 
-  return <ClientPage />;
+  return <Client />;
 };
