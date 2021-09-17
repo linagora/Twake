4872e9584c0351ab7dc38e01b0ea65d849acec6c
Christophe Hamerling <chamerling@linagora.com>
2021-09-17 16:24:48 +0200

    Add WorkspaceAPIClient

---
+++ twake/frontend/src/app/services/workspaces/WorkspaceAPIClient.ts
@@ -0,0 +1,29 @@
+import { CompanyType } from 'app/models/Company';
+import { WorkspaceType } from 'app/models/Workspace';
+import Api from '../Api';
+import { TwakeService } from '../Decorators/TwakeService';
+
+@TwakeService('WorkspaceAPIClientService')
+class WorkspaceAPIClient {
+
+  /**
+   * Get all workspaces for a company
+   *
+   * @param id
+   */
+  async list(companyId: string): Promise<WorkspaceType[]> {
+    return Api.get<{ resources: WorkspaceType[]}>(
+      `/internal/services/workspaces/v1/companies/${companyId}/workspaces`,
+    )
+    .then(result => result.resources && result.resources.length ? result.resources : []);
+  }
+
+  async listCompanies(userId: string | undefined): Promise<CompanyType[]> {
+    return Api.get<{ resources: CompanyType[]}>(
+      `/internal/services/users/v1/users/${userId}/companies`,
+    )
+    .then(result => result.resources);
+  }
+}
+
+export default new WorkspaceAPIClient();
