d8e6da9847e6ca469642016013c3138dda74d9b7
Christophe Hamerling <chamerling@linagora.com>
2021-09-24 15:29:00 +0200

    Add get Workspace function

--- twake/frontend/src/app/services/workspaces/WorkspaceAPIClient.ts
+++ twake/frontend/src/app/services/workspaces/WorkspaceAPIClient.ts
@@ -3,21 +3,44 @@ import { WorkspaceType } from 'app/models/Workspace';
 import Api from '../Api';
 import { TwakeService } from '../Decorators/TwakeService';
 
+const PREFIX = '/internal/services/workspaces/v1/companies';
+
 @TwakeService('WorkspaceAPIClientService')
 class WorkspaceAPIClient {
 
   /**
    * Get all workspaces for a company
    *
-   * @param id
+   * @param companyId
    */
   async list(companyId: string): Promise<WorkspaceType[]> {
     return Api.get<{ resources: WorkspaceType[]}>(
-      `/internal/services/workspaces/v1/companies/${companyId}/workspaces`,
+      `${PREFIX}/${companyId}/workspaces`,
     )
     .then(result => result.resources && result.resources.length ? result.resources : []);
   }
 
+  /**
+   * Get a workspace
+   *
+   * @param companyId
+   * @param workspaceId
+   * @returns
+   */
+  async get(companyId: string, workspaceId: string): Promise<WorkspaceType> {
+    return Api.get<{ resource: WorkspaceType}>(
+      `${PREFIX}/${companyId}/workspaces/${workspaceId}`,
+    )
+    .then(result => result.resource);
+
+  }
+
+  /**
+   * Get companies for a given user
+   *
+   * @param userId
+   * @returns
+   */
   async listCompanies(userId: string): Promise<CompanyType[]> {
     return Api.get<{ resources: CompanyType[]}>(
       `/internal/services/users/v1/users/${userId}/companies`,
