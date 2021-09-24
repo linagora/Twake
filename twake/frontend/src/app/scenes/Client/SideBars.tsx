6f78cb940131c521ba4a4fffd9e3ed121246c753
Christophe Hamerling <chamerling@linagora.com>
2021-09-24 15:24:56 +0200

    Cleanup sidebar

--- twake/frontend/src/app/scenes/Client/SideBars.tsx
+++ twake/frontend/src/app/scenes/Client/SideBars.tsx
@@ -1,16 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-use-before-define
+import React from 'react';
+import { Layout } from 'antd';
+
 import ChannelsBar from './ChannelsBar/ChannelsBar';
 import WorkspacesBar from './WorkspacesBar/WorkspacesBar';
 import RouterServices from 'app/services/RouterService';
-import React from 'react';
-import { Layout } from 'antd';
 
 export default () => {
   const { companyId, workspaceId } = RouterServices.getStateFromRoute();
 
   return (
     <Layout style={{ height: '100%', backgroundColor: 'var(--secondary)' }}>
-      <WorkspacesBar key={'workspacebar-' + companyId} />
-      <ChannelsBar key={'channelbar-' + workspaceId} />
+      <WorkspacesBar key={`workspacebar-${companyId}`} />
+      <ChannelsBar key={`channelbar-${workspaceId}`} />
     </Layout>
   );
 };
