462ed94945d311a81cd48c24bc2bcb53425f3a4f
Romaric Mourgues <rmourgues@linagora.com>
2021-10-05 18:19:47 +0200

    🐳  On-premise install fix (#1681)
    * Set container last compatible version
    
    * Tested working version

--- twake/start.sh
+++ twake/start.sh
@@ -3,9 +3,9 @@ cp -nR default-configuration/* configuration/
 
 docker-compose pull
 
-docker-compose up -d scylladb
+docker-compose up -d scylladb rabbitmq
 sleep 5m #Wait scylladb to startup
-docker-compose up -d php rabbitmq
+docker-compose up -d php
 sleep 10m #Wait php to create tables in scylladb
 
 docker-compose up -d
