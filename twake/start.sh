77aae4b10fff8877fbc496f1a178c6d2da2d0e7e
Romaric Mourgues <rmourgues@linagora.com>
2021-10-05 14:35:48 +0200

    Set container last compatible version

--- twake/start.sh
+++ twake/start.sh
@@ -5,7 +5,4 @@ docker-compose pull
 
 docker-compose up -d scylladb
 sleep 5m #Wait scylladb to startup
-docker-compose up -d php rabbitmq
-sleep 10m #Wait php to create tables in scylladb
-
 docker-compose up -d
