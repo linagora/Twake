3bb2897344b2e0e6e8c15ca8ca078ed5b86656cd
Fredy P <fpulido@linagora.com>
2021-10-21 10:28:06 -0400

    start_sh: ensures the script run and fail properly
    - adds shebang
    - set -e to exit if any line fails
    - only run as root (required to copy root owner directories and files)

--- twake/start.sh
+++ twake/start.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+set -e
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 cp -n docker-compose.onpremise.yml docker-compose.yml
 
 if [ ! -d ./configuration ]; then #create configuration folder
