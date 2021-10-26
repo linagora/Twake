f9758a49f09652ac8be9ca46627f952cb05e8a38
Christophe Hamerling <chamerling@linagora.com>
2021-10-26 15:45:28 +0200

    Update backend API

--- twake/backend/node/src/services/online/api.ts
+++ twake/backend/node/src/services/online/api.ts
@@ -1,11 +1,19 @@
 import { TwakeServiceProvider } from "../../core/platform/framework";
 
-export type UsersOnlineMessage = {
-  ids: Array<string>;
+export type OnlineTuple = [string, boolean];
+
+export type UsersOnlineMessage = Array<OnlineTuple>;
+
+export type OnlineGetRequest = {
+  /* Array of ids to get status */
+  data: Array<string>;
+};
+
+export type OnlineGetResponse = {
+  data: UsersOnlineMessage;
 };
 
 export interface OnlineServiceAPI extends TwakeServiceProvider {
   isOnline(userId: string): Promise<boolean>;
-  setOnline(userIds: Array<string>): Promise<void>;
-  setOffline(userIds: Array<string>): Promise<void>;
+  setLastSeenOnline(userIds: Array<string>, lastSeen: number): Promise<void>;
 }
