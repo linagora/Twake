db836d2eac3036bcefaefa5aabf163969636396e
Christophe Hamerling <chamerling@linagora.com>
2021-10-28 10:26:31 +0200

    Clean realtime hook

--- twake/frontend/src/app/services/Realtime/useRealtime.ts
+++ twake/frontend/src/app/services/Realtime/useRealtime.ts
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import useWebSocket from 'app/services/WebSocket/hooks/useWebSocket';
 import Logger from 'app/services/Logger';
-import { Maybe } from 'app/types';
 import { RealtimeEventAction } from './types';
 
 const logger = Logger.getLogger('useRealtimeRoom');
@@ -12,20 +11,21 @@ export type RealtimeRoomService<T> = {
   emit: (event: string, data: T) => void;
 };
 
-const useRealtimeRoom = <T>(roomName: Maybe<string>, tag: string, onEvent: (action: RealtimeEventAction, event: T) => void) => {
+const useRealtimeRoom = <T>(roomName: string, tagName: string, onEvent: (action: RealtimeEventAction, event: T) => void) => {
   const { websocket } = useWebSocket();
   const [lastEvent, setLastEvent] = useState<{ action: string; resource: T }>();
+  const [room] = useState(roomName);
+  const [tag] = useState(tagName);
 
   useEffect(() => {
-    // TODO: Do not subscribe/unsubscribe everytime
-    if (roomName && websocket) {
-      websocket.join(roomName, tag, (type: string, event: { action: RealtimeEventAction, resource: T }) => {
+    if (room && websocket) {
+      websocket.join(room, tag, (type: string, event: { action: RealtimeEventAction, resource: T }) => {
         logger.debug('Received WebSocket event', type, event);
         if (type === 'realtime:resource') {
           setLastEvent(event);
           onEvent(event.action, event.resource);
         } else if (type === 'realtime:join:success') {
-          logger.debug(`Room ${roomName} has been joined`);
+          logger.debug(`Room ${room} has been joined`);
         } else {
           logger.debug('Event type is not supported', type);
         }
@@ -33,11 +33,11 @@ const useRealtimeRoom = <T>(roomName: Maybe<string>, tag: string, onEvent: (acti
     }
 
     return () => {
-      logger.debug(`Destroy for room ${roomName}`);
+      logger.debug(`Destroy for room ${room}`);
+      websocket?.leave(room, tag);
     };
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [websocket, tag, roomName]);
+  }, [websocket, tag, room, onEvent]);
 
   return {
     lastEvent,
