d170e64b3441400f9f4062444b9665cbb6421f2c
romka <8026787@gmail.com>
2021-11-16 13:33:52 +0300

    move texts to localization module

--- twake/frontend/src/app/scenes/Join/Join.tsx
+++ twake/frontend/src/app/scenes/Join/Join.tsx
@@ -2,13 +2,11 @@
 import React, { useEffect, useState } from 'react';
 import { MagicLinksJoinService, MagicLinksJoinResponse } from 'services/MagicLinks/MagicLinks';
 import './Join.scss';
-import { Layout, Row, Col, Space, Typography, Button, Divider } from 'antd';
+import { Layout, Row, Col, Space, Typography, Button, Divider, message } from 'antd';
 import { useParams } from 'react-router-dom';
-import RouterService from 'app/services/RouterService';
-import Groups from 'services/workspaces/groups.js';
-import Workspaces from 'services/workspaces/workspaces.js';
 import WorkspaceAPIClient from 'services/workspaces/WorkspaceAPIClient';
 import WorkspacesService from 'services/workspaces/workspaces';
+import Languages from 'services/languages/languages';
 
 const { Title, Text } = Typography;
 
@@ -17,9 +15,14 @@ type PropsType = {
   [key: string]: any;
 };
 
+type JoinError = {
+  title: string,
+  description: string
+}
+
 export default (props: PropsType): JSX.Element => {
 
-  const [error, setError] = useState<string>();
+  const [error, setError] = useState<JoinError>();
   const [info, setInfo] = useState<MagicLinksJoinResponse>();
   const [busy, setBusy] = useState<boolean>(false);
 
@@ -27,7 +30,7 @@ export default (props: PropsType): JSX.Element => {
   let service = new MagicLinksJoinService(params.token, (val: boolean) => setBusy(val));
 
   useEffect(() => {
-    service.getInfo().then(info => setInfo(info)).catch(() => setError("This link is not available anymore"));
+    service.getInfo().then(info => setInfo(info)).catch(() => setError({title: Languages.t("scenes.join.wrong_link_title"), description: Languages.t("scenes.join.wrong_link_description")}));
   }, []);
 
 
@@ -39,10 +42,10 @@ export default (props: PropsType): JSX.Element => {
       document.location.href = info.auth_url;
     } else {
       service.join().then(resource => {
-        console.log("going to somewhere");
         setBusy(true);
         WorkspaceAPIClient.get(resource.company.id, resource.workspace.id)
           .then(workspace => {
+            WorkspacesService.addToUser(workspace); 
             WorkspacesService.select(workspace);
           });
 
@@ -58,26 +61,25 @@ export default (props: PropsType): JSX.Element => {
     <Layout.Content>
       <Row justify="center" align="middle" style={{ height: '100%' }}>
         <Col>
-          {(!error && !info) && <Text>Loading data</Text>}
+          {(!error && !info) && <Text>{Languages.t("scenes.join.loading")}</Text>}
 
           {error && <Space direction="vertical" align="center">
-            <Title>{error} <span role="img" aria-label="">✋</span></Title>
-            <Text>Ask the person who invited you to join this company for a new link or create your own company</Text>
+            <Title>{error.title} <span role="img" aria-label="">✋</span></Title>
+            <Text>{error.description}</Text>
             <Divider />
             <Button disabled={busy} loading={busy} type="primary" onClick={onCreateCompanyBtnClick}
-            >Create the company</Button>
+            >{Languages.t("scenes.join.create_the_company_button")}</Button>
           </Space>}
 
           {info && <Space direction="vertical" align="center">
-            <Title>Linagora</Title>
-            <Title>Join {info.workspace.name} from {info.company.name}! <span role="img" aria-label="">👋</span></Title>
-            <Text>Twake is an open-source digital workspace built to improve your team productivity</Text>
+            <Title>{Languages.t("scenes.join.join_workspace_from_company", [info.company.name, info.workspace.name])} <span role="img" aria-label="">👋</span></Title>
+            <Text>{Languages.t("scenes.join.twake_description")}</Text>
             <Divider />
-            { !info.auth_url ?  
-             <Button disabled={busy} loading={busy} type="primary" onClick={onJoinAccountBtnClick}>Join the team</Button>
-            :<Button disabled={busy} loading={busy} type="primary" className="gray-btn" onClick={onJoinAccountBtnClick}>Log in or create an account first</Button>
+            {info.auth_url
+              ? <Button disabled={busy} loading={busy} type="primary" className="gray-btn" onClick={onJoinAccountBtnClick}>{Languages.t("scenes.join.login_first_button")}</Button>
+              : <Button disabled={busy} loading={busy} type="primary" onClick={onJoinAccountBtnClick}>{Languages.t("scenes.join.join_the_team_button")}</Button>
             }
-            
+
           </Space>}
         </Col>
       </Row>
