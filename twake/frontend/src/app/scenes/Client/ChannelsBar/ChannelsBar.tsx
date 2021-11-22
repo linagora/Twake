339b42fc5aa385834335f85783f72721a83ecb8c
tissarni <tissarni>
2021-11-22 17:45:16 +0100

    Edit the loading channel bar with ant skeleton

--- twake/frontend/src/app/scenes/Client/ChannelsBar/ChannelsBar.tsx
+++ twake/frontend/src/app/scenes/Client/ChannelsBar/ChannelsBar.tsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-use-before-define
-import React, { useEffect } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import classNames from 'classnames';
 import PerfectScrollbar from 'react-perfect-scrollbar';
-import { Layout } from 'antd';
+import { Layout, Skeleton } from 'antd';
 
 import CurrentUser from './Parts/CurrentUser/CurrentUser';
-import { CompanyApplications } from './ChannelsApps/ChannelsApps';
+import { CompanyApplications} from './ChannelsApps/ChannelsApps';
 import ChannelsWorkspace from './ChannelsWorkspace/ChannelsWorkspace';
 import ChannelsUser from './ChannelsUser/ChannelsUser';
 import Footer from './Parts/Footer.js';
@@ -20,30 +20,17 @@ import ChannelsBarService from 'app/services/channels/ChannelsBarService';
 import AccessRightsService from 'app/services/AccessRightsService';
 import useRouterCompany from 'app/state/recoil/hooks/useRouterCompany';
 import useRouterWorkspace from 'app/state/recoil/hooks/useRouterWorkspace';
+import { LoadingCompany } from './Parts/CurrentUser/CompanyHeader/CompanyHeader';
 
 import './ChannelsBar.scss';
 
-
-
 export const LoadingChannels = () => {
   return (
-    <div style={{ paddingTop: 8 }}>
-      <div className="channel" />
-      <div className="channel" />
-      <div className="channel" />
-      <div className="channel_category" />
-      <div className="channel" />
-      <div className="channel" />
-      <div className="channel_category" />
-      <div className="channel" />
-      <div className="channel" />
-      <div className="channel" />
-      <div className="channel" />
-      <div className="channel_category" />
-      <div className="channel" />
-      <div className="channel" />
-      <div className="channel" />
-    </div>
+    <Layout.Sider theme="light" width={220} className={classNames('channels_view_loading')} style={{ height: '100%', width: "90%", alignItems: 'center' }}>
+      <LoadingCompany />
+      <Channel_loading />
+    </Layout.Sider> 
+    
   );
 };
 
@@ -52,9 +39,9 @@ export default () => {
   const workspaceId = useRouterWorkspace(); 
 
   //Need to be fix, here ready always == true
-  const ready = true || ChannelsBarService.useWatcher(() => {
+  const ready = ChannelsBarService.useWatcher(async () => {
     return (
-      ChannelsBarService.isReady(companyId, workspaceId) &&
+      await ChannelsBarService.isReady(companyId, workspaceId) &&
       //ChannelsBarService.isReady(companyId, workspaceId, ['applications']) &&
       ChannelsBarService.isReady(companyId, 'direct')
     );
@@ -100,24 +87,37 @@ export default () => {
       <CurrentUser />
 
       {!ready && <LoadingChannels />}
-
-      <ScrollWithHiddenComponents
-        disabled={!ready}
-        tag="channel_bar_component"
-        scrollTopComponent={<HiddenNotificationsButton position="top" type="important" />}
-        scrollBottomComponent={<HiddenNotificationsButton position="bottom" type="important" />}
-      >
-        <PerfectScrollbar options={{ suppressScrollX: true }}>
-          <React.Suspense fallback={<></>}>
-            <CompanyApplications companyId={companyId} />
-          </React.Suspense>
-          <ChannelsWorkspace key={`workspace_chans_${workspaceId}`} />
-          <ChannelsUser key={companyId} />
-          {AccessRightsService.hasLevel(workspaceId, 'moderator') &&
-            Workspaces.getCurrentWorkspace().stats.total_members <= 5 && <AddUserButton />}
-        </PerfectScrollbar>
-      </ScrollWithHiddenComponents>
-      <Footer />
+        <ScrollWithHiddenComponents
+          disabled={!ready}
+          tag="channel_bar_component"
+          scrollTopComponent={<HiddenNotificationsButton position="top" type="important" />}
+          scrollBottomComponent={<HiddenNotificationsButton position="bottom" type="important" />}
+        >
+          <PerfectScrollbar options={{ suppressScrollX: true }}>
+            <Suspense fallback={<></>}>
+              <CompanyApplications companyId={companyId} />
+            </Suspense>
+            <ChannelsWorkspace key={`workspace_chans_${workspaceId}`} />
+            <ChannelsUser key={companyId} />
+            {AccessRightsService.hasLevel(workspaceId, 'moderator') &&
+              Workspaces.getCurrentWorkspace().stats.total_members <= 5 && <AddUserButton />}
+          </PerfectScrollbar>
+        </ScrollWithHiddenComponents>
+        <Footer />
     </Layout.Sider>
   );
 };
+
+export const Channel_loading = () => {
+  return (
+    <div className="channels_view_loader ">
+      <div className="applications_channels_loader small-x-margin">
+        <Skeleton title={false} paragraph={{rows : 3, width:["45%", "40%","50%"]}}/>
+      </div>
+      <div className="channels_loader small-x-margin">
+          <Skeleton title={{style:{height:22} , width:"45%"}} paragraph={{rows:4, width:["100%", "80%","90%","100"]}}/>
+          <Skeleton title={{style:{height:22}, width:"55%"}} paragraph={{rows:4, width:["60%", "90%","100%", "85%"]}}/>
+      </div>
+    </div>
+  );
+}
--- twake/frontend/src/app/scenes/Client/ChannelsBar/Parts/CurrentUser/CompanyHeader/CompanyHeader.tsx
+++ twake/frontend/src/app/scenes/Client/ChannelsBar/Parts/CurrentUser/CompanyHeader/CompanyHeader.tsx
@@ -1,11 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-use-before-define
 import React from 'react';
+import { Skeleton } from 'antd';
 
 import UserService from 'services/user/UserService';
 import Icon from 'components/Icon/Icon.js';
 import Emojione from 'components/Emojione/Emojione';
 import NotificationDelay from '../Notifications/NotificationDelay';
 import useCurrentUser from 'app/services/user/hooks/useCurrentUser';
+import { SkeletonTitleProps } from 'antd/lib/skeleton/Title';
+import { SkeletonParagraphProps } from 'antd/lib/skeleton/Paragraph';
 
 import './CompanyHeader.scss';
 
@@ -44,6 +47,19 @@ export default (props: PropsType): JSX.Element => {
       </div>
       <div className="notifications">
         <NotificationDelay />
+      </div>
+    </div>
+  );
+};
+
+export const LoadingCompany = () => {
+  const workspace_name_loading : SkeletonTitleProps = {style: {height:28}, width: "40%"}
+  const user_mail_loading : SkeletonParagraphProps = {rows: 1, width: "100%"}
+  return (
+    <div className="current_company_header_loader ">
+      <div className="current_company_loader small-x-margin">
+        <Skeleton title={workspace_name_loading} paragraph={false}/>
+        <Skeleton title={false} paragraph={user_mail_loading}/>  
       </div>
     </div>
   );
