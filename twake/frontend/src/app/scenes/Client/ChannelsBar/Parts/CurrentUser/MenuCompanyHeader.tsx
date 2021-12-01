136a34e205bb023690680653e98e9ab6a8eea412
Romaric Mourgues <rmourgues@linagora.com>
2021-12-01 19:15:05 +0100

    Fix top left menu

--- twake/frontend/src/app/scenes/Client/ChannelsBar/Parts/CurrentUser/MenuCompanyHeader.tsx
+++ twake/frontend/src/app/scenes/Client/ChannelsBar/Parts/CurrentUser/MenuCompanyHeader.tsx
@@ -1,4 +1,5 @@
 import environment from 'app/environment/environment';
+import { CurrentCompanyLogo } from 'app/scenes/Client/WorkspacesBar/Components/CompanySelector';
 import { useCurrentCompany } from 'app/state/recoil/hooks/useCompanies';
 import { useCurrentWorkspace } from 'app/state/recoil/hooks/useWorkspaces';
 import React from 'react';
@@ -8,14 +9,21 @@ export default () => {
   const { workspace } = useCurrentWorkspace();
 
   return (
-    <>
-      <b>
-        {company.name} - {workspace?.name}
-      </b>
-      <br />
-      <span style={{ opacity: 0.5 }}>{environment.api_root_url}</span>
-      <br />
-      <span className="link">Plan {company.plan?.name}</span>
-    </>
+    <div style={{ display: 'flex', flexDirection: 'row', height: 48, alignItems: 'start' }}>
+      <div style={{ width: 40, height: 32 }}>
+        <CurrentCompanyLogo size={32} />
+      </div>
+      <div style={{ flex: 1, lineHeight: '1.2em' }}>
+        <b style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+          {company.name} - {workspace?.name}
+        </b>
+        <br />
+        <span style={{ opacity: 0.5 }}>
+          {environment.api_root_url.split('//').pop()?.split('/').shift()}
+        </span>
+        <br />
+        <span className="link">Plan {company.plan?.name}</span>
+      </div>
+    </div>
   );
 };
