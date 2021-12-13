dbb8ef227371aec1b9b5ea1193bdbace0e89d61c
tissarni <tissarni>
2021-12-13 16:36:04 +0100

    clean after fisrt review

--- twake/frontend/src/app/scenes/Client/ChannelsBar/Parts/CurrentUser/SaveNewStatus.tsx
+++ twake/frontend/src/app/scenes/Client/ChannelsBar/Parts/CurrentUser/SaveNewStatus.tsx
@@ -1,42 +1,29 @@
-import React, { useEffect, useCallback, useState, useRef } from 'react';
+import React, { useCallback, useState, useRef } from 'react';
 import { Button } from 'antd';
+import classNames from 'classnames';
 
 import Languages from 'services/languages/languages';
 import { useCurrentUser } from 'app/state/recoil/hooks/useCurrentUser';
 import MenusManager from 'app/components/Menus/MenusManager';
 import InputWithIcon from 'app/components/Inputs/InputWithIcon';
-import classNames from 'classnames';
 
 import './SaveNewStatus.scss';
 
 type PropsType = {
-  level: any;
+  level: number;
 };
 
-export default (props: PropsType): JSX.Element => {
+export default ({ level }: PropsType): JSX.Element => {
   const { user, updateStatus } = useCurrentUser();
   const icon = user?.status?.split(' ')[0] || '';
   const text = user?.status?.split(' ').slice(1).join(' ') || '';
   const [status, setStatus] = useState([icon, text]);
-  const toto = useRef(status);
+  const statusRef = useRef(status);
 
   const save = useCallback(() => {
     updateStatus(status);
     MenusManager.closeMenu();
   }, [status, updateStatus]);
-
-  useEffect(() => {
-    const listener = (event: { code: string; preventDefault: () => void }) => {
-      if (event.code === 'Enter' || event.code === 'NumpadEnter') {
-        console.log('Enter key was pressed. Run your function.');
-        save();
-      }
-    };
-    document.addEventListener('keydown', listener);
-    return () => {
-      document.removeEventListener('keydown', listener);
-    };
-  }, [save]);
 
   const reset = useCallback(() => {
     updateStatus(['', '']);
@@ -51,7 +38,7 @@ export default (props: PropsType): JSX.Element => {
     >
       <InputWithIcon
         focusOnDidMount
-        menu_level={props.level}
+        menu_level={level}
         placeholder={Languages.t(
           'scenes.app.popup.appsparameters.pages.status_tilte',
           [],
@@ -62,15 +49,15 @@ export default (props: PropsType): JSX.Element => {
           setStatus(value);
         }}
       />
-      <div className="save-new-status-container">
+      <div className="save-new-status-container" tabIndex={0}>
         {
           <Button
-            className={classNames({ 'error-button': toto.current === status })}
-            type={toto.current === status ? 'default' : 'ghost'}
-            onClick={toto.current === status ? reset : save}
+            className={classNames({ 'error-button': statusRef.current === status })}
+            type={statusRef.current === status ? 'default' : 'ghost'}
+            onClick={statusRef.current === status ? reset : save}
           >
             {Languages.t(
-              toto.current === status
+              statusRef.current === status
                 ? 'scenes.app.channelsbar.currentuser.reset'
                 : 'scenes.app.channelsbar.currentuser.update',
             )}
