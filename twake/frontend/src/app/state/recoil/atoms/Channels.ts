7d8bb8e9203f544f7562c94064f5679baf22d4f8
Stéphane VIEIRA <svieira@linagora.com>
2022-01-04 15:34:37 +0100

    #1864 Recoil part 1

--- twake/frontend/src/app/state/recoil/atoms/Channels.ts
+++ twake/frontend/src/app/state/recoil/atoms/Channels.ts
@@ -1,8 +1,19 @@
-import { atomFamily } from 'recoil';
+import { atom, atomFamily } from 'recoil';
 import _ from 'lodash';
+
 import { ChannelType } from 'app/models/Channel';
 
 export const ChannelState = atomFamily<ChannelType, string>({
   key: 'ChannelState',
   default: id => ({ id } as ChannelType),
+});
+
+export const ChannelsMineState = atom<ChannelType[]>({
+  key: 'ChannelsMineState',
+  default: [],
+});
+
+export const ChannelsReachableState = atom<ChannelType[]>({
+  key: 'ChannelsReachableState',
+  default: [],
 });
--- twake/frontend/src/app/state/recoil/hooks/useChannels.ts
+++ twake/frontend/src/app/state/recoil/hooks/useChannels.ts
@@ -1,6 +1,12 @@
-import { useRecoilCallback, useRecoilValue } from 'recoil';
+import { useEffect } from 'react';
+import { useRecoilCallback, useRecoilState, useRecoilValue } from 'recoil';
+
 import { ChannelType } from 'app/models/Channel';
-import { ChannelState } from '../atoms/Channels';
+import { ChannelsMineState, ChannelsReachableState, ChannelState } from '../atoms/Channels';
+import useRouterWorkspace from './router/useRouterWorkspace';
+import useRouterCompany from './router/useRouterCompany';
+import ChannelsMineAPIClient from 'app/services/channels/ChannelsMineAPIClient';
+import ChannelsReachableAPIClient from 'app/services/channels/ChannelsReachableAPIClient';
 
 export function useChannel(channelId: string) {
   const channel = useRecoilValue(ChannelState(channelId));
@@ -13,4 +19,40 @@ export function useSetChannel() {
     if (channel.id) set(ChannelState(channel.id), channel);
   });
   return { set };
+}
+
+export function useChannels(): {
+  mine: ChannelType[];
+  reachable: ChannelType[];
+  refresh: (mine?: boolean, reachable?: boolean) => void;
+} {
+  const companyId = useRouterCompany();
+  const workspaceId = useRouterWorkspace();
+  const [mine, setMine] = useRecoilState(ChannelsMineState);
+  const [reachable, setReachable] = useRecoilState(ChannelsReachableState);
+
+  const refreshMine = async () => {
+    const res = await ChannelsMineAPIClient.get(companyId, workspaceId);
+
+    if (res) setMine(res);
+  };
+
+  const refreshReachable = async () => {
+    const res = await ChannelsReachableAPIClient.get(companyId, workspaceId);
+
+    if (res) setReachable(res);
+  };
+
+  const refresh = (mine: boolean = true, reachable: boolean = true) => {
+    if (mine) refreshMine();
+    if (reachable) refreshReachable();
+  };
+
+  useEffect(() => {
+    refresh();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { mine, reachable, refresh };
 }
