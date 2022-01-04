38382e29d67d04f5909609b9a5397f5508cd046c
Stéphane VIEIRA <svieira@linagora.com>
2022-01-04 12:41:15 +0100

    #1864 Add ChannelsReachableAPIClient

---
+++ twake/frontend/src/app/services/channels/ChannelsReachableAPIClient.ts
@@ -0,0 +1,23 @@
+import Api from '../Api';
+import { ChannelType } from 'app/models/Channel';
+import { TwakeService } from '../Decorators/TwakeService';
+
+type ChannelsReachableGetResponse = { resources: ChannelType[] };
+
+@TwakeService('ChannelsReachableAPIClientService')
+class ChannelsReachableAPIClient {
+  private readonly prefix = '/internal/services/channels/v1/companies';
+
+  /**
+   * @param companyId
+   * @param workspaceId
+   * @return channels that user is not a member but could join
+   */
+  async get(companyId: string, workspaceId: string): Promise<ChannelType[]> {
+    return Api.get<ChannelsReachableGetResponse>(
+      `${this.prefix}/${companyId}/workspaces/${workspaceId}/channels?include_users=false`,
+    ).then(result => result.resources);
+  }
+}
+
+export default new ChannelsReachableAPIClient();
