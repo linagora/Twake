e248eb14453e85ff5499d398a9291f899274225f
Romaric Mourgues <rmourgues@linagora.com>
2022-01-10 11:46:46 +0100

    Fix console hooks

--- twake/backend/node/src/services/console/web/schemas.ts
+++ twake/backend/node/src/services/console/web/schemas.ts
@@ -4,7 +4,7 @@ export const consoleHookSchema = {
     properties: {
       secret_key: { type: "string" },
     },
-    required: ["secret_key"],
+    required: [""],
   },
   body: {
     type: "object",
@@ -12,6 +12,7 @@ export const consoleHookSchema = {
       type: { type: "string" },
       content: { type: "object" },
       signature: { type: "string" },
+      secret_key: { type: "string" },
     },
     required: ["type", "content"],
   },
