4fef4214afc6a81c7710bab3a83c9e2c71d5c6be
Romaric Mourgues <rmourgues@linagora.com>
2022-02-02 17:46:48 +0100

    Fix missing errors in logs

--- twake/backend/node/src/core/platform/services/webserver/error.ts
+++ twake/backend/node/src/core/platform/services/webserver/error.ts
@@ -2,8 +2,14 @@ import { FastifyInstance } from "fastify";
 
 function serverErrorHandler(server: FastifyInstance): void {
   server.setErrorHandler(async (err, request, reply) => {
-    server.log.debug(err.toString());
-    await reply.status(500).send(err);
+    console.error(`Got 500 error on request ${request.id} : `, err);
+    server.log.debug(`Got 500 error on request ${request.id} : ${err.toString()}`);
+    await reply.status(500).send({
+      statusCode: 500,
+      error: "Internal Server Error",
+      message: "Something went wrong",
+      requestId: request.id,
+    });
   });
 }
 
