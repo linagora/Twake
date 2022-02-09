c53a7b9943b530c1e4f989dc52ec4fce32184a28
Romaric Mourgues <rmourgues@linagora.com>
2022-02-09 17:20:07 +0100

    Keep original status code for errors

--- twake/backend/node/src/core/platform/services/webserver/error.ts
+++ twake/backend/node/src/core/platform/services/webserver/error.ts
@@ -2,14 +2,16 @@ import { FastifyInstance } from "fastify";
 
 function serverErrorHandler(server: FastifyInstance): void {
   server.setErrorHandler(async (err, request, reply) => {
-    console.error(`Got 500 error on request ${request.id} : `, err);
-    server.log.debug(`Got 500 error on request ${request.id} : ${err.toString()}`);
-    await reply.status(500).send({
-      statusCode: 500,
-      error: "Internal Server Error",
-      message: "Something went wrong",
-      requestId: request.id,
-    });
+    console.error(`Got ${reply.statusCode} error on request ${request.id} : `, err);
+    server.log.debug(`Got ${reply.statusCode} error on request ${request.id} : ${err.toString()}`);
+    if (reply.statusCode === 500) {
+      await reply.status(500).send({
+        statusCode: 500,
+        error: "Internal Server Error",
+        message: "Something went wrong",
+        requestId: request.id,
+      });
+    }
   });
 }
 
