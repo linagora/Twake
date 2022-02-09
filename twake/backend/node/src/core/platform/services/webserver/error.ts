10b252d18ffc704a8ec67aa8463927473e476319
Romaric Mourgues <rmourgues@linagora.com>
2022-02-09 20:53:07 +0100

    Fix serverErrorHandler

--- twake/backend/node/src/core/platform/services/webserver/error.ts
+++ twake/backend/node/src/core/platform/services/webserver/error.ts
@@ -4,14 +4,16 @@ function serverErrorHandler(server: FastifyInstance): void {
   server.setErrorHandler(async (err, request, reply) => {
     console.error(`Got ${reply.statusCode} error on request ${request.id} : `, err);
     server.log.debug(`Got ${reply.statusCode} error on request ${request.id} : ${err.toString()}`);
-    if (reply.statusCode === 500) {
-      await reply.status(500).send({
-        statusCode: 500,
-        error: "Internal Server Error",
-        message: "Something went wrong",
-        requestId: request.id,
-      });
-    }
+    await reply.send(
+      reply.statusCode == 500
+        ? {
+            statusCode: reply.statusCode,
+            error: "Internal Server Error",
+            message: "Something went wrong",
+            requestId: request.id,
+          }
+        : err,
+    );
   });
 }
 
