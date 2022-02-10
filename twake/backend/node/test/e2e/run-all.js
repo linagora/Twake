fb76747c822eed593633f1786e90ca164798fac6
Romaric Mourgues <rmourgues@linagora.com>
2022-02-10 14:35:25 +0100

    Test again all of this

--- twake/backend/node/test/e2e/run-all.js
+++ twake/backend/node/test/e2e/run-all.js
@@ -49,11 +49,25 @@ srcFiles = srcFiles.filter(p => p.indexOf(".spec.ts") >= 0 || p.indexOf(".test.t
   let failed = 0;
   let passed = 0;
 
-  for (const path of srcFiles) {
+  for (const path of [
+    "test/e2e/workspaces/workspaces.invite-tokens.spec.ts",
+    "test/e2e/workspaces/workspaces.invite-tokens.spec.ts",
+    "test/e2e/workspaces/workspaces.invite-tokens.spec.ts",
+    "test/e2e/workspaces/workspaces.invite-tokens.spec.ts",
+    "test/e2e/workspaces/workspaces.invite-tokens.spec.ts",
+    "test/e2e/workspaces/workspaces.invite-tokens.spec.ts",
+    "test/e2e/workspaces/workspace-users.spec.ts",
+    "test/e2e/workspaces/workspace-users.spec.ts",
+    "test/e2e/workspaces/workspace-users.spec.ts",
+    "test/e2e/workspaces/workspace-users.spec.ts",
+    "test/e2e/workspaces/workspace-users.spec.ts",
+    "test/e2e/workspaces/workspace-users.spec.ts",
+  ]) {
     const testName = `test/e2e/${path.split("test/e2e/")[1]}`;
     const args = `${testName} --forceExit --coverage --detectOpenHandles --runInBand --testTimeout=60000 --verbose=true`;
+
     try {
-      const out = await exec("jest", args.split(" "));
+      const out = await exec("jest", args.split(" "), true);
       if (out.code !== 0) {
         //To get all the logs, we run it again
         console.log(`FAIL ${testName}`);
