90851120f9de6fce653a9a89ffb659ec96d24b7d
Romaric Mourgues <rmourgues@linagora.com>
2022-05-31 15:55:38 +0200

    🛠 Fix workspace user search (#2205)

--- twake/frontend/src/app/features/users/types/user.ts
+++ twake/frontend/src/app/features/users/types/user.ts
@@ -31,6 +31,9 @@ export type UserType = {
   groups_id?: string[];
   id?: string;
   identity_provider?: string;
+  provider_id?: string;
+  provider?: string;
+  last_activity?: number;
   isNew?: boolean;
   isRobot?: boolean;
   language?: string;
@@ -49,6 +52,7 @@ export type UserType = {
   username: string;
   companies?: UserCompanyType[];
   preferences: UserPreferencesType;
+  preference?: UserPreferencesType;
 
   /**
    * this field is filled when available and so we cannot rely on it except on search service for filtering
