3c21c8392742fa389452d650171eddfa2bf27227
Khaled FERJANI <kferjani@linagora.com>
2022-06-14 14:34:58 +0100

    unit tests #2278

---
+++ twake/backend/node/test/unit/services/previews/services/links/processing/images.test.ts
@@ -0,0 +1,62 @@
+import { describe, expect, it, jest, beforeEach, afterEach, afterAll } from "@jest/globals";
+import { generateImageUrlPreview } from "../../../../../../../src/services/previews/services/links/processing/image";
+
+import getFavicons from "get-website-favicon";
+import imageProbe from "probe-image-size";
+
+jest.mock("get-website-favicon");
+jest.mock("probe-image-size");
+
+beforeEach(() => {
+  (imageProbe as any).mockImplementation(() => ({
+    width: 320,
+    height: 240,
+  }));
+
+  (getFavicons as any).mockImplementation(() => ({
+    icons: [
+      {
+        src: "http://foo.bar/favicon.ico",
+      },
+    ],
+  }));
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+afterAll(() => {
+  jest.restoreAllMocks();
+});
+
+describe("the generateImageUrlPreview function", () => {
+  it("should return a promise", () => {
+    const result = generateImageUrlPreview("http://foo.bar");
+    expect(result).toBeInstanceOf(Promise);
+  });
+
+  it("should generate a preview for a given image url", async () => {
+    const result = await generateImageUrlPreview("https://foo.bar/image.jpg");
+    expect(result).toEqual({
+      title: "image.jpg", // title should be the file name
+      domain: "foo.bar",
+      favicon: "http://foo.bar/favicon.ico",
+      url: "https://foo.bar/image.jpg",
+      img_height: 320,
+      img_width: 240,
+      description: null,
+      img: "https://foo.bar/image.jpg",
+    });
+  });
+
+  // should return nothing in case of error
+  it("should return nothing in case of error", async () => {
+    (imageProbe as any).mockImplementation(() => {
+      throw new Error("failed to probe image");
+    });
+
+    const result = await generateImageUrlPreview("https://foo.bar/image.jpg");
+    expect(result).toBeUndefined();
+  });
+});
