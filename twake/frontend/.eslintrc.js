47906ae854ba06fcff3d3cd0a61ce6ced2eac080
Romaric Mourgues <rmourgues@linagora.com>
2022-06-14 14:41:15 +0200

    Add storybook

---
+++ twake/frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  ignorePatterns: ['**/deprecated/*.js'],
+  rules: {
+    'react/display-name': 'off',
+    '@typescript-eslint/ban-types': 'off',
+  },
+};
--- twake/frontend/.storybook/addons.js
+++
@@ -1,8 +0,0 @@
-import '@storybook/addon-actions/register';
-import '@storybook/addon-links/register';
-import '@storybook/addon-knobs/register';
-import '@storybook/addon-storysource/register';
-import '@storybook/addon-options/register';
-import '@storybook/addon-notes/register';
-import '@dump247/storybook-state/register';
-import 'storybook-addon-specifications/register';
