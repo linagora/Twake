02d549a6d31e559b89a72142177a3a444f423345
Khaled FERJANI <kferjani@linagora.com>
2022-06-16 14:48:25 +0100

    🛠 Fixed link preview not being responsive for mobile

--- twake/frontend/src/app/views/applications/messages/message/parts/LinkPreview.tsx
+++ twake/frontend/src/app/views/applications/messages/message/parts/LinkPreview.tsx
@@ -5,7 +5,6 @@ import { useMessage } from 'app/features/messages/hooks/use-message';
 import User from 'app/features/users/services/current-user-service';
 import { MessageContext } from '../message-with-replies';
 import { X } from 'react-feather';
-import { Col, Row } from 'antd';
 
 type PropsType = {
   preview: MessageLinkType;
@@ -16,45 +15,46 @@ export default ({ preview }: PropsType): React.ReactElement => {
   const { deleteLinkPreview, message } = useMessage(context);
 
   return (
-    <Row>
-      <Col>
-        <div className="ant-card ant-card-bordered ant-card-small ant-card-type-inner link-preview">
-          {message.user_id === User.getCurrentUserId() ? (
-            <div className="delete-link-preview">
-              <X size={16} onClick={() => deleteLinkPreview(preview.url)} />
+    <div className="xs:max-w-xs max-w-md ant-card ant-card-bordered ant-card-small ant-card-type-inner link-preview">
+      {message.user_id === User.getCurrentUserId() ? (
+        <div className="delete-link-preview">
+          <X size={16} onClick={() => deleteLinkPreview(preview.url)} />
+        </div>
+      ) : null}
+      <div className="ant-card-body">
+        <div className="ant-card-meta">
+          <div className="ant-card-meta-detail">
+            <div className="ant-card-meta-avatar">
+              {preview.favicon && (
+                <span className="ant-avatar ant-avatar-circle ant-avatar-image">
+                  <img alt={preview.domain} src={preview.favicon} />
+                </span>
+              )}
+              <span className="link-preview-domain">{preview.domain}</span>
             </div>
-           ) : null}
-          <div className="ant-card-body">
-            <div className="ant-card-meta">
-              <div className="ant-card-meta-detail">
-                <div className="ant-card-meta-avatar">
-                  {preview.favicon && (
-                    <span className="ant-avatar ant-avatar-circle ant-avatar-image">
-                      <img alt={preview.domain} src={preview.favicon} />
-                    </span>
-                  )}
-                  <span className="link-preview-domain">{preview.domain}</span>
-                </div>
-                <div className="preview-title">
-                  <a href={preview.url} target="_blank" rel="noreferrer">
-                    {preview.title}
-                  </a>
-                </div>
-                <div className="ant-card-meta-description">{preview.description}</div>
-              </div>
+            <div className="preview-title">
+              <a
+                href={preview.url}
+                target="_blank"
+                rel="noreferrer"
+                className="truncate text-ellipsis	max-w-full"
+              >
+                {preview.title}
+              </a>
             </div>
-            {preview.img && (
-              <div className="ant-card-cover">
-                <img
-                  alt={preview.title}
-                  src={preview.img}
-                  onClick={() => window.open(preview.url, '_blank')}
-                />
-              </div>
-            )}
+            <div className="ant-card-meta-description">{preview.description}</div>
           </div>
         </div>
-      </Col>
-    </Row>
+        {preview.img && (
+          <div className="ant-card-cover">
+            <img
+              alt={preview.title}
+              src={preview.img}
+              onClick={() => window.open(preview.url, '_blank')}
+            />
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
