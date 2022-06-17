ae256f5b84ff3160766db8759ac93254eb2e1223
Romaric Mourgues <rmourgues@linagora.com>
2022-06-17 10:51:33 +0200

    Add usable colors

--- twake/frontend/tailwind.config.js
+++ twake/frontend/tailwind.config.js
@@ -1,5 +1,13 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+let shades = [];
+['slate', 'red', 'orange', 'green', 'blue'].map(color => {
+  [50, 100, 200, 300, 400, 500, 600, 700, 800, 900].map(shade => {
+    shades.push(`bg-${color}-${shade}`);
+    shades.push(`text-${color}-${shade}`);
+  });
+});
+
 module.exports = {
   content: ['./src/**/*.{html,js,ts,tsx}'],
   darkMode: 'class',
@@ -21,6 +29,6 @@ module.exports = {
       },
     },
   },
-  safelist: ['bg-green-500', 'bg-red-500', 'bg-orange-500'],
+  safelist: ['bg-green-500', 'bg-red-500', 'bg-orange-500', ...shades],
   plugins: [require('@tailwindcss/forms')],
 };
