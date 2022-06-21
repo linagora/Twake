b0c49afd2e69023fca3ea16b6843626c6fcccb2e
Romaric Mourgues <rmourgues@linagora.com>
2022-06-21 08:58:32 +0200

    Make sure badly formatted files are not returned

--- twake/backend/node/src/services/messages/web/controllers/views/search-files.ts
+++ twake/backend/node/src/services/messages/web/controllers/views/search-files.ts
@@ -119,7 +119,9 @@ export default async (
     }
   }
 
-  messageFiles = messageFiles.filter(mf => mf.message.subtype !== "deleted");
+  messageFiles = messageFiles
+    .filter(mf => mf.message.subtype !== "deleted")
+    .filter(a => a?.metadata?.external_id);
 
   return {
     resources: messageFiles,
