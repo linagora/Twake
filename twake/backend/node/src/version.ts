8d8de6ce64b6b31eaed69c09a173d17f2406ae5d
Romaric Mourgues <rmourgues@linagora.com>
2022-06-24 17:27:54 +0200

    ⬆️ Create next version number and changelog

--- twake/backend/node/src/version.ts
+++ twake/backend/node/src/version.ts
@@ -1,7 +1,7 @@
 export default {
-  current: /* @VERSION_DETAIL */ "2022.Q2.975",
+  current: /* @VERSION_DETAIL */ "2022.Q3.1050",
   minimal: {
-    web: /* @MIN_VERSION_WEB */ "2022.Q1.874",
+    web: /* @MIN_VERSION_WEB */ "2022.Q2.975",
     mobile: /* @MIN_VERSION_MOBILE */ "2022.Q1.874",
   },
 };
