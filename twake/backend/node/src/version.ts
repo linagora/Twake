9b0e9d32c87f526bbcbe8e7462310ea049a463a4
Romaric Mourgues <rmourgues@linagora.com>
2022-07-21 18:38:11 +0200

    🩹 Patch on 2022.Q3.1050

--- twake/backend/node/src/version.ts
+++ twake/backend/node/src/version.ts
@@ -1,5 +1,5 @@
 export default {
-  current: /* @VERSION_DETAIL */ "2022.Q3.1050",
+  current: /* @VERSION_DETAIL */ "2022.Q3.1060",
   minimal: {
     web: /* @MIN_VERSION_WEB */ "2022.Q2.975",
     mobile: /* @MIN_VERSION_MOBILE */ "2022.Q1.874",
--- twake/frontend/src/app/environment/version.ts
+++ twake/frontend/src/app/environment/version.ts
@@ -1,5 +1,5 @@
 export default {
   version: /* @VERSION */ '2022.Q3',
-  version_detail: /* @VERSION_DETAIL */ '2022.Q3.1050',
+  version_detail: /* @VERSION_DETAIL */ '2022.Q3.1060',
   version_name: /* @VERSION_NAME */ 'Albatros',
 };
