94f831b3c287cff5065dfe37bdae54bf789211d1
Romaric Mourgues <rmourgues@linagora.com>
2022-08-03 12:10:39 +0200

    Fix spy on

--- twake/backend/node/test/unit/core/platform/framework/decorators/realtime/created.test.ts
+++ twake/backend/node/test/unit/core/platform/framework/decorators/realtime/created.test.ts
@@ -1,12 +1,12 @@
 import { describe, expect, it, jest } from "@jest/globals";
 import { CreateResult } from "../../../../../../../src/core/platform/framework/api/crud-service";
 import { RealtimeCreated } from "../../../../../../../src/core/platform/framework/decorators";
-import { eventBus } from "../../../../../../../src/core/platform/services/realtime/bus";
+import { websocketEventBus } from "../../../../../../../src/core/platform/services/realtime/bus";
 import { ResourcePath } from "../../../../../../../src/core/platform/services/realtime/types";
 
 describe("The RealtimeCreated decorator", () => {
   it("should call the original method send back original result but do not emit event if result type is wrong", async done => {
-    const emitSpy = jest.spyOn(eventBus, "emit");
+    const emitSpy = jest.spyOn(websocketEventBus, "emit");
 
     class TestMe {
       @RealtimeCreated({ room: "/foo/bar" })
@@ -31,7 +31,7 @@ describe("The RealtimeCreated decorator", () => {
   });
 
   it("should call the original method send back original result and emit event", async done => {
-    const emitSpy = jest.spyOn(eventBus, "emit");
+    const emitSpy = jest.spyOn(websocketEventBus, "emit");
 
     class TestMe {
       @RealtimeCreated({ room: "/foo/bar", path: "/foo/bar/baz" })
@@ -72,7 +72,7 @@ describe("The RealtimeCreated decorator", () => {
   });
 
   it("should emit event with path computed from function", async done => {
-    const emitSpy = jest.spyOn(eventBus, "emit");
+    const emitSpy = jest.spyOn(websocketEventBus, "emit");
 
     class TestMe {
       @RealtimeCreated<string>(input => [
