e218b6bf7bdfa730e9049acadc7d94bebe3e1280
Romaric Mourgues <rmourgues@linagora.com>
2022-09-19 09:52:50 +0200

    Fix css / tailwind

--- twake/frontend/src/app/components/user-card/user-card.tsx
+++ twake/frontend/src/app/components/user-card/user-card.tsx
@@ -7,8 +7,7 @@ import UserService from 'app/features/users/services/current-user-service';
 import { UserType } from 'app/features/users/types/user';
 import './user-card.scss';
 import useRouterCompany from 'app/features/router/hooks/use-router-company';
-import { TrashIcon } from '@heroicons/react/outline'
-
+import { TrashIcon } from '@heroicons/react/outline';
 
 type PropsType = {
   user: UserType;
@@ -21,9 +20,9 @@ export default (props: PropsType): JSX.Element => {
   return (
     <>
       {!UserService.isInCompany(props.user, companyId) && (
-        <div className="bg-zinc-700 flex items-center content-center custom-message mt-0 text-center text-xs py-2 px-2">
-            <TrashIcon className="h-4 w-4 inline-block" />
-            <span className="text-white ml-1">{Languages.t('general.user.deactivated')}</span>
+        <div className="text-white bg-zinc-700 flex items-center content-center -mt-4 -mx-4 mb-2 text-center text-xs p-2">
+          <TrashIcon className="h-4 w-4 inline-block" />
+          <span className="text-white ml-1">{Languages.t('general.user.deactivated')}</span>
         </div>
       )}
       <div className="user-card allow_selection">
