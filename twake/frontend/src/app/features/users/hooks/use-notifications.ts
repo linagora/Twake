7bdc70ed282d420a3fec111a7a550298669f9556
Romaric Mourgues <rmourgues@linagora.com>
2022-09-30 12:18:21 +0200

    Update knowledge graph

--- twake/frontend/src/app/features/users/hooks/use-notifications.ts
+++ twake/frontend/src/app/features/users/hooks/use-notifications.ts
@@ -85,9 +85,6 @@ export const useNotifications = () => {
           'id',
         );
         setBadges(list);
-        list.map(notification => {
-          userNotificationApiClient.acknowledge(notification);
-        });
       },
     [setBadges, badges],
   );
