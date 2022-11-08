fd5313286d9ffb5d10fbba62b0dcf7395b1d0af2
Romaric Mourgues <rmourgues@linagora.com>
2022-11-08 09:37:54 +0100

    Fix displaying the forwarded message

--- twake/frontend/src/app/views/applications/messages/message/parts/MessageContent.tsx
+++ twake/frontend/src/app/views/applications/messages/message/parts/MessageContent.tsx
@@ -3,6 +3,7 @@ import { useChannel, useIsChannelMember } from 'app/features/channels/hooks/use-
 import PseudoMarkdownCompiler from 'app/features/global/services/pseudo-markdown-compiler-service';
 import { useMessage } from 'app/features/messages/hooks/use-message';
 import { useVisibleMessagesEditorLocation } from 'app/features/messages/hooks/use-message-editor';
+import { MessageWithReplies } from 'app/features/messages/types/message';
 import useRouterWorkspace from 'app/features/router/hooks/use-router-workspace';
 import { useUser } from 'app/features/users/hooks/use-user';
 import User from 'app/features/users/services/current-user-service';
@@ -12,12 +13,13 @@ import QuotedContent, { useQuotedMessage } from 'app/molecules/quoted-content';
 import { ViewContext } from 'app/views/client/main-view/MainContent';
 import classNames from 'classnames';
 import 'moment-timezone';
-import { useContext, useEffect, useState } from 'react';
+import { ReactNode, useContext, useEffect, useState } from 'react';
 import { gotoMessage } from 'src/utils/messages';
 import { MessageContext } from '../message-with-replies';
 import Blocks from './Blocks';
 import DeletedContent from './DeletedContent';
 import LinkPreview from './LinkPreview';
+import MessageForward from './message-forward';
 import MessageAttachments from './MessageAttachments';
 import MessageEdition from './MessageEdition';
 import MessageHeader from './MessageHeader';
@@ -45,8 +47,7 @@ export default (props: Props) => {
   const quotedMessage = useQuotedMessage(message, context);
 
   const { channel } = useChannel(channelId);
-  const showQuotedMessage =
-    quotedMessage && quotedMessage.thread_id && channel.visibility === 'direct';
+  const showQuotedMessage = quotedMessage && quotedMessage.thread_id;
   let authorName = '';
   const currentRouterWorkspace = useRouterWorkspace();
   const workspaceId =
@@ -115,11 +116,31 @@ export default (props: Props) => {
       key={`message_container_${message.id}`}
     >
       <MessageHeader linkToThread={props.linkToThread} />
-      {showQuotedMessage && !showEdition && (
+      {showQuotedMessage && !showEdition && quotedMessage.channel_id === context.channelId && (
         <MessageQuote
           className="mb-1"
           author={authorName}
           message={quotedContent}
+          closable={false}
+          deleted={deletedQuotedMessage}
+          goToMessage={() =>
+            gotoMessage(
+              quotedMessage,
+              quotedMessage.company_id || context.companyId,
+              quotedMessage.channel_id || context.channelId,
+              quotedMessage.workspace_id || workspaceId,
+            )
+          }
+        />
+      )}
+      {showQuotedMessage && !showEdition && quotedMessage.channel_id !== context.channelId && (
+        <MessageForward
+          onAction={(type: string, id: string, context: unknown, passives: unknown) => {
+            if (isChannelMember) onAction(type, id, context, passives);
+          }}
+          className="mb-1"
+          author={authorName}
+          message={quotedMessage}
           closable={false}
           deleted={deletedQuotedMessage}
           goToMessage={() =>
@@ -138,45 +159,25 @@ export default (props: Props) => {
         </div>
       )}
       {!showEdition && (
-        <div className="content-parent dont-break-out">
-          {deleted === true ? (
-            <div className="deleted-message">
-              <DeletedContent userId={message.user_id || ''} key={`deleted_${message.thread_id}`} />
-            </div>
-          ) : (
+        <MessageBlockContent
+          onAction={(type: string, id: string, context: unknown, passives: unknown) => {
+            if (isChannelMember) onAction(type, id, context, passives);
+          }}
+          deleted={deleted}
+          linkToThread={props.linkToThread}
+          message={message}
+          className={classNames({
+            message_is_loading: messageIsLoading,
+            'message-not-sent': messageSaveFailed,
+          })}
+          suffix={
             <>
-              <div
-                className={classNames('content allow_selection', {
-                  message_is_loading: messageIsLoading,
-                  'message-not-sent': messageSaveFailed,
-                })}
-              >
-                {!!props.linkToThread && message.text}
-                {!props.linkToThread && (
-                  <>
-                    <Blocks
-                      blocks={message.blocks}
-                      fallback={PseudoMarkdownCompiler.transformBackChannelsUsers(message.text)}
-                      onAction={(type: string, id: string, context: unknown, passives: unknown) => {
-                        if (isChannelMember) onAction(type, id, context, passives);
-                      }}
-                      allowAdvancedBlocks={message.subtype === 'application'}
-                    />
-                  </>
-                )}
-              </div>
-
               {message?.files && (message?.files?.length || 0) > 0 && <MessageAttachments />}
-              {message?.links &&
-                (message?.links?.length || 0) > 0 &&
-                message.links
-                  .filter(link => link && (link.title || link.description || link.img))
-                  .map((preview, i) => <LinkPreview key={i} preview={preview} />)}
               {!messageSaveFailed && <Reactions />}
               {messageSaveFailed && !messageIsLoading && <RetryButtons />}
             </>
-          )}
-        </div>
+          }
+        />
       )}
       {isChannelMember &&
         !showEdition &&
@@ -193,6 +194,56 @@ export default (props: Props) => {
         )}
       {showMessageStatus && !showEdition && (
         <MessageStatus key={`message_status_${message.id}`} status={message.status} />
+      )}
+    </div>
+  );
+};
+
+export const MessageBlockContent = ({
+  deleted,
+  message,
+  linkToThread,
+  suffix,
+  className,
+  onAction,
+}: {
+  deleted: boolean;
+  linkToThread?: boolean;
+  message: MessageWithReplies;
+  suffix?: ReactNode;
+  className?: string;
+  onAction: (type: string, id: string, context: unknown, passives: unknown) => void;
+}) => {
+  return (
+    <div className="content-parent dont-break-out">
+      {deleted === true ? (
+        <div className="deleted-message">
+          <DeletedContent userId={message.user_id || ''} key={`deleted_${message.thread_id}`} />
+        </div>
+      ) : (
+        <>
+          <div className={'content allow_selection' + (className || '')}>
+            {!!linkToThread && message.text}
+            {!linkToThread && (
+              <>
+                <Blocks
+                  blocks={message.blocks}
+                  fallback={PseudoMarkdownCompiler.transformBackChannelsUsers(message.text)}
+                  onAction={onAction}
+                  allowAdvancedBlocks={message.subtype === 'application'}
+                />
+              </>
+            )}
+          </div>
+
+          {message?.links &&
+            (message?.links?.length || 0) > 0 &&
+            message.links
+              .filter(link => link && (link.title || link.description || link.img))
+              .map((preview, i) => <LinkPreview key={i} preview={preview} />)}
+
+          {suffix}
+        </>
       )}
     </div>
   );
