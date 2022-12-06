cbe5635ce01e8e965173384bfb7d5fb86646263f
Romaric Mourgues <rmourgues@linagora.com>
2022-12-06 16:06:42 +0100

    Create hooks for new Drive in node

---
+++ twake/frontend/src/app/features/drive/api-client/api-client.ts
@@ -0,0 +1,40 @@
+import Api from '../../global/framework/api-service';
+import { DriveItem, DriveItemDetails, DriveItemVersion } from '../types';
+
+export class DriveApiClient {
+  static async get(companyId: string, id: string | 'trash' | '') {
+    return await Api.get<DriveItemDetails>(
+      `/internal/services/documents/v1/companies/${companyId}/item/${id}`,
+    );
+  }
+
+  static async remove(companyId: string, id: string | 'trash' | '') {
+    return await Api.delete<void>(
+      `/internal/services/documents/v1/companies/${companyId}/item/${id}`,
+    );
+  }
+
+  static async update(companyId: string, id: string, update: Partial<DriveItem>) {
+    return await Api.post<Partial<DriveItem>, DriveItem>(
+      `/internal/services/documents/v1/companies/${companyId}/item/${id}`,
+      update,
+    );
+  }
+
+  static async create(
+    companyId: string,
+    data: { item: Partial<DriveItem>; version: Partial<DriveItemVersion> },
+  ) {
+    return await Api.post<
+      { item: Partial<DriveItem>; version: Partial<DriveItemVersion> },
+      DriveItem
+    >(`/internal/services/documents/v1/companies/${companyId}/item`, data);
+  }
+
+  static async createVersion(companyId: string, id: string, version: Partial<DriveItemVersion>) {
+    return await Api.post<Partial<DriveItemVersion>, DriveItemVersion>(
+      `/internal/services/documents/v1/companies/${companyId}/item/${id}/version`,
+      version,
+    );
+  }
+}
---
+++ twake/frontend/src/app/features/drive/hooks/use-drive-children.tsx
@@ -0,0 +1,58 @@
+import { useGlobalEffect } from 'app/features/global/hooks/use-global-effect';
+import { ToasterService } from 'app/features/global/services/toaster-service';
+import { LoadingState } from 'app/features/global/state/atoms/Loading';
+import useRouterCompany from 'app/features/router/hooks/use-router-company';
+import { useCallback } from 'react';
+import { useRecoilCallback, useRecoilState, useRecoilValue } from 'recoil';
+import { DriveApiClient } from '../api-client/api-client';
+import { DriveItemAtom, DriveItemChildrenAtom } from '../state/store';
+import { DriveItem } from '../types';
+
+/**
+ * Returns the children of a drive item
+ * @param id
+ * @returns
+ */
+export const useDriveChildren = (id: string | 'trash' | '' = '') => {
+  const companyId = useRouterCompany();
+  const [loading, setLoading] = useRecoilState(LoadingState('useDriveChildren-' + id));
+  const children = useRecoilValue(DriveItemChildrenAtom(id));
+
+  const refresh = useRecoilCallback(
+    ({ set, snapshot }) =>
+      async () => {
+        setLoading(true);
+        try {
+          const details = await DriveApiClient.get(companyId, id);
+          set(DriveItemChildrenAtom(id), details.children);
+          set(DriveItemAtom(id), details);
+          for (const child of details.children) {
+            const currentValue = snapshot.getLoadable(DriveItemAtom(child.id)).contents;
+            set(DriveItemAtom(child.id), { ...currentValue, item: child });
+          }
+          setLoading(false);
+          return details;
+        } catch (e) {
+          ToasterService.error('Unable to load your files.');
+        }
+        setLoading(false);
+      },
+    [companyId, id, setLoading],
+  );
+
+  const create = useCallback(
+    async (item: Partial<DriveItem>, version: Partial<DriveItem>) => {
+      setLoading(true);
+      try {
+        await DriveApiClient.create(companyId, { item, version });
+        await refresh();
+      } catch (e) {
+        ToasterService.error('Unable to create a new file.');
+      }
+      setLoading(false);
+    },
+    [id, setLoading, refresh],
+  );
+
+  return { loading, create, children, refresh };
+};
