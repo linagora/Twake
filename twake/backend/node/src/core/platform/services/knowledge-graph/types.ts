30c35e9529fa2b741f012c9a6969c0d9cec7a1ca
Romaric Mourgues <rmourgues@linagora.com>
2022-12-06 12:38:14 +0100

    Create kg push listener

--- twake/backend/node/src/core/platform/services/knowledge-graph/types.ts
+++ twake/backend/node/src/core/platform/services/knowledge-graph/types.ts
@@ -90,3 +90,20 @@ export enum KnowledgeGraphEvents {
   MESSAGE_UPSERT = "kg:message:upsert",
   USER_UPSERT = "kg:user:upsert",
 }
+
+export type KnowledgeGraphCallbackEvent = {
+  recipients: {
+    type: "user";
+    id: string; // KG user id which is a md5 of the email
+  }[];
+  event: {
+    type: "user_tags"; //More events will be added later
+    data: {
+      //For user_tags event only
+      tags?: {
+        value: string;
+        weight: number;
+      }[];
+    };
+  };
+};
