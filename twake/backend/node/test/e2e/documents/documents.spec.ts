3f0fb93b833024201877fc2c00f6b5b1b7c6822d
Khaled FERJANI <kferjani@linagora.com>
2023-01-04 11:13:26 +0100

    added more e2e tests

--- twake/backend/node/test/e2e/documents/documents.spec.ts
+++ twake/backend/node/test/e2e/documents/documents.spec.ts
@@ -1,9 +1,24 @@
 import { describe, beforeEach, afterEach, it, expect } from "@jest/globals";
+import { deserialize } from "class-transformer";
 import { init, TestPlatform } from "../setup";
-import { e2e_createDocument } from "./utils";
+import { e2e_createDocument, e2e_getDocument } from "./utils";
 
 describe("the Documents feature", () => {
   let platform: TestPlatform;
+
+  class DriveFileMockClass {
+    id: string;
+    name: string;
+    size: number;
+    added: string;
+    parent_id: string;
+  }
+
+  class DriveItemDetailsMockClass {
+    path: string[];
+    item: DriveFileMockClass;
+    children: DriveFileMockClass[];
+  }
 
   beforeEach(async () => {
     platform = await init({
@@ -37,15 +52,31 @@ describe("the Documents feature", () => {
   describe("On user send Drive item", () => {
     it("did create the drive item", async done => {
       const item = {
-        company_id: "123",
-        name: "drive item",
+        name: "new test file",
+        parent_id: "root",
+        company_id: platform.workspace.company_id,
       };
 
       const version = {};
 
       const response = await e2e_createDocument(platform, item, version);
+      const result = deserialize<DriveFileMockClass>(DriveFileMockClass, response.body);
 
-      expect(response.body).toBeDefined();
+      expect(result).toBeDefined();
+      expect(result.name).toEqual("new test file");
+      expect(result.added).toBeDefined();
+
+      done && done();
+    });
+
+    it("did fetch the drive item", async done => {
+      const response = await e2e_getDocument(platform, "");
+      const result = deserialize<DriveItemDetailsMockClass>(
+        DriveItemDetailsMockClass,
+        response.body,
+      );
+
+      expect(result.item.name).toEqual("root");
 
       done && done();
     });
--- twake/backend/node/test/e2e/documents/utils.ts
+++ twake/backend/node/test/e2e/documents/utils.ts
@@ -2,7 +2,8 @@ import { DriveFile } from "../../../src/services/documents/entities/drive-file";
 import { FileVersion } from "../../../src/services/documents/entities/file-version";
 import { TestPlatform } from "../setup";
 
-const url = "/internal/services/messages/v1";
+const url = "/internal/services/documents/v1";
+
 export const e2e_createDocument = async (
   platform: TestPlatform,
   item: Partial<DriveFile>,
@@ -19,6 +20,22 @@ export const e2e_createDocument = async (
     payload: {
       item,
       version,
+    },
+  });
+
+  await new Promise(resolve => setTimeout(resolve, 200));
+
+  return response;
+};
+
+export const e2e_getDocument = async (platform: TestPlatform, id: string | "root" | "trash") => {
+  const token = await platform.auth.getJWTToken();
+
+  const response = await platform.app.inject({
+    method: "GET",
+    url: `${url}/companies/${platform.workspace.company_id}/item/${id}`,
+    headers: {
+      authorization: `Bearer ${token}`,
     },
   });
 
