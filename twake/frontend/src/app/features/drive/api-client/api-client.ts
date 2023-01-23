da93c78c5b0ec7a7dcc6c0ae3b423213b3dcdc0a
Romaric Mourgues <rmourgues@linagora.com>
2023-01-23 11:18:50 +0100

    Add download token generation

--- twake/frontend/src/app/features/drive/api-client/api-client.ts
+++ twake/frontend/src/app/features/drive/api-client/api-client.ts
@@ -42,18 +42,29 @@ export class DriveApiClient {
     );
   }
 
+  static async getDownloadToken(companyId: string, ids: string[], versionId?: string) {
+    return Api.get<{ token: string }>(
+      `/internal/services/documents/v1/companies/${companyId}/item/download/token` +
+        `?items=${ids.join(',')}&version_id=${versionId}`,
+    );
+  }
+
   static async getDownloadUrl(companyId: string, id: string, versionId?: string) {
+    const { token } = await DriveApiClient.getDownloadToken(companyId, [id], versionId);
     if (versionId)
       return Api.route(
-        `/internal/services/documents/v1/companies/${companyId}/item/${id}/download?version_id=${versionId}`,
+        `/internal/services/documents/v1/companies/${companyId}/item/${id}/download?version_id=${versionId}&token=${token}`,
       );
-    return Api.route(`/internal/services/documents/v1/companies/${companyId}/item/${id}/download`);
+    return Api.route(
+      `/internal/services/documents/v1/companies/${companyId}/item/${id}/download?token=${token}`,
+    );
   }
 
   static async getDownloadZipUrl(companyId: string, ids: string[]) {
+    const { token } = await DriveApiClient.getDownloadToken(companyId, ids);
     return Api.route(
       `/internal/services/documents/v1/companies/${companyId}/item/download/zip` +
-        `?items=${ids.join(',')}`,
+        `?items=${ids.join(',')}&token=${token}`,
     );
   }
 }
