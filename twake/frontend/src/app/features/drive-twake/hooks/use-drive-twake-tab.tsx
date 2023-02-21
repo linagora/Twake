c7509487bf49157202c5351d9e43347eebfeea30
Romaric Mourgues <rmourgues@linagora.com>
2023-02-21 09:23:29 +0100

    Prepare frontend logic for tabs configuration

--- twake/frontend/src/app/features/drive-twake/hooks/use-drive-twake-tab.tsx
+++ twake/frontend/src/app/features/drive-twake/hooks/use-drive-twake-tab.tsx
@@ -1,5 +1,5 @@
 import useRouterCompany from 'app/features/router/hooks/use-router-company';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { DriveTwakeApiClient } from '../api-client/api-client';
 import { DriveTwakeTabAtom } from '../state/store';
@@ -7,16 +7,23 @@ import { DriveTwakeTabAtom } from '../state/store';
 export const useDriveTwakeTab = (tabId: string) => {
   const companyId = useRouterCompany();
   const [tab, setTab] = useRecoilState(DriveTwakeTabAtom(tabId));
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    DriveTwakeApiClient.getTab(companyId, tabId).then(setTab);
+    setLoading(true);
+    DriveTwakeApiClient.getTab(companyId, tabId)
+      .then(setTab)
+      .finally(() => setLoading(false));
   }, [companyId, tabId]);
 
   return {
     tab,
     setTab: async (itemId: string) => {
+      setLoading(true);
       const tab = await DriveTwakeApiClient.setTab(companyId, tabId, itemId);
       if (tab.item_id) setTab(tab);
+      setLoading(false);
     },
+    loading,
   };
 };
--- twake/frontend/src/app/views/applications/drive/twake-tab-configuration.tsx
+++ twake/frontend/src/app/views/applications/drive/twake-tab-configuration.tsx
@@ -1,11 +1,27 @@
+import { Modal } from 'app/atoms/modal';
 import { useDriveTwakeTab } from 'app/features/drive-twake/hooks/use-drive-twake-tab';
+import { useDriveItem } from 'app/features/drive/hooks/use-drive-item';
+import { useState } from 'react';
 import { EmbedContext } from '.';
 
 export default ({ context }: { context?: EmbedContext }) => {
-  const { tab, setTab } = useDriveTwakeTab(context?.tabId || '');
-  //TODO load tab configuration from drive backend
-  // If nothing is configured, then show the selector and when selected the folder will give access to the whole channel
-  // If configured then show the content of the tab and forward the fact that the access is done through a specific channel
+  const { tab, setTab, loading } = useDriveTwakeTab(context?.tabId || '');
+  const { item, loading: itemLoading } = useDriveItem(tab?.item_id || '');
+  const [modifyConfiguration, setModifyConfiguration] = useState(false);
 
-  return <div>TODO: show tab configuration page</div>;
+  if (loading || itemLoading) return <></>;
+
+  // If nothing is configured, then show the selector and when selected the folder will give access to the whole channel
+  const modalOpen = !tab || !item || modifyConfiguration;
+
+  // If configured then show the content of the tab and forward the fact that the access is done through a specific channel
+  return (
+    <div>
+      <Modal open={modalOpen} onClose={() => {}}>
+        //TODO
+      </Modal>
+
+      {tab && item && <>TODO: show tab content page</>}
+    </div>
+  );
 };
